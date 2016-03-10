92f530bcc6e9b2aec6dad8fd24f133efeb835557
Jeen Broekstra <jeen.broekstra@gmail.com>
2016-03-11 10:48:10 +1300

    #6 added RDF4JException, made OpenRDFException subtype

--- core/model/src/main/java/org/eclipse/rdf4j/RDF4JException.java
+++ core/model/src/main/java/org/eclipse/rdf4j/RDF4JException.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ * Copyright (c) 2015 Eclipse RDF4J contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
@@ -8,30 +8,28 @@
 package org.eclipse.rdf4j;
 
 /**
- * General superclass of all unchecked exceptions that parts of OpenRDF Sesame
- * can throw.
+ * Superclass of all (runtime) exceptions thrown by RDF4J.
  * 
- * @author jeen
- * @deprecated use {@link RDF4JException} instead.
+ * @author Jeen Broekstra
  */
-@Deprecated
-public abstract class OpenRDFException extends RDF4JException {
+public abstract class RDF4JException extends RuntimeException {
 
-	private static final long serialVersionUID = 8913366826930181397L;
+	private static final long serialVersionUID = 5283957703704198489L;
 
-	public OpenRDFException() {
+	public RDF4JException() {
 		super();
 	}
 
-	public OpenRDFException(String msg) {
+	public RDF4JException(String msg) {
 		super(msg);
 	}
 
-	public OpenRDFException(Throwable t) {
+	public RDF4JException(Throwable t) {
 		super(t);
 	}
 
-	public OpenRDFException(String msg, Throwable t) {
+	public RDF4JException(String msg, Throwable t) {
 		super(msg, t);
+
 	}
 }
