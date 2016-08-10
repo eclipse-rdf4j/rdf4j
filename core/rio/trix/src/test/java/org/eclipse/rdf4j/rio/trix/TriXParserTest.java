370e3b3a7b37c5b003c14ac558758d3ec78613b8
Jeen Broekstra <jeen.broekstra@gmail.com>
2016-08-11 11:09:49 +1200

    #280 workaround in unit tests to account for different platform locales.
    Signed-off-by: Jeen Broekstra <jeen.broekstra@gmail.com>

--- core/rio/trix/src/test/java/org/eclipse/rdf4j/rio/trix/TriXParserTest.java
+++ core/rio/trix/src/test/java/org/eclipse/rdf4j/rio/trix/TriXParserTest.java
@@ -7,30 +7,18 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.trix;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.net.URL;
-import java.util.Collection;
-import java.util.Iterator;
 
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-import org.eclipse.rdf4j.model.vocabulary.DC;
-import org.eclipse.rdf4j.model.vocabulary.RDFS;
 import org.eclipse.rdf4j.rio.RDFParseException;
 import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.helpers.ParseErrorCollector;
 import org.eclipse.rdf4j.rio.helpers.StatementCollector;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -73,7 +61,8 @@ public class TriXParserTest {
 			parser.parse(in, "");
 		}
 		catch (RDFParseException e) {
-			assertEquals("Content is not allowed in prolog. [line 1, column 1]", e.getMessage());
+			// FIXME exact error message is locale-dependent. Just fall through, error is expected. See #280.
+			// assertEquals("Content is not allowed in prolog. [line 1, column 1]", e.getMessage());
 		}
 		finally {
 			// Reset System Error output to ensure that we don't interfere with other tests
