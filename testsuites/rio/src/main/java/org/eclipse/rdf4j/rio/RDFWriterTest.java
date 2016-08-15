fe71b5e433d68df977b54f982005addcc57d40b5
Peter Ansell <p_ansell@yahoo.com>
2016-08-15 00:22:51 -0400

    issue#207 : Reduce the scope of performance testing while unit testing
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- testsuites/rio/src/main/java/org/eclipse/rdf4j/rio/RDFWriterTest.java
+++ testsuites/rio/src/main/java/org/eclipse/rdf4j/rio/RDFWriterTest.java
@@ -704,7 +704,7 @@ public abstract class RDFWriterTest {
 	{
 		Model model = new LinkedHashModel();
 
-		for (int i = 0; i < 100000; i++) {
+		for (int i = 0; i < 10000; i++) {
 
 			Value obj = potentialObjects.get(prng.nextInt(potentialObjects.size()));
 			if (obj == litBigPlaceholder) {
