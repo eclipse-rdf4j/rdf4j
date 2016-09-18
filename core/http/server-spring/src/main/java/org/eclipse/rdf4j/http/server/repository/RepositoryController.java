fe189fc84ca0bb2048f396579d8ab1ffd30f18fd
Mark Hale <mark.hale@physics.org>
2016-09-18 11:40:33 +0100

    Make use of QueryResults helper methods.
    Signed-off-by: Mark Hale <mark.hale@physics.org>

--- core/http/server-spring/src/main/java/org/eclipse/rdf4j/http/server/repository/RepositoryController.java
+++ core/http/server-spring/src/main/java/org/eclipse/rdf4j/http/server/repository/RepositoryController.java
@@ -29,7 +29,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpStatus;
 import org.eclipse.rdf4j.RDF4JException;
-import org.eclipse.rdf4j.common.iteration.DistinctIteration;
 import org.eclipse.rdf4j.common.lang.FileFormat;
 import org.eclipse.rdf4j.common.lang.service.FileFormatServiceRegistry;
 import org.eclipse.rdf4j.common.webapp.util.HttpServerUtil;
@@ -52,11 +51,10 @@ import org.eclipse.rdf4j.query.QueryEvaluationException;
 import org.eclipse.rdf4j.query.QueryInterruptedException;
 import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.query.QueryResult;
+import org.eclipse.rdf4j.query.QueryResults;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.query.UnsupportedQueryLanguageException;
-import org.eclipse.rdf4j.query.impl.IteratingGraphQueryResult;
-import org.eclipse.rdf4j.query.impl.IteratingTupleQueryResult;
 import org.eclipse.rdf4j.query.impl.SimpleDataset;
 import org.eclipse.rdf4j.query.resultio.BooleanQueryResultWriterRegistry;
 import org.eclipse.rdf4j.query.resultio.TupleQueryResultWriterRegistry;
@@ -382,11 +380,11 @@ public class RepositoryController extends AbstractController {
 	private static QueryResult<?> distinct(QueryResult<?> qr) {
 		if (qr instanceof TupleQueryResult) {
 			TupleQueryResult tqr = (TupleQueryResult) qr;
-			return new IteratingTupleQueryResult(tqr.getBindingNames(), new DistinctIteration<>(tqr));
+			return QueryResults.distinctResults(tqr);
 		}
 		else if (qr instanceof GraphQueryResult) {
 			GraphQueryResult gqr = (GraphQueryResult)qr;
-			return new IteratingGraphQueryResult(gqr.getNamespaces(), new DistinctIteration<>(gqr));
+			return QueryResults.distinctResults(gqr);
 		}
 		else {
 			return qr;
