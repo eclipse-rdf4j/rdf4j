b3a4877d738315e51b243d4e53762afc15df5a88
Jeen Broekstra <jeen.broekstra@gmail.com>
2016-10-20 12:57:56 +1100

    #164 fix compilation failure
    Signed-off-by: Jeen Broekstra <jeen.broekstra@gmail.com>

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/TupleFunctionEvaluationStrategy.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/TupleFunctionEvaluationStrategy.java
@@ -23,6 +23,7 @@ import org.eclipse.rdf4j.query.algebra.Var;
 import org.eclipse.rdf4j.query.algebra.evaluation.EvaluationStrategy;
 import org.eclipse.rdf4j.query.algebra.evaluation.QueryBindingSet;
 import org.eclipse.rdf4j.query.algebra.evaluation.TripleSource;
+import org.eclipse.rdf4j.query.algebra.evaluation.federation.AbstractFederatedServiceResolver;
 import org.eclipse.rdf4j.query.algebra.evaluation.federation.FederatedServiceResolver;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.TupleFunction;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.TupleFunctionRegistry;
@@ -52,6 +53,12 @@ public class TupleFunctionEvaluationStrategy extends StrictEvaluationStrategy {
 	{
 		super(tripleSource, dataset, serviceResolver, iterationCacheSyncThreshold);
 		this.tupleFuncRegistry = tupleFuncRegistry;
+	}
+
+	public TupleFunctionEvaluationStrategy(TripleSource tripleSource, Dataset dataset,
+			AbstractFederatedServiceResolver serviceResolver, TupleFunctionRegistry tupleFunctionRegistry)
+	{
+		this(tripleSource, dataset, serviceResolver, tupleFunctionRegistry, 0);
 	}
 
 	@Override
