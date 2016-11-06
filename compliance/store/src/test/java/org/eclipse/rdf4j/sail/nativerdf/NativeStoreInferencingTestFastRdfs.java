9ddeeabed601b371502684a984e74eca12b7225e
Håvard M. Ottestad <hmottestad@gmail.com>
2016-11-06 17:27:38 +0100

    initial commit
    Signed-off-by: Håvard M. Ottestad <hmottestad@gmail.com>

---
+++ compliance/store/src/test/java/org/eclipse/rdf4j/sail/nativerdf/NativeStoreInferencingTestFastRdfs.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sail.nativerdf;
+
+import org.eclipse.rdf4j.sail.InferencingTest;
+import org.eclipse.rdf4j.sail.NotifyingSail;
+import org.eclipse.rdf4j.sail.Sail;
+import org.eclipse.rdf4j.sail.inferencer.fc.FastRdfsForwardChainingSail;
+import org.eclipse.rdf4j.sail.inferencer.fc.ForwardChainingRDFSInferencer;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+
+public class NativeStoreInferencingTestFastRdfs extends InferencingTest {
+
+	@Rule
+	public TemporaryFolder tempDir = new TemporaryFolder();
+
+	@Override
+	protected Sail createSail() {
+		try {
+			NotifyingSail sailStack = new NativeStore(tempDir.newFolder("nativestore"), "spoc,posc");
+			sailStack = new FastRdfsForwardChainingSail(sailStack, true);
+			return sailStack;
+		}
+		catch (IOException e) {
+			throw new AssertionError(e);
+		}
+	}
+}
