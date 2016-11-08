400a597d7e9cc79c5901845f7469e45341600923
Håvard M. Ottestad <hmottestad@gmail.com>
2016-11-08 19:35:55 +0100

    Updated header
    Signed-off-by: Håvard M. Ottestad <hmottestad@gmail.com>

--- core/sail/inferencer/src/main/java/org/eclipse/rdf4j/sail/inferencer/fc/FastRdfsForwardChainingSail.java
+++ core/sail/inferencer/src/main/java/org/eclipse/rdf4j/sail/inferencer/fc/FastRdfsForwardChainingSail.java
@@ -1,5 +1,11 @@
 package org.eclipse.rdf4j.sail.inferencer.fc;
-
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
 import org.eclipse.rdf4j.common.iteration.Iterations;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Resource;
