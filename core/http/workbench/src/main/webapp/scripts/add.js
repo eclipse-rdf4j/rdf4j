af496b53e91765fffa3e84ddad4721c6b3a29d6e
Jeen Broekstra <jeen.broekstra@gmail.com>
2016-11-16 17:28:38 +1100

    #67 fix in handling auto detect option
    Signed-off-by: Jeen Broekstra <jeen.broekstra@gmail.com>

--- core/http/workbench/src/main/webapp/scripts/add.js
+++ core/http/workbench/src/main/webapp/scripts/add.js
@@ -33,8 +33,6 @@ var workbench;
             var istext = (selected == 'text');
             $('#text').prop('disabled', !istext);
             var contentType = $('#Content-Type');
-            var firstType = contentType.find('option:first');
-            firstType.prop('disabled', true);
             $('#source-' + selected).prop('checked', true);
             var isfile = (selected == 'file');
             var file = $('#file');
@@ -42,7 +40,9 @@ var workbench;
             var isurl = (selected == 'url');
             var url = $('#url');
             url.prop('disabled', !isurl);
+            var autodetect = contentType.find("option[value='autodetect']");
             if (istext) {
+                autodetect.prop('disabled', true);
                 var turtle = contentType.find("option[value='application/x-turtle']");
                 if (turtle.length == 0) {
                     turtle = contentType.find("option[value='text/turtle']");
@@ -52,7 +52,8 @@ var workbench;
                 }
             }
             else {
-                firstType.prop('selected', true);
+                autodetect.prop('disabled', false);
+                autodetect.prop('selected', true);
                 var baseURI = $('#baseURI');
                 var checked = $('#useForContext').prop('checked');
                 if (isfile) {
