84ef44c23397d552be6f9fc49be3ba246034be33
Jeen Broekstra <jeen.broekstra@gmail.com>
2016-11-25 09:31:39 +1100

    #668 reverted hashCode implementation to conform to API spec; modified unit tests
    Signed-off-by: Jeen Broekstra <jeen.broekstra@gmail.com>

--- core/model/src/main/java/org/eclipse/rdf4j/model/impl/SimpleLiteral.java
+++ core/model/src/main/java/org/eclipse/rdf4j/model/impl/SimpleLiteral.java
@@ -169,18 +169,10 @@ public class SimpleLiteral implements Literal {
 		return false;
 	}
 
-	// overrides Object.hashCode(), implements hashCode()
+	// overrides Object.hashCode(), implements Literal.hashCode()
 	@Override
 	public int hashCode() {
-		int hashCode = label.hashCode();
-		if (language != null) {
-			hashCode = 31 * hashCode + language.hashCode();
-		}
-		if (datatype != null) {
-			hashCode = 31 * hashCode + datatype.hashCode();
-		}
-
-		return hashCode;
+		return label.hashCode();
 	}
 
 	/**
