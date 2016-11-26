5b40085af348b82c4fb5065a4749809c6b2c1ab7
Håvard M. Ottestad <hmottestad@gmail.com>
2016-11-26 19:58:52 +0100

    Added a new test that fails
    Signed-off-by: Håvard M. Ottestad <hmottestad@gmail.com>

--- compliance/store/src/test/java/org/eclipse/rdf4j/sail/memory/FastRdfsForwardChainingMemInferencingTest.java
+++ compliance/store/src/test/java/org/eclipse/rdf4j/sail/memory/FastRdfsForwardChainingMemInferencingTest.java
@@ -13,7 +13,7 @@ import org.eclipse.rdf4j.sail.Sail;
 import org.eclipse.rdf4j.sail.inferencer.fc.FastRdfsForwardChainingSail;
 import org.eclipse.rdf4j.sail.inferencer.fc.ForwardChainingRDFSInferencer;
 
-public class FastMemInferencingTest extends InferencingTest {
+public class FastRdfsForwardChainingMemInferencingTest extends InferencingTest {
 
 	@Override
 	protected Sail createSail() {
