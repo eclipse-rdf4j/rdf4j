d78a739fa45b343c0e2b88376cd3ad1677437b5c
Jeen Broekstra <jeen.broekstra@gmail.com>
2017-01-11 11:12:58 +1100

    #643 redone code simplification without reformatting and Model
    Signed-off-by: Jeen Broekstra <jeen.broekstra@gmail.com>

--- core/sail/inferencer/src/main/java/org/eclipse/rdf4j/sail/inferencer/fc/ForwardChainingSchemaCachingRDFSInferencer.java
+++ core/sail/inferencer/src/main/java/org/eclipse/rdf4j/sail/inferencer/fc/ForwardChainingSchemaCachingRDFSInferencer.java
@@ -8,22 +8,10 @@
 
 package org.eclipse.rdf4j.sail.inferencer.fc;
 
-import org.eclipse.rdf4j.common.iteration.Iterations;
-import org.eclipse.rdf4j.model.Resource;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.ValueFactory;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-import org.eclipse.rdf4j.repository.Repository;
-import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.eclipse.rdf4j.repository.RepositoryResult;
-import org.eclipse.rdf4j.sail.NotifyingSail;
-import org.eclipse.rdf4j.sail.SailConflictException;
-import org.eclipse.rdf4j.sail.SailException;
-import org.eclipse.rdf4j.sail.inferencer.InferencerConnection;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -33,6 +21,22 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.StampedLock;
 import java.util.stream.Collectors;
+
+import org.eclipse.rdf4j.common.iteration.Iterations;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryResult;
+import org.eclipse.rdf4j.sail.NotifyingSail;
+import org.eclipse.rdf4j.sail.SailConflictException;
+import org.eclipse.rdf4j.sail.SailException;
+import org.eclipse.rdf4j.sail.inferencer.InferencerConnection;
 
 /**
  * @author Håvard Mikkelsen Ottestad
@@ -47,38 +51,41 @@ public class ForwardChainingSchemaCachingRDFSInferencer extends AbstractForwardC
 
     private StampedLock readWriteLock = new StampedLock();
 
+    private AtomicInteger numberOfThreadsWaitingForWriteLock = new AtomicInteger(0);
+    
     // If false, the inferencer will skip some RDFS rules.
     boolean useAllRdfsRules = true;
 
-    // Tbox cache
-    Collection<Resource> properties = new HashSet<>();
-    Collection<Resource> types = new HashSet<>();
-    Collection<Statement> subClassOfStatements = new ArrayList<>();
-    Collection<Statement> subPropertyOfStatements = new ArrayList<>();
-    Collection<Statement> rangeStatements = new ArrayList<>();
-    Collection<Statement> domainStatements = new ArrayList<>();
+    // Schema cache
+    private final Collection<Resource> properties = new HashSet<>();
+    private final Collection<Resource> types = new HashSet<>();
+    
+    private final Collection<Statement> subClassOfStatements = new ArrayList<>();
+    private final Collection<Statement> subPropertyOfStatements = new ArrayList<>();
+    private final Collection<Statement> rangeStatements = new ArrayList<>();
+    private final Collection<Statement> domainStatements = new ArrayList<>();
 
-    // Forward chained Tbox cache as lookup tables
-    Map<Resource, Set<Resource>> calculatedTypes = new HashMap<>();
-    Map<Resource, Set<Resource>> calculatedProperties = new HashMap<>();
-    Map<Resource, Set<Resource>> calculatedRange = new HashMap<>();
-    Map<Resource, Set<Resource>> calculatedDomain = new HashMap<>();
+    // Forward chained schema cache as lookup tables
+    private final Map<Resource, Set<Resource>> calculatedTypes = new HashMap<>();
+    private final Map<Resource, Set<Resource>> calculatedProperties = new HashMap<>();
+    private final Map<Resource, Set<Resource>> calculatedRange = new HashMap<>();
+    private final Map<Resource, Set<Resource>> calculatedDomain = new HashMap<>();
 
     // The inferencer has been instantiated from another inferencer and shares it's schema with that one
     private boolean sharedSchema;
 
     void clearInferenceTables() {
-        properties = new HashSet<>();
-        types = new HashSet<>();
-        subClassOfStatements = new ArrayList<>();
-        subPropertyOfStatements = new ArrayList<>();
-        rangeStatements = new ArrayList<>();
-        domainStatements = new ArrayList<>();
+        properties.clear();
+        types.clear();
+        subClassOfStatements.clear();
+        subPropertyOfStatements.clear();
+        rangeStatements.clear();
+        domainStatements.clear();
 
-        calculatedTypes = new HashMap<>();
-        calculatedProperties = new HashMap<>();
-        calculatedRange = new HashMap<>();
-        calculatedDomain = new HashMap<>();
+        calculatedTypes.clear();
+        calculatedProperties.clear();
+        calculatedRange.clear();
+        calculatedDomain.clear();
     }
 
     void readLock(ForwardChainingSchemaCachingRDFSInferencerConnection connection) {
@@ -110,7 +117,6 @@ public class ForwardChainingSchemaCachingRDFSInferencer extends AbstractForwardC
     }
 
 
-    private AtomicInteger numberOfThreadsWaitingForWriteLock = new AtomicInteger(0);
 
     void upgradeLock(ForwardChainingSchemaCachingRDFSInferencerConnection connection) {
 
@@ -201,14 +207,10 @@ public class ForwardChainingSchemaCachingRDFSInferencer extends AbstractForwardC
         }
 
 
-        ForwardChainingSchemaCachingRDFSInferencerConnection connection = null;
+        try (final ForwardChainingSchemaCachingRDFSInferencerConnection conn = getConnection() ){
+            conn.begin();
 
-        try {
-            connection = getConnection();
-            final ForwardChainingSchemaCachingRDFSInferencerConnection finalConnection = connection;
-            finalConnection.begin();
-
-            finalConnection.addAxiomStatements();
+            conn.addAxiomStatements();
 
 
             List<Statement> tboxStatments = new ArrayList<>();
@@ -219,24 +221,20 @@ public class ForwardChainingSchemaCachingRDFSInferencer extends AbstractForwardC
                     schemaConnection.begin();
                     RepositoryResult<Statement> statements = schemaConnection.getStatements(null, null, null);
                     tboxStatments = Iterations.stream(statements)
-                        .peek(finalConnection::statementCollector)
+                        .peek(conn::processForSchemaCache)
                         .collect(Collectors.toList());
                     schemaConnection.commit();
                 }
             }
 
-            finalConnection.calculateInferenceMaps();
+            calculateInferenceMaps(conn);
 
             if (schema != null) {
-                tboxStatments.forEach(statement -> finalConnection.addStatement(statement.getSubject(), statement.getPredicate(), statement.getObject(), statement.getContext()));
+                tboxStatments.forEach(statement -> conn.addStatement(statement.getSubject(), statement.getPredicate(), statement.getObject(), statement.getContext()));
             }
 
-            finalConnection.commit();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
-        }
+            conn.commit();
+        } 
 
     }
 
@@ -312,6 +310,262 @@ public class ForwardChainingSchemaCachingRDFSInferencer extends AbstractForwardC
         return ret;
 
     }
+    
+    long getSchemaSize() {
+    	return subClassOfStatements.size() + subPropertyOfStatements.size() + rangeStatements.size() 
+    			+ domainStatements.size() + properties.size() + types.size();
+    }
+    
+    void calculateInferenceMaps(ForwardChainingSchemaCachingRDFSInferencerConnection conn) {
+        calculateSubClassOf(subClassOfStatements);
+        properties.forEach(predicate -> {
+        				conn.addInferredStatement(predicate, RDF.TYPE, RDF.PROPERTY);
+        				calculatedProperties.put(predicate, new HashSet<>());
+        			});
+        calculateSubPropertyOf(subPropertyOfStatements);
 
+        calculateRangeDomain(rangeStatements, calculatedRange);
+        calculateRangeDomain(domainStatements, calculatedDomain);
+
+
+        calculatedTypes.forEach((subClass, superClasses) -> {
+            conn.addInferredStatement(subClass, RDFS.SUBCLASSOF, subClass);
+
+            superClasses.forEach(superClass -> {
+                conn.addInferredStatement(subClass, RDFS.SUBCLASSOF, superClass);
+                conn.addInferredStatement(superClass, RDFS.SUBCLASSOF, superClass);
+
+            });
+        });
+
+        calculatedProperties.forEach((sub, sups) -> {
+            conn.addInferredStatement(sub, RDFS.SUBPROPERTYOF, sub);
+
+            sups.forEach(sup -> {
+                conn.addInferredStatement(sub, RDFS.SUBPROPERTYOF, sup);
+                conn.addInferredStatement(sup, RDFS.SUBPROPERTYOF, sup);
+
+            });
+        });
+    }
+
+    void addSubClassOfStatement(Statement st) {
+    			subClassOfStatements.add(st);
+    			types.add(st.getSubject());
+    			types.add((Resource)st.getObject());
+    }
+    
+    void addSubPropertyOfStatement(Statement st) {
+		subPropertyOfStatements.add(st);
+		properties.add(st.getSubject());
+		properties.add((Resource)st.getObject());
+    }
+    
+    void addRangeStatement(Statement st) {
+    	rangeStatements.add(st);
+    	properties.add(st.getSubject());
+    	types.add((Resource)st.getObject());
+    }
+    
+    void addDomainStatement(Statement st) {
+    	domainStatements.add(st);
+    	properties.add(st.getSubject());
+    	types.add((Resource)st.getObject());
+    }
+    
+    boolean hasType(Resource r) {
+    	return types.contains(r);
+    }
+    
+    void addType(Resource r) {
+    	types.add(r);
+    }
+
+    boolean hasProperty(Resource property) {
+    	return properties.contains(property);
+    }
+    
+    void addProperty(Resource property) {
+       	properties.add(property);
+    }
+    
+    Set<Resource> resolveTypes(Resource value) {
+        Set<Resource> iris = calculatedTypes.get(value);
+
+        return iris != null ? iris : Collections.emptySet();
+    }
+
+    Set<Resource> resolveProperties(Resource predicate) {
+        Set<Resource> iris = calculatedProperties.get(predicate);
+
+        return iris != null ? iris : Collections.emptySet();
+    }
+
+
+    Set<Resource> resolveRangeTypes(IRI predicate) {
+        Set<Resource> iris = calculatedRange.get(predicate);
+
+        return iris != null ? iris : Collections.emptySet();
+    }
+
+    Set<Resource> resolveDomainTypes(IRI predicate) {
+        Set<Resource> iris = calculatedDomain.get(predicate);
+
+        return iris != null ? iris : Collections.emptySet();
+    }
+
+
+    private void calculateSubClassOf(Collection<Statement> subClassOfStatements) {
+        types.forEach(type -> {
+            if (!calculatedTypes.containsKey(type)) {
+                calculatedTypes.put(type, new HashSet<>());
+            }
+
+            calculatedTypes.get(type).add(type);
+
+        });
+
+
+        subClassOfStatements.forEach(s -> {
+            Resource subClass = s.getSubject();
+            if (!calculatedTypes.containsKey(subClass)) {
+                calculatedTypes.put(subClass, new HashSet<>());
+            }
+
+            calculatedTypes.get(subClass).add((Resource) s.getObject());
+
+        });
+
+
+        // Fixed point approach to finding all sub-classes.
+        // prevSize is the size of the previous application of the function
+        // newSize is the size of the current application of the function
+        // Fixed point is reached when they are the same.
+        // Eg. Two consecutive applications return the same number of subclasses
+        long prevSize = 0;
+        final long[] newSize = {-1};
+        while (prevSize != newSize[0]) {
+
+            prevSize = newSize[0];
+
+            newSize[0] = 0;
+
+            calculatedTypes.forEach((key, value) -> {
+                List<Resource> temp = new ArrayList<>();
+                value.forEach(superClass -> temp
+                    .addAll(resolveTypes(superClass)));
+
+                value.addAll(temp);
+                newSize[0] += value.size();
+            });
+
+
+        }
+    }
+
+    private void calculateSubPropertyOf(Collection<Statement> subPropertyOfStatemenets) {
+
+        subPropertyOfStatemenets.forEach(s -> {
+            Resource subClass = s.getSubject();
+            Resource superClass = (Resource) s.getObject();
+            if (!calculatedProperties.containsKey(subClass)) {
+                calculatedProperties.put(subClass, new HashSet<>());
+            }
+
+            if (!calculatedProperties.containsKey(superClass)) {
+                calculatedProperties.put(superClass, new HashSet<>());
+            }
+
+            calculatedProperties.get(subClass).add((Resource) s.getObject());
+
+        });
+
+        // Fixed point approach to finding all sub-properties.
+        // prevSize is the size of the previous application of the function
+        // newSize is the size of the current application of the function
+        // Fixed point is reached when they are the same.
+        long prevSize = 0;
+        final long[] newSize = {-1};
+        while (prevSize != newSize[0]) {
+
+            prevSize = newSize[0];
+
+            newSize[0] = 0;
+
+            calculatedProperties.forEach((key, value) -> {
+                List<Resource> temp = new ArrayList<>();
+                value.forEach(superProperty -> temp.addAll(resolveProperties(superProperty)));
+
+                value.addAll(temp);
+                newSize[0] += value.size();
+            });
+
+
+        }
+    }
+
+    private void calculateRangeDomain(Collection<Statement> rangeOrDomainStatements, Map<Resource, Set<Resource>> calculatedRangeOrDomain) {
+
+        rangeOrDomainStatements.forEach(s -> {
+            Resource predicate = s.getSubject();
+            if (!calculatedProperties.containsKey(predicate)) {
+                calculatedProperties.put(predicate, new HashSet<>());
+            }
+
+            if (!calculatedRangeOrDomain.containsKey(predicate)) {
+                calculatedRangeOrDomain.put(predicate, new HashSet<>());
+            }
+
+            calculatedRangeOrDomain.get(predicate).add((Resource) s.getObject());
+
+            if (!calculatedTypes.containsKey(s.getObject())) {
+                calculatedTypes.put((Resource) s.getObject(), new HashSet<>());
+            }
+
+        });
+
+
+        calculatedProperties
+            .keySet()
+            .stream()
+            .filter(key -> !calculatedRangeOrDomain.containsKey(key))
+            .forEach(key -> calculatedRangeOrDomain.put(key, new HashSet<>()));
+
+        // Fixed point approach to finding all ranges or domains.
+        // prevSize is the size of the previous application of the function
+        // newSize is the size of the current application of the function
+        // Fixed point is reached when they are the same.
+        long prevSize = 0;
+        final long[] newSize = {-1};
+        while (prevSize != newSize[0]) {
+
+            prevSize = newSize[0];
+
+            newSize[0] = 0;
+
+            calculatedRangeOrDomain.forEach((key, value) -> {
+                List<Resource> resolvedBySubProperty = new ArrayList<>();
+                resolveProperties(key).forEach(newPredicate -> {
+                    Set<Resource> iris = calculatedRangeOrDomain.get(newPredicate);
+                    if (iris != null) {
+                        resolvedBySubProperty.addAll(iris);
+                    }
+
+                });
+
+                List<Resource> resolvedBySubClass = new ArrayList<>();
+                value.addAll(resolvedBySubProperty);
+
+
+                value.stream().map(this::resolveTypes).forEach(resolvedBySubClass::addAll);
+
+                value.addAll(resolvedBySubClass);
+
+                newSize[0] += value.size();
+            });
+
+
+        }
+    }
 
 }
