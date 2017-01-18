f09eaf11c465edf7dbd3a7316f7a9bb505df7c7a
Jeen Broekstra <jeen.broekstra@gmail.com>
2017-01-18 17:03:52 +1100

    #729 code formatting and cleanup
    Signed-off-by: Jeen Broekstra <jeen.broekstra@gmail.com>

--- core/repository/sail/src/main/java/org/eclipse/rdf4j/repository/sail/helpers/SailUpdateExecutor.java
+++ core/repository/sail/src/main/java/org/eclipse/rdf4j/repository/sail/helpers/SailUpdateExecutor.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.net.URL;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
