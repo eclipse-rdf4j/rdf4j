823c2fe86e5d7c7823d7e401c496056676099b47
Mark Hale <mark.hale@physics.org>
2017-01-18 16:18:13 +0000

    issues/#181. Added test cases.
    Signed-off-by: Mark Hale <mark.hale@physics.org>

--- compliance/sparql/src/test/java/org/eclipse/rdf4j/sail/federation/FederationSparqlTest.java
+++ compliance/sparql/src/test/java/org/eclipse/rdf4j/sail/federation/FederationSparqlTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ * Copyright (c) 2016 Eclipse RDF4J contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
@@ -7,69 +7,46 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.sail.federation;
 
-import org.eclipse.rdf4j.query.Dataset;
-import org.eclipse.rdf4j.query.parser.sparql.manifest.SPARQL11ManifestTest;
-import org.eclipse.rdf4j.query.parser.sparql.manifest.SPARQLQueryTest;
-import org.eclipse.rdf4j.repository.Repository;
-import org.eclipse.rdf4j.repository.dataset.DatasetRepository;
+import static org.junit.Assert.assertFalse;
+
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.junit.Test;
 
-import junit.framework.Test;
+public class FederationSparqlTest {
 
-public class FederationSparqlTest extends SPARQLQueryTest {
-
-	public static Test suite()
+	@Test
+	public void quickTest()
 		throws Exception
 	{
-		return SPARQL11ManifestTest.suite(new Factory() {
+		SailRepository repo1 = new SailRepository(new MemoryStore());
+		SailRepository repo2 = new SailRepository(new MemoryStore());
+		Federation fed = new Federation();
+		fed.addMember(repo1);
+		fed.addMember(repo2);
+		SailRepository repoFed = new SailRepository(fed);
+		repoFed.initialize();
 
-			public SPARQLQueryTest createSPARQLQueryTest(String testURI, String name, String queryFileURL,
-					String resultFileURL, Dataset dataSet, boolean laxCardinality)
-			{
-				return new FederationSparqlTest(testURI, name, queryFileURL, resultFileURL, dataSet,
-						laxCardinality);
-			}
+		try (RepositoryConnection conn = repo1.getConnection()) {
+			conn.add(getClass().getResource("/testcases-sparql-1.0-w3c/data-r2/algebra/var-scope-join-1.ttl"),
+					null, null, conn.getValueFactory().createIRI("http://example/g1"));
+		}
+		try (RepositoryConnection conn = repo2.getConnection()) {
+			conn.add(getClass().getResource("/testcases-sparql-1.0-w3c/data-r2/algebra/var-scope-join-1.ttl"),
+					null, null, conn.getValueFactory().createIRI("http://example/g2"));
+		}
 
-			public SPARQLQueryTest createSPARQLQueryTest(String testURI, String name, String queryFileURL,
-					String resultFileURL, Dataset dataSet, boolean laxCardinality, boolean checkOrder)
-			{
-				String[] ignoredTests = {
-						// test case incompatible with RDF 1.1 - see
-						// http://lists.w3.org/Archives/Public/public-sparql-dev/2013AprJun/0006.html
-						"STRDT   TypeErrors",
-						// test case incompatible with RDF 1.1 - see
-						// http://lists.w3.org/Archives/Public/public-sparql-dev/2013AprJun/0006.html
-						"STRLANG   TypeErrors",
-						// known issue: SES-937
-						"sq03 - Subquery within graph pattern, graph variable is not bound",
-						};
-
-				return new FederationSparqlTest(testURI, name, queryFileURL, resultFileURL, dataSet,
-						laxCardinality, checkOrder, ignoredTests);
-			}
-			// skip 'service' tests for now since they require presence of remote sparql endpoints.
-		}, true, true, false, "service");
-	}
-
-	public FederationSparqlTest(String testURI, String name, String queryFileURL, String resultFileURL,
-			Dataset dataSet, boolean laxCardinality, String... ignoredTests)
-	{
-		super(testURI, name, queryFileURL, resultFileURL, dataSet, laxCardinality, ignoredTests);
-	}
-
-	public FederationSparqlTest(String testURI, String name, String queryFileURL, String resultFileURL,
-			Dataset dataSet, boolean laxCardinality, boolean checkOrder, String... ignoredTests)
-	{
-		super(testURI, name, queryFileURL, resultFileURL, dataSet, laxCardinality, checkOrder, ignoredTests);
-	}
-
-	@Override
-	protected Repository newRepository() {
-		Federation sail = new Federation();
-		sail.addMember(new SailRepository(new MemoryStore()));
-		sail.addMember(new SailRepository(new MemoryStore()));
-		sail.addMember(new SailRepository(new MemoryStore()));
-		return new DatasetRepository(new SailRepository(sail));
+		String query = "PREFIX : <http://example/> SELECT * { graph :g1 {?X  :name 'paul'} { graph :g2 {?Y :name 'george' . OPTIONAL { ?X :email ?Z } } } }";
+		boolean hasResults;
+		try (RepositoryConnection conn = repoFed.getConnection()) {
+			TupleQuery tq = conn.prepareTupleQuery(QueryLanguage.SPARQL, query);
+			TupleQueryResult tqr = tq.evaluate();
+			hasResults = tqr.hasNext();
+		}
+		assertFalse(hasResults);
 	}
 }
