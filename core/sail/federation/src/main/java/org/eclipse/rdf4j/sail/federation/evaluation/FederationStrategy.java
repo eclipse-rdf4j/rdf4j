12b08371bdc1c12ece5af2445448417cc83c6a3b
Mark Hale <mark.hale@physics.org>
2017-01-18 17:44:55 +0000

    Issues/#181. Fix for test.
    Signed-off-by: Mark Hale <mark.hale@physics.org>

--- core/sail/federation/src/main/java/org/eclipse/rdf4j/sail/federation/evaluation/FederationStrategy.java
+++ core/sail/federation/src/main/java/org/eclipse/rdf4j/sail/federation/evaluation/FederationStrategy.java
@@ -91,7 +91,9 @@ public class FederationStrategy extends StrictEvaluationStrategy {
 		for (int i = 1, n = join.getNumberOfArguments(); i < n; i++) {
 
 			TupleExpr rightArg = join.getArg(i);
-			if (TupleExprs.containsProjection(rightArg)) {
+			if (TupleExprs.containsProjection(rightArg)
+					|| (rightArg instanceof OwnedTupleExpr && ((OwnedTupleExpr)rightArg).getQuery() != null))
+			{
 				TupleExpr leftArg = join.getArg(i - 1);
 				collectedBindingNames.addAll(leftArg.getBindingNames());
 				result = new HashJoinIteration(this, result, collectedBindingNames,
