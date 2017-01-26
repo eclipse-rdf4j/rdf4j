50637c139a3b5fa307e3abd167dd01d99e5b7aae
Peter Ansell <p_ansell@yahoo.com>
2017-01-27 09:22:41 +1100

    issue #740 : Fix triplicate implementation of QueueCursor
    QueueCursor was copied verbatim into core/sail/federation, but none of its APIs were changed, so it can be simply deprecated and hollowed out
    
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- core/sail/federation/src/main/java/org/eclipse/rdf4j/sail/federation/evaluation/QueueCursor.java
+++ core/sail/federation/src/main/java/org/eclipse/rdf4j/sail/federation/evaluation/QueueCursor.java
@@ -7,33 +7,15 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.sail.federation.evaluation;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.eclipse.rdf4j.common.iteration.LookAheadIteration;
-import org.eclipse.rdf4j.query.QueryEvaluationException;
-
 /**
  * Makes working with a queue easier by adding the methods {@link #done()} and {@link #toss(Exception)} and
  * automatically converting the exception into a QueryEvaluationException with an appropriate stack trace.
  * 
  * @author James Leigh
+ * @deprecated Use {@link org.eclipse.rdf4j.http.client.QueueCursor} instead	
  */
-public class QueueCursor<E> extends LookAheadIteration<E, QueryEvaluationException> {
-
-	private final AtomicBoolean done = new AtomicBoolean(false);
-
-	private final BlockingQueue<E> queue;
-
-	private final E afterLast = createAfterLast();
-
-	private final Queue<Throwable> exceptions = new LinkedList<Throwable>();
+@Deprecated
+public class QueueCursor<E> extends org.eclipse.rdf4j.http.client.QueueCursor<E> {
 
 	/**
 	 * Creates an <tt>QueueCursor</tt> with the given (fixed) capacity and default access policy.
@@ -55,134 +37,7 @@ public class QueueCursor<E> extends LookAheadIteration<E, QueryEvaluationExcepti
 	 *        in FIFO order; if <tt>false</tt> the access order is unspecified.
 	 */
 	public QueueCursor(int capacity, boolean fair) {
-		super();
-		this.queue = new ArrayBlockingQueue<E>(capacity, fair);
-	}
-
-	/**
-	 * The next time {@link #next()} is called this exception will be thrown. If it is not a
-	 * QueryEvaluationException or RuntimeException it will be wrapped in a QueryEvaluationException.
-	 */
-	public void toss(Exception exception) {
-		synchronized (exceptions) {
-			exceptions.add(exception);
-		}
-	}
-
-	/**
-	 * Adds another item to the queue, blocking while the queue is full.
-	 */
-	public void put(E item)
-		throws InterruptedException
-	{
-		if (!done.get()) {
-			queue.put(item);
-		}
-	}
-
-	/**
-	 * Indicates the method {@link #put(Object)} will not be called in the queue anymore.
-	 */
-	public void done() {
-		// Lazily set here, and then come back in handleClose and use set if necessary
-		done.lazySet(true);
-		try {
-			queue.add(afterLast);
-		}
-		catch (IllegalStateException e) { // NOPMD
-			// no thread is waiting on this queue anyway
-		}
-	}
-
-	/**
-	 * Returns the next item in the queue or throws an exception.
-	 */
-	@Override
-	public E getNextElement()
-		throws QueryEvaluationException
-	{
-		try {
-			checkException();
-			E take;
-			if (done.get()) {
-				take = queue.poll();
-			}
-			else {
-				take = queue.take();
-				if (done.get()) {
-					done(); // in case the queue was full before
-				}
-			}
-			if (isAfterLast(take)) {
-				checkException();
-				done(); // put afterLast back for others
-				take = null; // NOPMD
-			}
-			return take;
-		}
-		catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			checkException();
-			throw new QueryEvaluationException(e);
-		}
-	}
-
-	@Override
-	public void handleClose()
-		throws QueryEvaluationException
-	{
-		try {
-			super.handleClose();
-		}
-		finally {
-			done.set(true);
-			do {
-				queue.clear(); // ensure extra room is available
-			}
-			while (!queue.offer(afterLast));
-			checkException();
-		}
-	}
-
-	public void checkException()
-		throws QueryEvaluationException
-	{
-		synchronized (exceptions) {
-			if (!exceptions.isEmpty()) {
-				try {
-					throw exceptions.remove();
-				}
-				catch (QueryEvaluationException e) {
-					modifyStackTraceAndRethrow(e, 1);
-				}
-				catch (RuntimeException e) {
-					modifyStackTraceAndRethrow(e, 0);
-				}
-				catch (Throwable e) { // NOPMD
-					throw new QueryEvaluationException(e);
-				}
-			}
-		}
-	}
-
-	private <X extends Exception> void modifyStackTraceAndRethrow(X exception, int firstIndex)
-		throws X
-	{
-		List<StackTraceElement> stack = new ArrayList<StackTraceElement>();
-		stack.addAll(Arrays.asList(exception.getStackTrace()));
-		StackTraceElement[] thisStack = new Throwable().getStackTrace(); // NOPMD
-		stack.addAll(Arrays.asList(thisStack).subList(firstIndex, thisStack.length));
-		exception.setStackTrace(stack.toArray(new StackTraceElement[stack.size()]));
-		throw exception;
-	}
-
-	private boolean isAfterLast(E take) {
-		return take == null || take == afterLast; // NOPMD
-	}
-
-	@SuppressWarnings("unchecked")
-	private E createAfterLast() {
-		return (E)new Object();
+		super(capacity, fair);
 	}
 
 }
