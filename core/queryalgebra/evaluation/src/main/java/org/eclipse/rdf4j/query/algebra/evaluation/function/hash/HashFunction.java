4a413e72891a8ec7a5f262601fe02791544442ef
Bart Hanssens <bart.hanssens@fedict.be>
2017-03-20 09:59:05 +0100

    Removed accidental space in copyright
    Signed-off-by: Bart Hanssens <bart.hanssens@fedict.be>

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/hash/HashFunction.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/hash/HashFunction.java
@@ -1,4 +1,4 @@
-/** *****************************************************************************
+/*******************************************************************************
  * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
