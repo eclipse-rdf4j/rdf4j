4481b32772cbe6b7e8a719d4f0e191a4c3816557
Bart Hanssens <bart.hanssens@fedict.be>
2017-03-20 09:50:18 +0100

    Back to original algo + added test
    Signed-off-by: Bart Hanssens <bart.hanssens@fedict.be>

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/hash/HashFunction.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/hash/HashFunction.java
@@ -1,12 +1,13 @@
-/*******************************************************************************
+/** *****************************************************************************
  * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
- *******************************************************************************/
+ ****************************************************************************** */
 package org.eclipse.rdf4j.query.algebra.evaluation.function.hash;
 
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -18,35 +19,28 @@ import org.eclipse.rdf4j.query.algebra.evaluation.function.Function;
 
 /**
  * Abstract hash function
- * 
+ *
  * @author jeen
  */
 public abstract class HashFunction implements Function {
-private final String HEX = "0123456789abcdef";
-	
+
 	/**
 	 * Calculate hash value, represented as hexadecimal string.
-	 * 
+	 *
 	 * @param text text
 	 * @param algorithm name of the hash algorithm
-	 * @return hexadecimal string
-	 * @throws NoSuchAlgorithmException 
+	 * @return hexadecimal string (padded with leading zeros if needed)
+	 * @throws NoSuchAlgorithmException
 	 */
 	protected String hash(String text, String algorithm)
-		throws NoSuchAlgorithmException
-	{
+			throws NoSuchAlgorithmException {
 		byte[] hash = MessageDigest.getInstance(algorithm).digest(text.getBytes());
+		BigInteger bi = new BigInteger(1, hash);
 
-		// convert to hexadecimal representation, with leading zeros
-		char[] hex = new char[hash.length * 2];
-		for (int i = 0, j = 0; i < hex.length; ) {
-			hex[i++] = HEX.charAt((hash[j] & 0xF0) >>> 4);
-			hex[i++] = HEX.charAt(hash[j++] & 0x0F);
-		}
-		return new String(hex);
+		return String.format("%0" + hash.length * 2 + "x", bi);
 	}
 
 	@Override
 	public abstract Literal evaluate(ValueFactory valueFactory, Value... args)
-		throws ValueExprEvaluationException;
+			throws ValueExprEvaluationException;
 }
