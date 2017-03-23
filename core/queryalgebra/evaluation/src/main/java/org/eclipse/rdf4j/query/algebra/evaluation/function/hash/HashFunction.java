ff8c407a131a988a887b120c8d549697fcfd6c52
Jeen Broekstra <jeenbroekstra@users.noreply.github.com>
2017-03-23 15:02:51 +1100

    Merge pull request #777 from Fedict/issues/#775-hash-zeros
    Convert bytes to hex string with leading zeros

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/hash/HashFunction.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/hash/HashFunction.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
- *******************************************************************************/
+ ****************************************************************************** */
 package org.eclipse.rdf4j.query.algebra.evaluation.function.hash;
 
 import java.math.BigInteger;
@@ -19,24 +19,28 @@ import org.eclipse.rdf4j.query.algebra.evaluation.function.Function;
 
 /**
  * Abstract hash function
- * 
+ *
  * @author jeen
  */
 public abstract class HashFunction implements Function {
 
+	/**
+	 * Calculate hash value, represented as hexadecimal string.
+	 *
+	 * @param text text
+	 * @param algorithm name of the hash algorithm
+	 * @return hexadecimal string (padded with leading zeros if needed)
+	 * @throws NoSuchAlgorithmException
+	 */
 	protected String hash(String text, String algorithm)
-		throws NoSuchAlgorithmException
-	{
+			throws NoSuchAlgorithmException {
 		byte[] hash = MessageDigest.getInstance(algorithm).digest(text.getBytes());
 		BigInteger bi = new BigInteger(1, hash);
-		String result = bi.toString(16);
-		if (result.length() % 2 != 0) {
-			return "0" + result;
-		}
-		return result;
+
+		return String.format("%0" + hash.length * 2 + "x", bi);
 	}
 
+	@Override
 	public abstract Literal evaluate(ValueFactory valueFactory, Value... args)
-		throws ValueExprEvaluationException;
-
+			throws ValueExprEvaluationException;
 }
