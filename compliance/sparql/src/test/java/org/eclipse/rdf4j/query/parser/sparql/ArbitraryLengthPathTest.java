af88968b866ea6d95bd25a841bdb5d01c620f12a
James Leigh <james.leigh@ontotext.com>
2017-03-27 17:52:17 -0400

    issue #781: Use hashCode of PathIteration to identify intermediate variables
    Signed-off-by: James Leigh <james.leigh@ontotext.com>

--- compliance/sparql/src/test/java/org/eclipse/rdf4j/query/parser/sparql/ArbitraryLengthPathTest.java
+++ compliance/sparql/src/test/java/org/eclipse/rdf4j/query/parser/sparql/ArbitraryLengthPathTest.java
@@ -7,9 +7,9 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.query.parser.sparql;
 
-import junit.framework.TestCase;
-
 import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
 import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
@@ -19,6 +19,8 @@ import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import junit.framework.TestCase;
 
 /**
  * @author james
@@ -88,6 +90,35 @@ public class ArbitraryLengthPathTest extends TestCase {
 	{
 		populate(100000);
 		String sparql = "ASK { <urn:test:root> <urn:test:hasChild>* <urn:test:node-end> }";
+		assertTrue(con.prepareBooleanQuery(QueryLanguage.SPARQL, sparql).evaluate());
+	}
+
+	@Test
+	public void testDirection()
+		throws Exception
+	{
+		ValueFactory vf = con.getValueFactory();
+		con.add(vf.createIRI("urn:test:a"), vf.createIRI("urn:test:rel"), vf.createIRI("urn:test:b"));
+		con.add(vf.createIRI("urn:test:b"), vf.createIRI("urn:test:rel"), vf.createIRI("urn:test:a"));
+		String sparql = "ASK { <urn:test:a> <urn:test:rel>* <urn:test:b> . <urn:test:b> <urn:test:rel>* <urn:test:a> }";
+		assertTrue(con.prepareBooleanQuery(QueryLanguage.SPARQL, sparql).evaluate());
+	}
+
+	@Test
+	public void testSimilarPatterns()
+		throws Exception
+	{
+		ValueFactory vf = con.getValueFactory();
+		con.add(vf.createIRI("urn:test:a"), RDF.TYPE, vf.createIRI("urn:test:c"));
+		con.add(vf.createIRI("urn:test:b"), RDF.TYPE, vf.createIRI("urn:test:d"));
+		con.add(vf.createIRI("urn:test:c"), RDFS.SUBCLASSOF, vf.createIRI("urn:test:e"));
+		con.add(vf.createIRI("urn:test:d"), RDFS.SUBCLASSOF, vf.createIRI("urn:test:f"));
+		String sparql = "ASK { \n"
+				+ "   values (?expectedTargetClass55555 ?expectedTargetClass5544T) {(<urn:test:e> <urn:test:f>)}.\n"
+				+ "   <urn:test:a> a ?linkTargetClass55555 .\n"
+				+ "   ?linkTargetClass55555 rdfs:subClassOf* ?expectedTargetClass55555 .\n"
+				+ "   <urn:test:b> a ?linkTargetClass55556 .\n"
+				+ "   ?linkTargetClass55556 rdfs:subClassOf* ?expectedTargetClass5544T . }";
 		assertTrue(con.prepareBooleanQuery(QueryLanguage.SPARQL, sparql).evaluate());
 	}
 
