00cca9a273de81d8c650a2b288e66ff6a1f0d1cc
Heshan Jayasinghe <shanujse@gmail.com>
2017-04-16 03:01:00 +0530

    Add basic Shape classes and SHACL Sail classes
    Signed-off-by: Heshan Jayasinghe <heshanjse>
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

---
+++ core/shacl/src/main/java/org.eclipse.rdf4j/shape/Shape.java
@@ -0,0 +1,13 @@
+package org.eclipse.rdf4j.common.io;
+
+/**
+ * Created by heshanjayasinghe on 4/16/17.
+ */
+public class Shape {
+    Resource targetClass;
+    PropertyShape property;
+
+    public Shape(){
+
+    }
+}
---
+++ core/shacl/src/main/java/org.eclipse.rdf4j/validation/SHACLConnection.java
@@ -0,0 +1,8 @@
+package org.eclipse.rdf4j.common.io;
+
+/**
+ * Created by heshanjayasinghe on 4/16/17.
+ */
+public class SHACLConnection {
+
+}
