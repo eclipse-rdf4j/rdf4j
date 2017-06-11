7fd2370f2378199b6c3edee7aa09944ddc6f0a59
Heshan Jayasinghe <shanujse@gmail.com>
2017-06-11 14:50:21 +0530

    path implementation
    Signed-off-by: Heshan Jayasinghe <heshanjse>
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/shacl/src/main/java/org/eclipse/rdf4j/AST/PathPropertyShape.java
+++ core/shacl/src/main/java/org/eclipse/rdf4j/AST/PathPropertyShape.java
@@ -1,7 +1,16 @@
 package org.eclipse.rdf4j.AST;
 
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+
 /**
  * Created by heshanjayasinghe on 6/11/17.
  */
-public class PathPropertyShape {
+public class PathPropertyShape extends PropertyShape {
+    Path path;
+
+    public PathPropertyShape(Resource next, SailRepositoryConnection connection) {
+        super(next, connection);
+    }
 }
+
