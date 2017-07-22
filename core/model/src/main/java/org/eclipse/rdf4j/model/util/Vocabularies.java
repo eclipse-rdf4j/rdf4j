d844d70fd1bc8f018239c4234015ff20faca77bd
Bart Hanssens <bart.hanssens@fedict.be>
2017-07-22 14:43:45 +0200

    Use LinkedHashSet instead of HashSet, and rename class to Vocabularies Signed-off-by:Bart Hanssens <bart.hanssens@fedict.be>

--- core/model/src/main/java/org/eclipse/rdf4j/model/util/Vocabularies.java
+++ core/model/src/main/java/org/eclipse/rdf4j/model/util/Vocabularies.java
@@ -8,7 +8,7 @@
 package org.eclipse.rdf4j.model.util;
 
 import java.lang.reflect.Field;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eclipse.rdf4j.model.IRI;
@@ -18,7 +18,7 @@ import org.eclipse.rdf4j.model.IRI;
  * 
  * @author Bart Hanssens
  */
-public class VocabularyUtil {
+public class Vocabularies {
 	/**
 	 * Get all the {@link IRI IRIs} of the classes and properties of a vocabulary.
 	 *
@@ -26,7 +26,7 @@ public class VocabularyUtil {
 	 * @return set of IRIs
 	 */
 	public static Set<IRI> getAllIRIs(Class vocabulary) {
-		Set<IRI> iris = new HashSet<>();
+		Set<IRI> iris = new LinkedHashSet<>();
 
 		for (Field f : vocabulary.getFields()) {
 			if (f.getType().equals(IRI.class)) {
