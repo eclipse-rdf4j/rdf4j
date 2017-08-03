16b258bb6f5df797f7c189a7b56b6a1faa41eae8
Heshan Jayasinghe <shanujse@gmail.com>
2017-08-03 07:54:30 +0530

    min count implementation 3rd aug 2017
    Signed-off-by: Heshan Jayasinghe <shanujse@gmail.com>

--- core/shacl/src/main/java/org/eclipse/rdf4j/plan/OuterLeftJoin.java
+++ core/shacl/src/main/java/org/eclipse/rdf4j/plan/OuterLeftJoin.java
@@ -3,6 +3,8 @@ package org.eclipse.rdf4j.plan;
 import org.eclipse.rdf4j.common.iteration.CloseableIteration;
 import org.eclipse.rdf4j.sail.SailException;
 
+import java.util.ArrayList;
+
 /**
  * Created by heshanjayasinghe on 7/15/17.
  */
@@ -18,25 +20,43 @@ public class OuterLeftJoin implements PlanNode {
 
     @Override
     public CloseableIteration<Tuple, SailException> iterator() {
-//        return new Iterator<Tuple>() {
+        ArrayList<Tuple> list = new ArrayList<Tuple>();
+        while (targetclass.iterator().hasNext()){
+            Tuple targetclasstuple = targetclass.iterator().next();
+            Tuple properytuple = properties.iterator().next();
+             //Tuple propertyTuple = ((Select) properties).shaclSailConnection.sail.
+
+        }
+       // CloseableIteration<String[], SailException> leftJointuple = (CloseableIteration<String[], SailException>) list.listIterator();
+//        return new CloseableIteration<Tuple,SailException>() {
+//
+//            @Override
+//            public void close() throws SailException {
+//                leftJointuple.close();
+//            }
 //
 //            int counter = 0;
 //
 //            @Override
 //            public boolean hasNext() {
-//
-//                return Iterators.size((Iterator<?>) properties) > counter;
-////                return dataSource.strings.length>counter;
+//                return leftJointuple.hasNext();
 //            }
 //
 //            @Override
 //            public Tuple next() {
-//            //   Tuple property =  properties.iterator().next();
+//
+//                Statement next = leftJointuple.next();
+//                Tuple tuple = new Tuple();
+//                tuple.line.add(next.getSubject());
 //
 //                counter++;
-//                Tuple tuple = new Tuple();
-//            //    tuple.line.add(property);
+//
 //                return tuple;
+//            }
+//
+//            @Override
+//            public void remove() throws SailException {
+//
 //            }
 //        };
         return null;
