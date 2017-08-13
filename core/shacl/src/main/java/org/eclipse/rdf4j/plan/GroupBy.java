d51e8a2a1ded57a2c2b0b661635b4f405d341d69
Heshan Jayasinghe <shanujse@gmail.com>
2017-08-14 02:21:11 +0530

    group by implementation
    Signed-off-by: Heshan Jayasinghe <heshanjse>
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/shacl/src/main/java/org/eclipse/rdf4j/plan/GroupBy.java
+++ core/shacl/src/main/java/org/eclipse/rdf4j/plan/GroupBy.java
@@ -1,7 +1,10 @@
 package org.eclipse.rdf4j.plan;
 
 import org.eclipse.rdf4j.common.iteration.CloseableIteration;
+import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.sail.SailException;
+
+import java.util.*;
 
 /**
  * Created by heshanjayasinghe on 7/17/17.
@@ -11,51 +14,62 @@ public class GroupBy implements PlanNode{
 
     public GroupBy(PlanNode outerLeftJoin){
         leftjoinnode = outerLeftJoin;
-        //Iterator<Tuple> aboveIterator = (Iterator<Tuple>) leftjoinnode.iterator();
+
+
     }
 
     @Override
     public CloseableIteration<Tuple, SailException> iterator() {
-//        return new Iterator<Tuple>() {
-//
-//            Iterator<Tuple> aboveIterator = above.iterator();
-//
-//            Tuple next = null;
-//
-//            @Override
-//            public boolean hasNext() {
-//
-//                if(next == null){
-//                    getNext();
-//                }
-//
-//                return next != null;
-//            }
-//
-//            private void getNext() {
-//                while(aboveIterator.hasNext()){
-//                    Tuple tempNExt = aboveIterator.next();
-//                    if(condition.condition(tempNExt)){
-//                        next = tempNExt;
-//                        return;
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public Tuple next() {
-//                if(next == null){
-//                    getNext();
-//                }
-//
-//                Tuple tempNext = next;
-//
-//                next = null;
-//
-//                return tempNext;
-//            }
-//        };
-        return null;
+
+        HashMap<Value,List<Value>> hashMap = new LinkedHashMap<Value,List<Value>>();
+
+        while (leftjoinnode.iterator().hasNext()){
+            Tuple leftjointuple = leftjoinnode.iterator().next();
+            boolean status = true;
+
+            for( Map.Entry<Value, List<Value>> entry : hashMap.entrySet() )
+            {
+                Value key = entry.getKey();
+                List<Value> values = entry.getValue();
+                if(key.stringValue().equals(leftjointuple.line.get(0))){
+                    values.add(leftjointuple.line.get(2));
+                    hashMap.put(leftjointuple.line.get(0), values);
+                    status = false;
+                }
+            }
+            if(status){
+                List<Value> element= new ArrayList<Value>();
+                element.add(leftjointuple.line.get(2));
+                hashMap.put(leftjointuple.line.get(0), element);
+            }
+
+        }
+
+        return new CloseableIteration<Tuple, SailException>()  {
+            Iterator<Map.Entry<Value, List<Value>>> hashmapiterator = hashMap.entrySet().iterator();
+
+            int counter = 0;
+            @Override
+            public void close() throws SailException {
+
+            }
+
+            @Override
+            public boolean hasNext() throws SailException {
+                return hashmapiterator.hasNext();
+            }
+
+            @Override
+            public Tuple next() throws SailException {
+                return (Tuple) hashmapiterator.next();
+            }
+
+            @Override
+            public void remove() throws SailException {
+
+            }
+        };
+
     }
 
     @Override
