b46ce1df1418fc46bfc868097f1f1451b650f619
Heshan Jayasinghe <shanujse@gmail.com>
2017-08-26 01:32:16 +0530

    Add Eclipse copyright license header
    Signed-off-by: Heshan Jayasinghe <heshanjse>
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/shacl/src/main/java/org/eclipse/rdf4j/AST/PlanGenerator.java
+++ core/shacl/src/main/java/org/eclipse/rdf4j/AST/PlanGenerator.java
@@ -1,5 +1,12 @@
-package org.eclipse.rdf4j.AST;
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
 
+package org.eclipse.rdf4j.AST;
 
 import org.eclipse.rdf4j.plan.PlanNode;
 import org.eclipse.rdf4j.validation.ShaclSailConnection;
--- testsuites/store/src/main/java/org/eclipse/rdf4j/shacl/mock/MockConsumeGroupPlanNode.java
+++ testsuites/store/src/main/java/org/eclipse/rdf4j/shacl/mock/MockConsumeGroupPlanNode.java
@@ -1,8 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+
 package org.eclipse.rdf4j.shacl.mock;
 
 import org.eclipse.rdf4j.common.iteration.CloseableIteration;
 import org.eclipse.rdf4j.plan.GroupPlanNode;
-import org.eclipse.rdf4j.plan.PlanNode;
 import org.eclipse.rdf4j.plan.Tuple;
 import org.eclipse.rdf4j.sail.SailException;
 
