4c2b7e755e8375486248647e3977c0a30f8ddf45
James Leigh <james.leigh@ontotext.com>
2017-09-12 15:05:16 -0400

    Fix eclipse/rdf4j#915: Remove sail related modules
    Signed-off-by: James Leigh <james.leigh@ontotext.com>

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/xsd/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Functions for casting values to various XML Schema datatypes
- * 
- * @see org.eclipse.rdf4j.model.vocabulary.XMLSchema
- */
-package org.eclipse.rdf4j.query.algebra.evaluation.function.xsd;
