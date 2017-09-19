c33dd55c4ffdc8dfda725b85584a2e87f3cb77ef
James Leigh <james.leigh@ontotext.com>
2017-09-19 14:16:46 -0400

    Fix #2: Restore compliance tests for server
    Signed-off-by: James Leigh <james.leigh@ontotext.com>

---
+++ compliance/sparql/src/test/java/org/eclipse/rdf4j/query/parser/sparql/SPARQLEmbeddedServer.java
@@ -0,0 +1,129 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.query.parser.sparql;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.BlockingChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.rdf4j.http.protocol.Protocol;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.config.RepositoryConfig;
+import org.eclipse.rdf4j.repository.config.RepositoryConfigException;
+import org.eclipse.rdf4j.repository.config.RepositoryConfigUtil;
+import org.eclipse.rdf4j.repository.http.HTTPRepository;
+import org.eclipse.rdf4j.repository.manager.SystemRepository;
+import org.eclipse.rdf4j.repository.sail.config.SailRepositoryConfig;
+import org.eclipse.rdf4j.sail.memory.config.MemoryStoreConfig;
+
+/**
+ * An embedded http server for SPARQL query testing. Initializes a memory store repository for each specified
+ * reposiotoryId.
+ * 
+ * @author Andreas Schwarte
+ */
+public class SPARQLEmbeddedServer {
+
+	private static final String HOST = "localhost";
+
+	private static final int PORT = 18080;
+
+	private static final String OPENRDF_CONTEXT = "/openrdf";
+
+	private final List<String> repositoryIds;
+
+	private final Server jetty;
+
+	/**
+	 * @param repositoryIds
+	 */
+	public SPARQLEmbeddedServer(List<String> repositoryIds) {
+		this.repositoryIds = repositoryIds;
+		System.clearProperty("DEBUG");
+
+		jetty = new Server();
+
+		Connector conn = new BlockingChannelConnector();
+		conn.setHost(HOST);
+		conn.setPort(PORT);
+		jetty.addConnector(conn);
+
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath(OPENRDF_CONTEXT);
+		// warPath configured in pom.xml maven-war-plugin configuration
+		webapp.setWar("./target/openrdf-sesame");
+		jetty.setHandler(webapp);
+	}
+
+	/**
+	 * @return the url to the repository with given id
+	 */
+	public String getRepositoryUrl(String repoId) {
+		return Protocol.getRepositoryLocation(getServerUrl(), repoId);
+	}
+
+	/**
+	 * @return the server url
+	 */
+	public String getServerUrl() {
+		return "http://" + HOST + ":" + PORT + OPENRDF_CONTEXT;
+	}
+
+	public void start()
+		throws Exception
+	{
+		File dataDir = new File(System.getProperty("user.dir") + "/target/datadir");
+		dataDir.mkdirs();
+		System.setProperty("info.aduna.platform.appdata.basedir", dataDir.getAbsolutePath());
+
+		jetty.start();
+
+		createTestRepositories();
+	}
+
+	public void stop()
+		throws Exception
+	{
+		Repository systemRepo = new HTTPRepository(
+				Protocol.getRepositoryLocation(getServerUrl(), SystemRepository.ID));
+		RepositoryConnection con = systemRepo.getConnection();
+		try {
+			con.clear();
+		}
+		finally {
+			con.close();
+			systemRepo.shutDown();
+		}
+
+		jetty.stop();
+		System.clearProperty("org.mortbay.log.class");
+	}
+
+	private void createTestRepositories()
+		throws RepositoryException, RepositoryConfigException
+	{
+		Repository systemRep = new HTTPRepository(
+				Protocol.getRepositoryLocation(getServerUrl(), SystemRepository.ID));
+
+		// create a memory store for each provided repository id
+		for (String repId : repositoryIds) {
+			MemoryStoreConfig memStoreConfig = new MemoryStoreConfig();
+			memStoreConfig.setPersist(false);
+			SailRepositoryConfig sailRepConfig = new SailRepositoryConfig(memStoreConfig);
+			RepositoryConfig repConfig = new RepositoryConfig(repId, sailRepConfig);
+
+			RepositoryConfigUtil.updateRepositoryConfigs(systemRep, repConfig);
+		}
+
+	}
+}
