58c55b45ec3f564cb22f36fd2e96167ef1680f17
Thomas Pellissier Tanon <thomas@pellissier-tanon.fr>
2017-10-13 10:48:25 +0200

    issue #184: Adds some mathematical operators related to xsd:duration to SPARQL extended mode
    The implementation is based on javax.xml.datatype
    
    Signed-off-by: Thomas Pellissier Tanon <thomas@pellissier-tanon.fr>

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/ExtendedEvaluationStrategy.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/ExtendedEvaluationStrategy.java
@@ -7,20 +7,23 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.query.algebra.evaluation.impl;
 
+import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.impl.BooleanLiteral;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.Dataset;
 import org.eclipse.rdf4j.query.QueryEvaluationException;
 import org.eclipse.rdf4j.query.algebra.Compare;
+import org.eclipse.rdf4j.query.algebra.MathExpr;
 import org.eclipse.rdf4j.query.algebra.evaluation.TripleSource;
 import org.eclipse.rdf4j.query.algebra.evaluation.ValueExprEvaluationException;
 import org.eclipse.rdf4j.query.algebra.evaluation.federation.FederatedServiceResolver;
 import org.eclipse.rdf4j.query.algebra.evaluation.util.QueryEvaluationUtil;
+import org.eclipse.rdf4j.query.algebra.evaluation.util.XMLDatatypeMathUtil;
 
 /**
  * SPARQL 1.1 extended query evaluation strategy. This strategy adds the use of virtual properties, as well as
- * extended comparison operators to the minimally-conforming {@link StrictEvaluationStrategy}.
+ * extended comparison and mathematical operators to the minimally-conforming {@link StrictEvaluationStrategy}.
  * 
  * @author Jeen Broekstra
  */
@@ -42,6 +45,20 @@ public class ExtendedEvaluationStrategy extends TupleFunctionEvaluationStrategy 
 		// return result of non-strict comparison.
 		return BooleanLiteral.valueOf(
 				QueryEvaluationUtil.compare(leftVal, rightVal, node.getOperator(), false));
+	}
+
+	@Override
+	public Value evaluate(MathExpr node, BindingSet bindings)
+			throws ValueExprEvaluationException, QueryEvaluationException
+	{
+		Value leftVal = evaluate(node.getLeftArg(), bindings);
+		Value rightVal = evaluate(node.getRightArg(), bindings);
+
+		if (leftVal instanceof Literal && rightVal instanceof Literal) {
+			return XMLDatatypeMathUtil.compute((Literal)leftVal, (Literal)rightVal, node.getOperator());
+		}
+
+		throw new ValueExprEvaluationException("Both arguments must be literals");
 	}
 
 }
