fc87e7e182303a5b4e8f411d9efa428f05850224
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2018-07-02 21:05:25 +0000

    Corrected package info
    Signed-off-by: Bart Hanssens <bart.hanssens@bosa.fgov.be>

--- console/src/test/java/org/eclipse/rdf4j/console/command/VerifyTest.java
+++ console/src/test/java/org/eclipse/rdf4j/console/command/VerifyTest.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *******************************************************************************/
-package org.eclipse.rdf4j.console;
+package org.eclipse.rdf4j.console.command;
 
 import static org.junit.Assert.assertTrue;
 
@@ -23,6 +23,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+
+import org.eclipse.rdf4j.console.ConsoleIO;
+import org.eclipse.rdf4j.console.ConsoleState;
 
 import static org.mockito.Mockito.mock;
 
