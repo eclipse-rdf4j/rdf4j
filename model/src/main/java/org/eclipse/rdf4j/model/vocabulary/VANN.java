9b8001f536f340b81fc40b88d06fe0d330127504
Pat McBennett <pat.mcbennett@gmail.com>
2018-08-24 09:40:52 +0100

    #1074 Fixed copyright notice, added underscores to constant names.
    Signed-off-by: Pat McBennett <pat.mcbennett@gmail.com>

--- model/src/main/java/org/eclipse/rdf4j/model/vocabulary/VANN.java
+++ model/src/main/java/org/eclipse/rdf4j/model/vocabulary/VANN.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ * Copyright (c) 2018 Eclipse RDF4J contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
@@ -14,8 +14,8 @@ import org.eclipse.rdf4j.model.impl.SimpleNamespace;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 
 /**
- * Constants for <a href="http://purl.org/vocab/vann/">a vocabulary for annotating vocabulary descriptions</a>
- * (VANN).
+ * Constants for <a href="http://purl.org/vocab/vann/">a vocabulary for annotating vocabulary
+ * descriptions</a> (VANN).
  *
  * @see <a href="http://purl.org/vocab/vann/">A vocabulary for annotating vocabulary descriptions (VANN)</a>
  */
@@ -32,31 +32,32 @@ public class VANN {
 	 */
 	public static final Namespace NS = new SimpleNamespace(PREFIX, NAMESPACE);
 
-	/** A reference to a resource that describes changes between this version of a vocabulary and the previous. */
+	/** A reference to a resource that describes changes between this version of a vocabulary and the
+	 * previous. */
 	public final static IRI CHANGES;
 
 	/** A reference to a resource that provides an example of how this resource can be used. */
 	public final static IRI EXAMPLE;
 
-	/** The preferred namespace prefix to use when using terms from this vocabulary in an XML document. */
-	public final static IRI PREFERREDNAMESPACEPREFIX;
+	/** The preferred namespace prefix to ue when using terms from this vocabulary in an XML document. */
+	public final static IRI PREFERRED_NAMESPACE_PREFIX;
 
 	/** The preferred namespace URI to use when using terms from this vocabulary in an XML document. */
-	public final static IRI PREFERREDNAMESPACEURI;
+	public final static IRI PREFERRED_NAMESPACE_URI;
 
 	/** A group of related terms in a vocabulary. */
-	public final static IRI TERMGROUP;
+	public final static IRI TERM_GROUP;
 
 	/** A reference to a resource that provides information on how this resource is to be used. */
-	public final static IRI USAGENOTE;
+	public final static IRI USAGE_NOTE;
 
 	static {
 		ValueFactory factory = SimpleValueFactory.getInstance();
 		CHANGES = factory.createIRI(VANN.NAMESPACE, "changes");
 		EXAMPLE = factory.createIRI(VANN.NAMESPACE, "example");
-		PREFERREDNAMESPACEPREFIX = factory.createIRI(VANN.NAMESPACE, "preferredNamespacePrefix");
-		PREFERREDNAMESPACEURI = factory.createIRI(VANN.NAMESPACE, "preferredNamespaceUri");
-		TERMGROUP = factory.createIRI(VANN.NAMESPACE, "termGroup");
-		USAGENOTE = factory.createIRI(VANN.NAMESPACE, "usageNote");
+		PREFERRED_NAMESPACE_PREFIX = factory.createIRI(VANN.NAMESPACE, "preferredNamespacePrefix");
+		PREFERRED_NAMESPACE_URI = factory.createIRI(VANN.NAMESPACE, "preferredNamespaceUri");
+		TERM_GROUP = factory.createIRI(VANN.NAMESPACE, "termGroup");
+		USAGE_NOTE = factory.createIRI(VANN.NAMESPACE, "usageNote");
 	}
 }
