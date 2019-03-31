8576e6b7baa6b329733d27964b4601def74054d0
Håvard Ottestad <hmottestad@gmail.com>
2019-03-31 09:44:49 +0200

    https://github.com/eclipse/rdf4j/issues/1374 log unsupported shacl features with test
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

---
+++ shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/UnknownShapesTest.java
@@ -0,0 +1,73 @@
+package org.eclipse.rdf4j.sail.shacl;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Context;
+import ch.qos.logback.core.LogbackException;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+import ch.qos.logback.core.status.Status;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnknownShapesTest {
+
+	@Test
+	public void testPropertyShapes() throws IOException {
+		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) LoggerFactory
+				.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+
+		MyAppender newAppender = new MyAppender();
+		root.addAppender(newAppender);
+
+		SailRepository shaclRepository = Utils.getInitializedShaclRepository("unknownProperties.ttl", false);
+
+		try (SailRepositoryConnection connection = shaclRepository.getConnection()) {
+			connection.begin();
+			connection.add(RDF.TYPE, RDF.TYPE, RDFS.RESOURCE);
+			connection.commit();
+		}
+
+		Set<String> relevantLog = newAppender.logged.stream()
+				.filter(m -> m.startsWith("Unsupported SHACL feature"))
+				.collect(Collectors.toSet());
+
+		Set<String> expected = new HashSet<>(Arrays.asList(
+				"Unsupported SHACL feature detected sh:unknownTarget in statement (http://example.com/ns#PersonShape, http://www.w3.org/ns/shacl#unknownTarget, http://www.w3.org/2000/01/rdf-schema#Class) [null]",
+				"Unsupported SHACL feature detected sh:unknownShaclProperty in statement (http://example.com/ns#PersonPropertyShape, http://www.w3.org/ns/shacl#unknownShaclProperty, \"1\"^^<http://www.w3.org/2001/XMLSchema#integer>) [null]"));
+
+		assertEquals(expected, relevantLog);
+
+	}
+
+	class MyAppender extends AppenderBase<ILoggingEvent> {
+
+		List<String> logged = new ArrayList<>();
+
+		@Override
+		public synchronized void doAppend(ILoggingEvent eventObject) {
+			logged.add(eventObject.getFormattedMessage());
+		}
+
+		@Override
+		protected void append(ILoggingEvent iLoggingEvent) {
+
+		}
+	}
+}
