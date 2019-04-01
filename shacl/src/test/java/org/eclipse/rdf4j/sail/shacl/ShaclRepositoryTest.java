79b28a6c84e27d75ffddb1e31ff876ab88e8e56e
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-04-01 14:16:44 +1100

    eclipse/rdf4j#1236 moved tests for shacl and spin

--- shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/ShaclRepositoryTest.java
+++ shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/ShaclRepositoryTest.java
@@ -6,27 +6,21 @@
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *******************************************************************************/
 
-package org.eclipse.rdf4j.repository.sail.memory;
+package org.eclipse.rdf4j.sail.shacl;
 
-import org.eclipse.rdf4j.IsolationLevel;
 import org.eclipse.rdf4j.repository.Repository;
-import org.eclipse.rdf4j.repository.RepositoryConnectionTest;
 import org.eclipse.rdf4j.repository.RepositoryTest;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.eclipse.rdf4j.sail.shacl.ShaclSail;
 
-public class ShaclRepositoryConnectionTest extends RepositoryConnectionTest {
-
-	public ShaclRepositoryConnectionTest(IsolationLevel level) {
-		super(level);
-	}
+public class ShaclRepositoryTest extends RepositoryTest {
 
 	@Override
 	protected Repository createRepository() {
 		ShaclSail shaclSail = new ShaclSail(new MemoryStore());
 		shaclSail.setIgnoreNoShapesLoadedException(true);
+
 		return new SailRepository(shaclSail);
 	}
-
 }
