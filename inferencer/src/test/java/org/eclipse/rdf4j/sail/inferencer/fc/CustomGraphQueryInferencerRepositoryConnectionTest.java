cdb8d995645be6887a44fa3e0582914af5c57dc3
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-04-01 14:13:52 +1100

    eclipse/rdf4j#1236 moved store and lucene test suites

---
+++ inferencer/src/test/java/org/eclipse/rdf4j/sail/inferencer/fc/CustomGraphQueryInferencerRepositoryConnectionTest.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sail.inferencer.fc;
+
+import java.io.IOException;
+
+import org.eclipse.rdf4j.IsolationLevel;
+import org.eclipse.rdf4j.common.io.ResourceUtil;
+import org.eclipse.rdf4j.query.MalformedQueryException;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.UnsupportedQueryLanguageException;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnectionTest;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.SailException;
+import org.eclipse.rdf4j.sail.inferencer.fc.CustomGraphQueryInferencer;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+
+public class CustomGraphQueryInferencerRepositoryConnectionTest extends RepositoryConnectionTest {
+
+	public CustomGraphQueryInferencerRepositoryConnectionTest(IsolationLevel level) {
+		super(level);
+	}
+
+	@Override
+	protected Repository createRepository()
+			throws MalformedQueryException, UnsupportedQueryLanguageException, SailException, IOException {
+		return new SailRepository(new CustomGraphQueryInferencer(new MemoryStore(), QueryLanguage.SPARQL,
+				ResourceUtil.getString("/custom-query-inferencing/rule.rq"),
+				ResourceUtil.getString("/custom-query-inferencing/match.rq")));
+	}
+}
