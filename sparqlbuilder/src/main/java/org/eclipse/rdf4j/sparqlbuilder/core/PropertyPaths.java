815c9959dbd4638d287b5fcb803229dd85395065
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-04-07 15:05:16 +1000

    GH-1326 fix formatting

--- sparqlbuilder/src/main/java/org/eclipse/rdf4j/sparqlbuilder/core/PropertyPaths.java
+++ sparqlbuilder/src/main/java/org/eclipse/rdf4j/sparqlbuilder/core/PropertyPaths.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
-Copyright (c) 2019 Eclipse RDF4J contributors.
-All rights reserved. This program and the accompanying materials
-are made available under the terms of the Eclipse Distribution License v1.0
-which accompanies this distribution, and is available at
-http://www.eclipse.org/org/documents/edl-v10.php.
-*******************************************************************************/
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
 
 package org.eclipse.rdf4j.sparqlbuilder.core;
 
@@ -14,10 +14,11 @@ public class PropertyPaths {
 	/**
 	 * Construct property paths for use with the {@link SparqlBuilder}
 	 * 
-	 * <p><b>Example:</b> {@code subject.has(path(zeroOrMore(property)), object)}.<p>
+	 * <p>
+	 * <b>Example:</b> {@code subject.has(path(zeroOrMore(property)), object)}.
+	 * <p>
 	 * 
-	 * @param aElements 
-	 *        the path elements
+	 * @param aElements the path elements
 	 * @return a property path
 	 */
 	public static RdfPredicate path(RdfPredicate... aElements) {
