b139919ee5be98d05e42ddadb6b7903a09e04f8a
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-05-01 20:13:46 +1000

    GH-1223 added a basic concurrency unit test

--- sail-api/src/test/java/org/eclipse/rdf4j/sail/helpers/AbstractSailTest.java
+++ sail-api/src/test/java/org/eclipse/rdf4j/sail/helpers/AbstractSailTest.java
@@ -8,7 +8,13 @@
 package org.eclipse.rdf4j.sail.helpers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
@@ -26,8 +32,6 @@ import org.junit.Test;
 public class AbstractSailTest {
 
 	AbstractSail subject;
-
-	SailConnection connDouble = mock(SailConnection.class);
 
 	@Before
 	public void setUp() throws Exception {
@@ -52,6 +56,11 @@ public class AbstractSailTest {
 
 			@Override
 			protected SailConnection getConnectionInternal() throws SailException {
+				SailConnection connDouble = mock(SailConnection.class);
+				doAnswer(f -> {
+					subject.connectionClosed(connDouble);
+					return null;
+				}).when(connDouble).close();
 				return connDouble;
 			}
 
@@ -63,7 +72,6 @@ public class AbstractSailTest {
 		assertThat(subject.isInitialized()).isFalse();
 		SailConnection conn = subject.getConnection();
 		assertThat(subject.isInitialized()).isTrue();
-		assertThat(conn).isEqualTo(connDouble);
 	}
 
 	@Test
@@ -72,16 +80,69 @@ public class AbstractSailTest {
 		subject.init();
 		SailConnection conn = subject.getConnection();
 		assertThat(subject.isInitialized()).isTrue();
-		assertThat(conn).isEqualTo(connDouble);
 	}
 
+	@Test
 	public void testExplicitInitTwice() {
 		assertThat(subject.isInitialized()).isFalse();
 		subject.init();
 		subject.init();
 		SailConnection conn = subject.getConnection();
 		assertThat(subject.isInitialized()).isTrue();
-		assertThat(conn).isEqualTo(connDouble);
+	}
+
+	@Test
+	public void testConcurrentAutoInit() throws Exception {
+		int count = 200;
+		CountDownLatch latch = new CountDownLatch(count);
+
+		for (int i = 0; i < count; i++) {
+			new Thread(new SailGetConnectionTask(subject, latch)).start();
+		}
+
+		if (!latch.await(30, TimeUnit.SECONDS)) {
+			fail("possible deadlock detected");
+		}
+	}
+
+	class SailGetConnectionTask implements Runnable {
+
+		private final AbstractSail sail;
+		private CountDownLatch connectionObtained;
+
+		public SailGetConnectionTask(AbstractSail sail, CountDownLatch connectionObtained) {
+			this.sail = sail;
+			this.connectionObtained = connectionObtained;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Runnable#run()
+		 */
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(new Random().nextInt(1000));
+				if ((new Random().nextInt() & 1) == 0) {
+					sail.init(); // 50% of our runs do an explicit init
+				}
+				try (SailConnection conn = sail.getConnection()) {
+					Thread.sleep(10);
+				}
+				if (new Random().nextInt(4) == 1) {
+					sail.shutDown(); // roughly one in four do a shutdown follow by another get connection
+					try (SailConnection conn = sail.getConnection()) {
+						Thread.sleep(10);
+					}
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				this.connectionObtained.countDown();
+			}
+		}
+
 	}
 
 }
