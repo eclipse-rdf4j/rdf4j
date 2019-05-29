cc315f290a985587ba0745f3fe1471be58dd46d5
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-05-29 18:44:51 +1000

    formatter :cop:

--- sail-base/src/main/java/org/eclipse/rdf4j/sail/base/SailSourceConnection.java
+++ sail-base/src/main/java/org/eclipse/rdf4j/sail/base/SailSourceConnection.java
@@ -65,7 +65,6 @@ public abstract class SailSourceConnection extends NotifyingSailConnectionBase
 
 	private static final Logger logger = LoggerFactory.getLogger(SailSourceConnection.class);
 
-
 	/*-----------*
 	 * Variables *
 	 *-----------*/
