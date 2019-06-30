ae32f115f6885a1c120dc1d0906c0e5b355e34fe
Jeen Broekstra <jeenbroekstra@users.noreply.github.com>
2019-06-30 13:37:50 +1000

    Merge pull request #255 from DataFabricRus/1229-lucenesailconn
    Make LuceneSailConnection to replace query patterns with results inplace

---
+++ lucene-api/src/test/java/org/eclipse/rdf4j/sail/lucene/GeoRelationQuerySpecTest.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sail.lucene;
+
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.GEO;
+import org.eclipse.rdf4j.model.vocabulary.GEOF;
+import org.eclipse.rdf4j.query.algebra.BindingSetAssignment;
+import org.eclipse.rdf4j.query.impl.EmptyBindingSet;
+import org.eclipse.rdf4j.query.impl.MapBindingSet;
+import org.eclipse.rdf4j.query.parser.ParsedQuery;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class GeoRelationQuerySpecTest extends SearchQueryEvaluatorTest {
+
+	private static final ValueFactory VF = SimpleValueFactory.getInstance();
+	private static final String QUERY = "PREFIX geo:  <" + GEO.NAMESPACE + ">\n" +
+			"PREFIX geof: <" + GEOF.NAMESPACE + ">" +
+			"PREFIX uom: <http://www.opengis.net/def/uom/OGC/1.0/>\n" +
+			"SELECT ?matchUri ?match { " +
+			"   ?matchUri geo:asWKT ?match . " +
+			"   FILTER(geof:sfIntersects(\"POLYGON ((2.315 48.855, 2.360 48.835, 2.370 48.850, 2.315 48.855))\"^^geo:wktLiteral, ?match)) "
+			+
+			"}";
+
+	@Test
+	public void testReplaceQueryPatternsWithNonEmptyResults() {
+		final String expectedQueryPlan = "Projection\n" +
+				"   ProjectionElemList\n" +
+				"      ProjectionElem \"matchUri\"\n" +
+				"      ProjectionElem \"match\"\n" +
+				"   BindingSetAssignment ([[toUri=urn:subject4;to=\"POLYGON ((2.3294 48.8726, 2.2719 48.8643, 2.3370 48.8398, 2.3294 48.8726))\"^^<http://www.opengis.net/ont/geosparql#wktLiteral>]])\n";
+		final ParsedQuery query = parseQuery(QUERY);
+
+		final List<SearchQueryEvaluator> queries = new ArrayList<>();
+
+		new GeoRelationQuerySpecBuilder(new SearchIndexImpl())
+				.process(query.getTupleExpr(), EmptyBindingSet.getInstance(), queries);
+		assertEquals(1, queries.size());
+
+		final GeoRelationQuerySpec querySpec = (GeoRelationQuerySpec) queries.get(0);
+
+		final MapBindingSet bindingSet = new MapBindingSet();
+		bindingSet.addBinding("toUri", VF.createIRI("urn:subject4"));
+		bindingSet.addBinding("to", VF.createLiteral(
+				"POLYGON ((2.3294 48.8726, 2.2719 48.8643, 2.3370 48.8398, 2.3294 48.8726))", GEO.WKT_LITERAL));
+
+		BindingSetAssignment bsa = new BindingSetAssignment();
+		bsa.setBindingSets(Collections.singletonList(bindingSet));
+
+		querySpec.replaceQueryPatternsWithResults(bsa);
+
+		assertEquals(
+				expectedQueryPlan,
+				querySpec.getParentQueryModelNode().getParentNode().toString());
+	}
+
+	@Test
+	public void testReplaceQueryPatternsWithEmptyResults() {
+		final String expectedQueryPlan = "Projection\n" +
+				"   ProjectionElemList\n" +
+				"      ProjectionElem \"matchUri\"\n" +
+				"      ProjectionElem \"match\"\n" +
+				"   EmptySet\n";
+		final ParsedQuery query = parseQuery(QUERY);
+
+		final List<SearchQueryEvaluator> queries = new ArrayList<>();
+
+		new GeoRelationQuerySpecBuilder(new SearchIndexImpl())
+				.process(query.getTupleExpr(), EmptyBindingSet.getInstance(), queries);
+		assertEquals(1, queries.size());
+
+		final GeoRelationQuerySpec querySpec = (GeoRelationQuerySpec) queries.get(0);
+
+		BindingSetAssignment bsa = new BindingSetAssignment();
+
+		querySpec.replaceQueryPatternsWithResults(bsa);
+
+		assertEquals(
+				expectedQueryPlan,
+				querySpec.getParentQueryModelNode().getParentNode().toString());
+	}
+
+}
---
+++ lucene-api/src/test/java/org/eclipse/rdf4j/sail/lucene/SearchQueryEvaluatorTest.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sail.lucene;
+
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.GEO;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.impl.MapBindingSet;
+import org.eclipse.rdf4j.query.parser.ParsedQuery;
+import org.eclipse.rdf4j.query.parser.sparql.SPARQLParser;
+import org.eclipse.rdf4j.sail.SailException;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+abstract class SearchQueryEvaluatorTest {
+	protected ParsedQuery parseQuery(String query) {
+		return new SPARQLParser().parseQuery(query, "urn:base:");
+	}
+
+	protected Collection<BindingSet> createBindingSet(String name, String iri) {
+		MapBindingSet bindingSet = new MapBindingSet();
+		bindingSet.addBinding(name, SimpleValueFactory.getInstance().createIRI(iri));
+		return Collections.singletonList(bindingSet);
+	}
+
+	protected class SearchIndexImpl implements SearchIndex {
+		protected Set<String> wktFields = Collections.singleton(SearchFields.getPropertyField(GEO.AS_WKT));
+
+		@Override
+		public void initialize(Properties parameters) throws Exception {
+		}
+
+		@Override
+		public Collection<BindingSet> evaluate(QuerySpec query) throws SailException {
+			return null;
+		}
+
+		@Override
+		public Collection<BindingSet> evaluate(SearchQueryEvaluator query) throws SailException {
+			return null;
+		}
+
+		@Override
+		public void beginReading() throws IOException {
+		}
+
+		@Override
+		public void endReading() throws IOException {
+		}
+
+		@Override
+		public void shutDown() throws IOException {
+		}
+
+		@Override
+		public boolean accept(Literal literal) {
+			return false;
+		}
+
+		@Override
+		public boolean isGeoField(String propertyName) {
+			return (wktFields != null) && wktFields.contains(propertyName);
+		}
+
+		@Override
+		public void begin() throws IOException {
+		}
+
+		@Override
+		public void commit() throws IOException {
+		}
+
+		@Override
+		public void rollback() throws IOException {
+		}
+
+		@Override
+		public void addStatement(Statement statement) throws IOException {
+		}
+
+		@Override
+		public void removeStatement(Statement statement) throws IOException {
+		}
+
+		@Override
+		public void addRemoveStatements(Collection<Statement> added, Collection<Statement> removed) throws IOException {
+		}
+
+		@Override
+		public void clearContexts(Resource... contexts) throws IOException {
+		}
+
+		@Override
+		public void addDocuments(Resource subject, List<Statement> statements) throws IOException {
+		}
+
+		@Override
+		public void clear() throws IOException {
+		}
+	}
+}
