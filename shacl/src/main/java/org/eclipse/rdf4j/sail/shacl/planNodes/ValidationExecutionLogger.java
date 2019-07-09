fba060004d7627a8bbd7c56439fd290e141e9c9b
Håvard Ottestad <hmottestad@gmail.com>
2019-07-09 15:56:51 +0200

    private field
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- shacl/src/main/java/org/eclipse/rdf4j/sail/shacl/planNodes/ValidationExecutionLogger.java
+++ shacl/src/main/java/org/eclipse/rdf4j/sail/shacl/planNodes/ValidationExecutionLogger.java
@@ -17,7 +17,7 @@ public class ValidationExecutionLogger {
 
 	private List<LogStatement> list = new ArrayList<>();
 
-	public boolean groupedLogging = true;
+	private static boolean groupedLogging = true;
 
 	void log(int depth, String name, Tuple tuple, PlanNode planNode, String id) {
 		LogStatement logStatement = new LogStatement(depth, name, tuple, planNode, id);
