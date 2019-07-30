060d5a4a01d032970676a8bdd9c63ebeddcbf4e6
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-07-30 21:17:20 +1000

    GH-52 added sparqlbuilder to rdf4j-client deps + javadoc cleanup

---
+++ model/src/main/java/org/eclipse/rdf4j/model/util/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Helper classes for working with RDF models.
+ */
+package org.eclipse.rdf4j.model.util;
---
+++ repository/api/src/main/java/org/eclipse/rdf4j/repository/util/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Helper classes for working with Repositories.
+ */
+package org.eclipse.rdf4j.repository.util;
