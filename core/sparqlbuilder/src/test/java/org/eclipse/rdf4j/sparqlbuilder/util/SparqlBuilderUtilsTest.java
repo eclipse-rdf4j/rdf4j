abe551063497ca31766f70b442db5a6da98a976c
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-09-07 10:06:42 +1000

    unit test setup [WIP]

---
+++ core/sparqlbuilder/src/test/java/org/eclipse/rdf4j/sparqlbuilder/util/SparqlBuilderUtilsTest.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sparqlbuilder.util;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SparqlBuilderUtilsTest {
+
+	@Test
+	public void testGetOrCreateAndModifyOptional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAppendAndNewlineIfPresent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAppendQueryElementIfPresent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAppendStringIfPresent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBracedString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBracketedString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParenthesizedString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQuotedString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLongQuotedString() {
+		fail("Not yet implemented");
+	}
+
+}
