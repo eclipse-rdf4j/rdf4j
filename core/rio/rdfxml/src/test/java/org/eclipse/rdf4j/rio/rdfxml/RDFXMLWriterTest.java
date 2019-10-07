55d0e5490271476f96f6f668114005818d727f61
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2019-10-07 23:37:25 +0200

    Added test

--- core/rio/rdfxml/src/test/java/org/eclipse/rdf4j/rio/rdfxml/RDFXMLWriterTest.java
+++ core/rio/rdfxml/src/test/java/org/eclipse/rdf4j/rio/rdfxml/RDFXMLWriterTest.java
@@ -7,9 +7,14 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.rdfxml;
 
+import java.io.StringWriter;
+import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.RDFWriterTest;
 import org.eclipse.rdf4j.rio.WriterConfig;
 import org.eclipse.rdf4j.rio.helpers.BasicWriterSettings;
+import org.eclipse.rdf4j.rio.helpers.XMLWriterSettings;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class RDFXMLWriterTest extends RDFWriterTest {
 
@@ -21,5 +26,32 @@ public class RDFXMLWriterTest extends RDFWriterTest {
 	protected void setupWriterConfig(WriterConfig config) {
 		config.set(BasicWriterSettings.PRETTY_PRINT, false);
 	}
-
+	
+	@Test
+	public void singleQuotes() {
+		StringWriter outputWriter = new StringWriter();
+		RDFWriter rdfWriter = rdfWriterFactory.getWriter(outputWriter);
+		rdfWriter.getWriterConfig().set(BasicWriterSettings.PRETTY_PRINT, true);
+		rdfWriter.getWriterConfig().set(XMLWriterSettings.INCLUDE_XML_PI, true);
+		rdfWriter.getWriterConfig().set(XMLWriterSettings.USE_SINGLE_QUOTES, true);
+		rdfWriter.startRDF();
+		rdfWriter.endRDF();
+		String res = outputWriter.toString();
+		Assert.assertTrue("Not equal", res.startsWith("<?xml version='1.0' encoding='UTF-8'?>"));
+		Assert.assertFalse("Contains unescaped \"", res.contains("\""));
+	}
+	
+	@Test
+	public void doubleQuotes() {
+		StringWriter outputWriter = new StringWriter();
+		RDFWriter rdfWriter = rdfWriterFactory.getWriter(outputWriter);
+		rdfWriter.getWriterConfig().set(BasicWriterSettings.PRETTY_PRINT, true);
+		rdfWriter.getWriterConfig().set(XMLWriterSettings.INCLUDE_XML_PI, true);
+		rdfWriter.getWriterConfig().set(XMLWriterSettings.USE_SINGLE_QUOTES, false);
+		rdfWriter.startRDF();
+		rdfWriter.endRDF();
+		String res = outputWriter.toString();
+		Assert.assertTrue("Not equal", res.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
+		Assert.assertFalse("Contains unescaped '", res.contains("'"));
+	}
 }
