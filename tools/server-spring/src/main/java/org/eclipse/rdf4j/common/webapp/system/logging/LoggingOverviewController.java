579fe763ff72398b63ccddf6593faacc520d75df
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2019-11-03 19:40:03 +0000

    Code formatting
    Signed-off-by: Bart Hanssens <bart.hanssens@bosa.fgov.be>

--- tools/server-spring/src/main/java/org/eclipse/rdf4j/common/webapp/system/logging/LoggingOverviewController.java
+++ tools/server-spring/src/main/java/org/eclipse/rdf4j/common/webapp/system/logging/LoggingOverviewController.java
@@ -43,7 +43,7 @@ public class LoggingOverviewController implements Controller {
 		model.put("logreader", logReader);
 		model.put("offset", new Integer(offset));
 		model.put("count", new Integer(count));
-		model.put("countsAvailable", Arrays.asList(new Integer[] { 50, 100, 200, 500}));
+		model.put("countsAvailable", Arrays.asList(new Integer[] { 50, 100, 200, 500 }));
 		if (logReader.supportsLevelFilter()) {
 			LogLevel level = logReader.getLevel();
 			model.put("level", (level == null) ? "ALL" : level.toString());
