9c9837d8534839889ad8671cd51539fb9c824422
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2019-11-03 21:24:40 +0000

    Fix for incorrect conflict resolution
    Signed-off-by: Bart Hanssens <bart.hanssens@bosa.fgov.be>

--- tools/workbench/src/main/java/org/eclipse/rdf4j/workbench/commands/UpdateServlet.java
+++ tools/workbench/src/main/java/org/eclipse/rdf4j/workbench/commands/UpdateServlet.java
@@ -60,7 +60,7 @@ public class UpdateServlet extends TransformationServlet {
 		try (RepositoryConnection con = repository.getConnection()) {
 			try {
 				con
-					.prepareUpdate(QueryLanguage.SPARQL, updateString);
+					.prepareUpdate(QueryLanguage.SPARQL, updateString)
 					.execute();
 			} catch (RepositoryException | MalformedQueryException | UpdateExecutionException e) {
 				throw new BadRequestException(e.getMessage());
