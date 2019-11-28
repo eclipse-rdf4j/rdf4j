2758f30ec74c855761657b0709693bad2eed2d40
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2019-11-28 12:05:37 +0100

    Update core/model/src/main/java/org/eclipse/rdf4j/model/vocabulary/TIME.java
    comment syntax
    
    Co-Authored-By: Jeen Broekstra <jeen.broekstra@gmail.com>

--- core/model/src/main/java/org/eclipse/rdf4j/model/vocabulary/TIME.java
+++ core/model/src/main/java/org/eclipse/rdf4j/model/vocabulary/TIME.java
@@ -1,4 +1,4 @@
-/**
+/*******************************************************************************
  * Copyright (c) 2019 Eclipse RDF4J contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
