35c75c323872603b9206678b2845cf134ccb49bd
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-12-19 18:48:34 +1100

    Revert "comment out embedded-elasticsearch dependency and remove all tests (#1754)"
    This reverts commit 0f60b28280c7cacdcae29eeeb538d1d6f068d13e.

---
+++ core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/compliance/ElasticsearchStoreTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sail.elasticsearchstore.compliance;
+
+import org.assertj.core.util.Files;
+import org.eclipse.rdf4j.sail.NotifyingSail;
+import org.eclipse.rdf4j.sail.NotifyingSailConnection;
+import org.eclipse.rdf4j.sail.RDFNotifyingStoreTest;
+import org.eclipse.rdf4j.sail.SailException;
+import org.eclipse.rdf4j.sail.elasticsearchstore.SingletonClientProvider;
+import org.eclipse.rdf4j.sail.elasticsearchstore.ElasticsearchStore;
+import org.eclipse.rdf4j.sail.elasticsearchstore.TestHelpers;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * An extension of RDFStoreTest for testing the class <tt>org.eclipse.rdf4j.sesame.sail.memory.MemoryStore</tt>.
+ */
+public class ElasticsearchStoreTest extends RDFNotifyingStoreTest {
+
+	/*---------*
+	 * Methods *
+	 *---------*/
+
+	private static EmbeddedElastic embeddedElastic;
+
+	private static File installLocation = Files.newTemporaryFolder();
+	static SingletonClientProvider clientPool;
+
+	@BeforeClass
+	public static void beforeClass() throws IOException, InterruptedException {
+
+		embeddedElastic = TestHelpers.startElasticsearch(installLocation);
+		clientPool = new SingletonClientProvider("localhost", embeddedElastic.getTransportTcpPort(), "cluster1");
+
+	}
+
+	@AfterClass
+	public static void afterClass() throws Exception {
+
+		clientPool.close();
+		TestHelpers.stopElasticsearch(embeddedElastic, installLocation);
+	}
+
+	@Override
+	protected NotifyingSail createSail() throws SailException {
+		NotifyingSail sail = new ElasticsearchStore(clientPool, "index1");
+		try (NotifyingSailConnection connection = sail.getConnection()) {
+			connection.begin();
+			connection.clear();
+			connection.commit();
+		}
+		return sail;
+	}
+}
---
+++ core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/config/ElasticsearchStoreConfigTest.java
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sail.elasticsearchstore.config;
+
+import org.eclipse.rdf4j.model.BNode;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.impl.TreeModel;
+import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.sail.config.SailConfigException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ElasticsearchStoreConfigTest {
+
+	private ElasticsearchStoreConfig subject;
+
+	private BNode implNode;
+
+	private ModelBuilder mb;
+
+	@Before
+	public void setUp() throws Exception {
+		subject = new ElasticsearchStoreConfig();
+		implNode = SimpleValueFactory.getInstance().createBNode();
+		mb = new ModelBuilder().subject(implNode);
+	}
+
+	@Test
+	public void defaultsCorrectlySet() {
+		assertThat(subject.getClusterName()).isNull();
+		assertThat(subject.getHostname()).isNull();
+		assertThat(subject.getIndex()).isNull();
+		assertThat(subject.getPort()).isEqualTo(-1);
+
+	}
+
+	@Test
+	public void parseFromModelSetValuesCorrectly() {
+
+		// FIXME we need to set formatting guidelines for this kind of thing
+		// @formatter:off
+		mb
+			.add(ElasticsearchStoreSchema.hostname, "host1")
+			.add(ElasticsearchStoreSchema.clusterName, "cluster1")
+			.add(ElasticsearchStoreSchema.index, "index1")
+			.add(ElasticsearchStoreSchema.port, 9300);
+		// @formatter:on
+
+		subject.parse(mb.build(), implNode);
+
+		assertThat(subject.getHostname()).isEqualTo("host1");
+		assertThat(subject.getClusterName()).isEqualTo("cluster1");
+		assertThat(subject.getIndex()).isEqualTo("index1");
+		assertThat(subject.getPort()).isEqualTo(9300);
+
+	}
+
+	@Test
+	public void parseFromPartialModelSetValuesCorrectly() {
+		mb
+				.add(ElasticsearchStoreSchema.hostname, "host1")
+				.add(ElasticsearchStoreSchema.port, 9300);
+
+		subject.parse(mb.build(), implNode);
+
+		assertThat(subject.getHostname()).isEqualTo("host1");
+		assertThat(subject.getPort()).isEqualTo(9300);
+	}
+
+	@Test(expected = SailConfigException.class)
+	public void parseInvalidModelGivesCorrectException() {
+
+		mb
+				.add(ElasticsearchStoreSchema.port, "port1");
+
+		subject.parse(mb.build(), implNode);
+
+	}
+
+	@Test
+	public void exportAddsAllConfigData() {
+
+		mb
+				.add(ElasticsearchStoreSchema.hostname, "host1")
+				.add(ElasticsearchStoreSchema.clusterName, "cluster1")
+				.add(ElasticsearchStoreSchema.index, "index1")
+				.add(ElasticsearchStoreSchema.port, 9300);
+		// @formatter:on
+
+		subject.parse(mb.build(), implNode);
+
+		Model m = new TreeModel();
+		Resource node = subject.export(m);
+
+		assertThat(m.contains(node, ElasticsearchStoreSchema.hostname, null)).isTrue();
+		assertThat(m.contains(node, ElasticsearchStoreSchema.clusterName, null)).isTrue();
+		assertThat(m.contains(node, ElasticsearchStoreSchema.index, null)).isTrue();
+		assertThat(m.contains(node, ElasticsearchStoreSchema.port, null)).isTrue();
+
+	}
+
+}
