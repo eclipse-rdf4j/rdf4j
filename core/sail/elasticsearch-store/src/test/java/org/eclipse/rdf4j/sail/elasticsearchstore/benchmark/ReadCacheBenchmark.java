5b2028b3d748bf899ac1b079da709e356cb2f7c3
Jeen Broekstra <jeen.broekstra@gmail.com>
2019-12-19 19:43:42 +1100

    Revert "comment out embedded-elasticsearch dependency and remove all tests (#1754)" (#1757)
    This reverts commit 0f60b28280c7cacdcae29eeeb538d1d6f068d13e.

---
+++ core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/benchmark/ReadCacheBenchmark.java
@@ -0,0 +1,233 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+
+package org.eclipse.rdf4j.sail.elasticsearchstore.benchmark;
+
+import org.apache.commons.io.IOUtils;
+import org.assertj.core.util.Files;
+import org.eclipse.rdf4j.IsolationLevels;
+import org.eclipse.rdf4j.common.iteration.Iterations;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.sail.elasticsearchstore.ElasticsearchStore;
+import org.eclipse.rdf4j.sail.elasticsearchstore.TestHelpers;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Håvard Ottestad
+ */
+@State(Scope.Benchmark)
+@Warmup(iterations = 20)
+@BenchmarkMode({ Mode.AverageTime })
+@Fork(value = 1, jvmArgs = { "-Xms8G", "-Xmx8G", "-Xmn4G", "-XX:+UseSerialGC" })
+//@Fork(value = 1, jvmArgs = {"-Xms8G", "-Xmx8G", "-Xmn4G", "-XX:+UseSerialGC", "-XX:+UnlockCommercialFeatures", "-XX:StartFlightRecording=delay=60s,duration=120s,filename=recording.jfr,settings=profile", "-XX:FlightRecorderOptions=samplethreads=true,stackdepth=1024", "-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints"})
+@Measurement(iterations = 10)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class ReadCacheBenchmark {
+
+	private static EmbeddedElastic embeddedElastic;
+
+	private static File installLocation = Files.newTemporaryFolder();
+
+	private SailRepository repoWithoutCache;
+	private SailRepository repoWithCache;
+
+	private static final String query1;
+	private static final String query5;
+	private static final String query6;
+
+	static {
+		try {
+			query1 = IOUtils.toString(getResourceAsStream("benchmarkFiles/query1.qr"), StandardCharsets.UTF_8);
+			query5 = IOUtils.toString(getResourceAsStream("benchmarkFiles/query5.qr"), StandardCharsets.UTF_8);
+			query6 = IOUtils.toString(getResourceAsStream("benchmarkFiles/query6.qr"), StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private List<Statement> statementList;
+
+	@Setup(Level.Trial)
+	public void beforeClass() throws IOException, InterruptedException {
+
+		embeddedElastic = TestHelpers.startElasticsearch(installLocation,
+				"/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home");
+
+		repoWithoutCache = new SailRepository(
+				new ElasticsearchStore("localhost", embeddedElastic.getTransportTcpPort(), "cluster1", "testindex1",
+						false));
+
+		repoWithCache = new SailRepository(
+				new ElasticsearchStore("localhost", embeddedElastic.getTransportTcpPort(), "cluster1", "testindex2",
+						true));
+
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+			connection.begin(IsolationLevels.NONE);
+			connection.clear();
+			connection.add(getResourceAsStream("benchmarkFiles/datagovbe-valid.ttl"), "", RDFFormat.TURTLE);
+			connection.commit();
+		}
+
+		try (SailRepositoryConnection connection = repoWithoutCache.getConnection()) {
+			connection.begin(IsolationLevels.NONE);
+			connection.clear();
+			connection.add(getResourceAsStream("benchmarkFiles/datagovbe-valid.ttl"), "", RDFFormat.TURTLE);
+			connection.commit();
+
+		}
+
+		System.gc();
+
+	}
+
+	private static InputStream getResourceAsStream(String name) {
+		return ReadCacheBenchmark.class.getClassLoader().getResourceAsStream(name);
+	}
+
+	@TearDown(Level.Trial)
+	public void afterClass() {
+
+		repoWithoutCache.shutDown();
+		TestHelpers.stopElasticsearch(embeddedElastic, installLocation);
+
+	}
+
+	@Benchmark
+	public List<BindingSet> groupByQueryWithoutCache() {
+
+		try (SailRepositoryConnection connection = repoWithoutCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query1)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> groupByQueryWithCache() {
+
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query1)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> groupByQueryWithCacheCleared() {
+		clearCache();
+
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query1)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> complexQueryWithoutCache() {
+
+		try (SailRepositoryConnection connection = repoWithoutCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query5)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> complexQueryWithCache() {
+
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query5)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> complexQueryWithCacheCleared() {
+		clearCache();
+
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query5)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> pathQueryWithoutCache() {
+
+		try (SailRepositoryConnection connection = repoWithoutCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query6)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> pathQueryWithCache() {
+
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+			return Iterations.asList(connection
+					.prepareTupleQuery(query6)
+					.evaluate());
+		}
+	}
+
+	@Benchmark
+	public List<BindingSet> pathQueryWithCacheCleared() {
+
+		clearCache();
+
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+
+			return Iterations.asList(connection
+					.prepareTupleQuery(query6)
+					.evaluate());
+		}
+	}
+
+	private void clearCache() {
+		try (SailRepositoryConnection connection = repoWithCache.getConnection()) {
+			connection.begin();
+			Literal literal = SimpleValueFactory.getInstance().createLiteral("jfiewojifjewo");
+			connection.add(RDFS.RESOURCE, RDFS.LABEL, literal);
+			connection.commit();
+			connection.begin();
+			connection.remove(RDFS.RESOURCE, RDFS.LABEL, literal);
+			connection.commit();
+		}
+	}
+
+}
---
+++ core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/compliance/ElasticsearchStoreConnectionTest.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.sail.elasticsearchstore.compliance;
+
+import org.assertj.core.util.Files;
+import org.eclipse.rdf4j.IsolationLevel;
+import org.eclipse.rdf4j.IsolationLevels;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnectionTest;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.elasticsearchstore.SingletonClientProvider;
+import org.eclipse.rdf4j.sail.elasticsearchstore.ElasticsearchStore;
+import org.eclipse.rdf4j.sail.elasticsearchstore.TestHelpers;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runners.Parameterized;
+import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ElasticsearchStoreConnectionTest extends RepositoryConnectionTest {
+
+	public ElasticsearchStoreConnectionTest(IsolationLevel level) {
+		super(level);
+	}
+
+	private static EmbeddedElastic embeddedElastic;
+
+	private static File installLocation = Files.newTemporaryFolder();
+	private static SingletonClientProvider clientPool;
+
+	@BeforeClass
+	public static void beforeClass() throws IOException, InterruptedException {
+
+		embeddedElastic = TestHelpers.startElasticsearch(installLocation);
+		clientPool = new SingletonClientProvider("localhost", embeddedElastic.getTransportTcpPort(), "cluster1");
+
+	}
+
+	@AfterClass
+	public static void afterClass() throws Exception {
+
+		clientPool.close();
+		TestHelpers.stopElasticsearch(embeddedElastic, installLocation);
+
+	}
+
+	@Parameterized.Parameters(name = "{0}")
+	public static IsolationLevel[] parameters() {
+		return new IsolationLevel[] {
+				IsolationLevels.NONE,
+				IsolationLevels.READ_UNCOMMITTED,
+				IsolationLevels.READ_COMMITTED
+		};
+	}
+
+	@Override
+	protected Repository createRepository() {
+		return new SailRepository(
+				new ElasticsearchStore(clientPool, "index1"));
+	}
+}
