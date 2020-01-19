ef0a261855755639a2ac10a55b266e6f937a5eda
Håvard Ottestad <hmottestad@gmail.com>
2020-01-19 12:04:10 +0100

    #1108 transaction status now uses single byte for higher TPS
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/sail/nativerdf/src/main/java/org/eclipse/rdf4j/sail/nativerdf/TxnStatusFile.java
+++ core/sail/nativerdf/src/main/java/org/eclipse/rdf4j/sail/nativerdf/TxnStatusFile.java
@@ -24,30 +24,35 @@ class TxnStatusFile {
 		 * No active transaction. This occurs if no transaction has been started yet, or if all transactions have been
 		 * committed or rolled back.
 		 */
-		NONE,
+		NONE((byte) 1),
 
 		/**
 		 * A transaction has been started, but was not yet committed or rolled back.
 		 */
-		ACTIVE,
+		ACTIVE((byte) 2),
 
 		/**
 		 * A transaction is being committed.
 		 */
-		COMMITTING,
+		COMMITTING((byte) 3),
 
 		/**
 		 * A transaction is being rolled back.
 		 */
-		ROLLING_BACK,
+		ROLLING_BACK((byte) 4),
 
 		/**
 		 * The transaction status is unknown.
 		 */
-		UNKNOWN;
-	}
+		UNKNOWN((byte) 5);
 
-	private static final Charset US_ASCII = Charset.forName("US-ASCII");
+		byte[] onDisk;
+
+		TxnStatus(byte onDisk) {
+			this.onDisk = new byte[1];
+			this.onDisk[0] = onDisk;
+		}
+	}
 
 	/**
 	 * The name of the transaction status file.
@@ -58,7 +63,7 @@ class TxnStatusFile {
 
 	/**
 	 * Creates a new transaction status file. New files are initialized with {@link TxnStatus#NONE}.
-	 * 
+	 *
 	 * @param dataDir The directory for the transaction status file.
 	 * @throws IOException If the file did not yet exist and could not be written to.
 	 */
@@ -77,37 +82,48 @@ class TxnStatusFile {
 
 	/**
 	 * Writes the specified transaction status to file.
-	 * 
+	 *
 	 * @param txnStatus The transaction status to write.
 	 * @throws IOException If the transaction status could not be written to file.
 	 */
 	public void setTxnStatus(TxnStatus txnStatus) throws IOException {
-		byte[] bytes = txnStatus.name().getBytes(US_ASCII);
+		byte[] bytes = txnStatus.onDisk;
 		nioFile.truncate(bytes.length);
 		nioFile.writeBytes(bytes, 0);
 	}
 
 	/**
 	 * Reads the transaction status from file.
-	 * 
+	 *
 	 * @return The read transaction status, or {@link TxnStatus#UNKNOWN} when the file contains an unrecognized status
 	 *         string.
 	 * @throws IOException If the transaction status file could not be read.
 	 */
 	public TxnStatus getTxnStatus() throws IOException {
 		byte[] bytes = nioFile.readBytes(0, (int) nioFile.size());
-		String s = new String(bytes, US_ASCII);
-		try {
-			return TxnStatus.valueOf(s);
-		} catch (IllegalArgumentException e) {
-			// use platform encoding for backwards compatibility with versions
-			// older than 2.6.6:
-			s = new String(bytes);
-			try {
-				return TxnStatus.valueOf(s);
-			} catch (IllegalArgumentException e2) {
-				return TxnStatus.UNKNOWN;
+		TxnStatus status = TxnStatus.UNKNOWN;
+
+		if (bytes.length == 1) {
+			switch (bytes[0]) {
+			case 1:
+				status = TxnStatus.NONE;
+				break;
+			case 2:
+				status = TxnStatus.ACTIVE;
+				break;
+			case 3:
+				status = TxnStatus.COMMITTING;
+				break;
+			case 4:
+				status = TxnStatus.ROLLING_BACK;
+				break;
+			case 5:
+				status = TxnStatus.UNKNOWN;
+				break;
 			}
 		}
+
+		return status;
+
 	}
 }
