8b54254db45469a0d93b2f6e44d73c94738d5cda
hmottestad <hmottestad@gmail.com>
2020-01-21 07:39:40 +0100

    #1108 transaction status now uses single byte for higher TPS (#1848)
    * #1108 transaction status now uses single byte for higher TPS
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>
    
    * #1108 backwards compatible
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>
    
    * #1108 test if we are compatible with the old txn-status files
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>
    
    * #1108 benchmarks
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>
    
    * #1108 reduced benchmark
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>
    
    * #1108 updates based on comments
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>
    
    * #1108 assert that current status has been updated correctly
    
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/sail/nativerdf/src/main/java/org/eclipse/rdf4j/sail/nativerdf/TxnStatusFile.java
+++ core/sail/nativerdf/src/main/java/org/eclipse/rdf4j/sail/nativerdf/TxnStatusFile.java
@@ -7,47 +7,64 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.sail.nativerdf;
 
+import org.eclipse.rdf4j.common.io.NioFile;
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 
-import org.eclipse.rdf4j.common.io.NioFile;
+import static java.nio.charset.StandardCharsets.US_ASCII;
 
 /**
  * Writes transaction statuses to a file.
  */
 class TxnStatusFile {
 
-	public static enum TxnStatus {
+	public enum TxnStatus {
 
 		/**
 		 * No active transaction. This occurs if no transaction has been started yet, or if all transactions have been
 		 * committed or rolled back.
 		 */
-		NONE,
+		NONE(TxnStatus.NONE_BYTE),
 
 		/**
 		 * A transaction has been started, but was not yet committed or rolled back.
 		 */
-		ACTIVE,
+		ACTIVE(TxnStatus.ACTIVE_BYTE),
 
 		/**
 		 * A transaction is being committed.
 		 */
-		COMMITTING,
+		COMMITTING(TxnStatus.COMMITTING_BYTE),
 
 		/**
 		 * A transaction is being rolled back.
 		 */
-		ROLLING_BACK,
+		ROLLING_BACK(TxnStatus.ROLLING_BACK_BYTE),
 
 		/**
 		 * The transaction status is unknown.
 		 */
-		UNKNOWN;
-	}
+		UNKNOWN(TxnStatus.UNKNOWN_BYTE);
 
-	private static final Charset US_ASCII = Charset.forName("US-ASCII");
+		private final byte[] onDisk;
+
+		TxnStatus(byte onDisk) {
+			this.onDisk = new byte[1];
+			this.onDisk[0] = onDisk;
+		}
+
+		byte[] getOnDisk() {
+			return onDisk;
+		}
+
+		private static final byte NONE_BYTE = (byte) 0b00000001;
+		private static final byte ACTIVE_BYTE = (byte) 0b00000010;
+		private static final byte COMMITTING_BYTE = (byte) 0b00000100;
+		private static final byte ROLLING_BACK_BYTE = (byte) 0b00001000;
+		private static final byte UNKNOWN_BYTE = (byte) 0b00010000;
+
+	}
 
 	/**
 	 * The name of the transaction status file.
@@ -58,7 +75,7 @@ class TxnStatusFile {
 
 	/**
 	 * Creates a new transaction status file. New files are initialized with {@link TxnStatus#NONE}.
-	 * 
+	 *
 	 * @param dataDir The directory for the transaction status file.
 	 * @throws IOException If the file did not yet exist and could not be written to.
 	 */
@@ -77,25 +94,55 @@ class TxnStatusFile {
 
 	/**
 	 * Writes the specified transaction status to file.
-	 * 
+	 *
 	 * @param txnStatus The transaction status to write.
 	 * @throws IOException If the transaction status could not be written to file.
 	 */
 	public void setTxnStatus(TxnStatus txnStatus) throws IOException {
-		byte[] bytes = txnStatus.name().getBytes(US_ASCII);
+		byte[] bytes = txnStatus.onDisk;
 		nioFile.truncate(bytes.length);
 		nioFile.writeBytes(bytes, 0);
 	}
 
 	/**
 	 * Reads the transaction status from file.
-	 * 
+	 *
 	 * @return The read transaction status, or {@link TxnStatus#UNKNOWN} when the file contains an unrecognized status
 	 *         string.
 	 * @throws IOException If the transaction status file could not be read.
 	 */
 	public TxnStatus getTxnStatus() throws IOException {
+		byte[] bytes = nioFile.readBytes(0, 1);
+
+		TxnStatus status;
+
+		switch (bytes[0]) {
+		case TxnStatus.NONE_BYTE:
+			status = TxnStatus.NONE;
+			break;
+		case TxnStatus.ACTIVE_BYTE:
+			status = TxnStatus.ACTIVE;
+			break;
+		case TxnStatus.COMMITTING_BYTE:
+			status = TxnStatus.COMMITTING;
+			break;
+		case TxnStatus.ROLLING_BACK_BYTE:
+			status = TxnStatus.ROLLING_BACK;
+			break;
+		case TxnStatus.UNKNOWN_BYTE:
+			status = TxnStatus.UNKNOWN;
+			break;
+		default:
+			status = getTxnStatusDeprecated();
+		}
+
+		return status;
+
+	}
+
+	private TxnStatus getTxnStatusDeprecated() throws IOException {
 		byte[] bytes = nioFile.readBytes(0, (int) nioFile.size());
+
 		String s = new String(bytes, US_ASCII);
 		try {
 			return TxnStatus.valueOf(s);
