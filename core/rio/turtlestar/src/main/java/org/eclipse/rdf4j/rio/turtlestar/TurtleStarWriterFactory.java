b7822a32f0d5215342bd9450fa15a76b84739634
Pavel Mihaylov <pavel@ontotext.com>
2020-01-30 15:46:03 +0200

    GH-1484: Minimal RDF* model and parser/writers for Turtle* and TriG*
    Signed-off-by: Pavel Mihaylov <pavel@ontotext.com>

---
+++ core/rio/turtlestar/src/main/java/org/eclipse/rdf4j/rio/turtlestar/TurtleStarWriterFactory.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Eclipse RDF4J contributors, Aduna, and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package org.eclipse.rdf4j.rio.turtlestar;
+
+import org.eclipse.rdf4j.common.net.ParsedIRI;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFWriter;
+import org.eclipse.rdf4j.rio.RDFWriterFactory;
+import org.eclipse.rdf4j.rio.turtle.ArrangedWriter;
+
+import java.io.OutputStream;
+import java.io.Writer;
+import java.net.URISyntaxException;
+
+/**
+ * An {@link RDFWriterFactory} for Turtle* writers.
+ *
+ * @author Pavel Mihaylov
+ */
+public class TurtleStarWriterFactory implements RDFWriterFactory {
+
+	/**
+	 * Returns {@link RDFFormat#TURTLESTAR}.
+	 */
+	@Override
+	public RDFFormat getRDFFormat() {
+		return RDFFormat.TURTLESTAR;
+	}
+
+	/**
+	 * Returns a new instance of {@link TurtleStarWriter}.
+	 */
+	@Override
+	public RDFWriter getWriter(OutputStream out) {
+		return new ArrangedWriter(new TurtleStarWriter(out));
+	}
+
+	@Override
+	public RDFWriter getWriter(OutputStream out, String baseURI) throws URISyntaxException {
+		return new ArrangedWriter(new TurtleStarWriter(out, new ParsedIRI(baseURI)));
+	}
+
+	/**
+	 * Returns a new instance of {@link TurtleStarWriter}.
+	 */
+	@Override
+	public RDFWriter getWriter(Writer writer) {
+		return new ArrangedWriter(new TurtleStarWriter(writer));
+	}
+
+	@Override
+	public RDFWriter getWriter(Writer writer, String baseURI) throws URISyntaxException {
+		return new ArrangedWriter(new TurtleStarWriter(writer, new ParsedIRI(baseURI)));
+	}
+}
