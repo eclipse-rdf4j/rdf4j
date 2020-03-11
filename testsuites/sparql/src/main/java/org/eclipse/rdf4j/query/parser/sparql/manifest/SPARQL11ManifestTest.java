b92c8429618230590e67e3f50a1d616567840e31
Jeen Broekstra <jeen.broekstra@gmail.com>
2020-03-11 13:08:10 +1100

    GH-1978 fix handling of empty results in grouping/aggregate
    - modified and extended unit tests
    - disabled incorrect W3C conformance test per discussion on GH-1978
    - refactored testsuite setup a bit to make future extension easier
    - added two simple test cases in manifest tests

--- testsuites/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/manifest/SPARQL11ManifestTest.java
+++ testsuites/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/manifest/SPARQL11ManifestTest.java
@@ -10,16 +10,10 @@ package org.eclipse.rdf4j.query.parser.sparql.manifest;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.JarURLConnection;
 import java.net.URL;
-import java.util.jar.JarFile;
-
-import junit.framework.TestResult;
-import junit.framework.TestSuite;
 
 import org.eclipse.rdf4j.OpenRDFUtil;
 import org.eclipse.rdf4j.common.io.FileUtil;
-import org.eclipse.rdf4j.common.io.ZipUtil;
 import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.query.BindingSet;
@@ -38,6 +32,9 @@ import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
 /**
  * Functionality for creating a JUnit test suite out of a W3C Working Group-style manifest for SPARQL query and update
  * tests.
@@ -51,25 +48,79 @@ public class SPARQL11ManifestTest {
 	private static File tmpDir;
 
 	/**
-	 * Creates a new {@link TestSuite} for executiong of {@link SPARQLQueryTest} s.
+	 * Creates a new {@link TestSuite} for execution of {@link SPARQLQueryTest} s.
 	 * 
-	 * @param factory                   a factory class that creates each individual test case.
-	 * @param officialWorkingGroupTests indicates whether to use the official W3C working group tests, or Sesame's own
-	 *                                  set of tests.
-	 * @param approvedTestsOnly         if <code>true</code>, use working group-approved tests only. Has no influence
-	 *                                  when officialWorkingGroup tests is set to <code>false</code>.
-	 * @param useRemoteTests            if set to <code>true</code>, use manifests and tests located at
-	 *                                  <code>http://www.w3.org/2009/sparql/docs/tests/data-sparql11/</code> , instead
-	 *                                  of local copies.
-	 * @param excludedSubdirs           an (optionally empty) list of subdirectories to exclude from testing. If
-	 *                                  specified, test cases in one of the supplied subdirs will not be executed. If
-	 *                                  left empty, all tests will be executed.
+	 * @param factory           a factory class that creates each individual test case.
+	 * @param manifestFile      url of the manifest file (may be remote or local).
+	 * @param approvedTestsOnly if <code>true</code>, use working group-approved tests only.
+	 * @param excludedSubdirs   an (optionally empty) list of subdirectories to exclude from testing. If specified, test
+	 *                          cases in one of the supplied subdirs will not be executed. If left empty, all tests will
+	 *                          be executed.
 	 * @return a TestSuite.
 	 * @throws Exception
 	 */
-	public static TestSuite suite(SPARQLQueryTest.Factory factory, boolean officialWorkingGroupTests,
-			boolean approvedTestsOnly, boolean useRemoteTests, String... excludedSubdirs) throws Exception {
-		final String manifestFile = getManifestFile(officialWorkingGroupTests, useRemoteTests);
+	public static TestSuite suite(SPARQLQueryTest.Factory factory, String manifestFile,
+			boolean approvedTestsOnly, String... excludedSubdirs) throws Exception {
+		TestSuite suite = new TestSuite(factory.getClass().getName()) {
+
+			@Override
+			public void run(TestResult result) {
+				try {
+					super.run(result);
+				} finally {
+					if (tmpDir != null) {
+						try {
+							FileUtil.deleteDir(tmpDir);
+						} catch (IOException e) {
+							System.err.println(
+									"Unable to clean up temporary directory '" + tmpDir + "': " + e.getMessage());
+						}
+					}
+				}
+			}
+		};
+
+		Repository manifestRep = new SailRepository(new MemoryStore());
+		try (RepositoryConnection con = manifestRep.getConnection()) {
+
+			addTurtle(con, new URL(manifestFile), manifestFile);
+
+			String query = " PREFIX qt: <http://www.w3.org/2001/sw/DataAccess/tests/test-query#> "
+					+ "PREFIX mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#> "
+					+ "SELECT DISTINCT ?manifestFile "
+					+ "WHERE { [] mf:include [ rdf:rest*/rdf:first ?manifestFile ] . }   ";
+
+			TupleQueryResult manifestResults = con.prepareTupleQuery(QueryLanguage.SPARQL, query, manifestFile)
+					.evaluate();
+
+			for (BindingSet bindingSet : manifestResults) {
+				String subManifestFile = bindingSet.getValue("manifestFile").stringValue();
+
+				if (includeSubManifest(subManifestFile, excludedSubdirs)) {
+					suite.addTest(SPARQLQueryTest.suite(subManifestFile, factory, approvedTestsOnly));
+				}
+			}
+		}
+		manifestRep.shutDown();
+
+		logger.info("Created aggregated test suite with " + suite.countTestCases() + " test cases.");
+		return suite;
+	}
+
+	/**
+	 * Creates a new {@link TestSuite} for execution of {@link SPARQLUpdateConformanceTest} s.
+	 * 
+	 * @param factory           a factory class that creates each individual test case.
+	 * @param manifestFile      url of the manifest file (may be remote or local).
+	 * @param approvedTestsOnly if <code>true</code>, use working group-approved tests only.
+	 * @param excludedSubdirs   an (optionally empty) list of subdirectories to exclude from testing. If specified, test
+	 *                          cases in one of the supplied subdirs will not be executed. If left empty, all tests will
+	 *                          be executed.
+	 * @return a TestSuite.
+	 * @throws Exception
+	 */
+	public static TestSuite suite(SPARQLUpdateConformanceTest.Factory factory, String manifestFile,
+			boolean approvedTestsOnly, String... excludedSubdirs) throws Exception {
 
 		TestSuite suite = new TestSuite(factory.getClass().getName()) {
 
@@ -91,120 +142,30 @@ public class SPARQL11ManifestTest {
 		};
 
 		Repository manifestRep = new SailRepository(new MemoryStore());
-		manifestRep.initialize();
-		RepositoryConnection con = manifestRep.getConnection();
+		try (RepositoryConnection con = manifestRep.getConnection()) {
 
-		addTurtle(con, new URL(manifestFile), manifestFile);
+			addTurtle(con, new URL(manifestFile), manifestFile);
 
-		String query = " PREFIX qt: <http://www.w3.org/2001/sw/DataAccess/tests/test-query#> "
-				+ "PREFIX mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#> "
-				+ "SELECT DISTINCT ?manifestFile "
-				+ "WHERE { [] mf:include [ rdf:rest*/rdf:first ?manifestFile ] . }   ";
+			String query = " PREFIX qt: <http://www.w3.org/2001/sw/DataAccess/tests/test-query#> "
+					+ "PREFIX mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#> "
+					+ "SELECT DISTINCT ?manifestFile "
+					+ "WHERE { [] mf:include [ rdf:rest*/rdf:first ?manifestFile ] . }   ";
 
-		TupleQueryResult manifestResults = con.prepareTupleQuery(QueryLanguage.SPARQL, query, manifestFile).evaluate();
+			TupleQueryResult manifestResults = con.prepareTupleQuery(QueryLanguage.SPARQL, query, manifestFile)
+					.evaluate();
 
-		while (manifestResults.hasNext()) {
-			BindingSet bindingSet = manifestResults.next();
-			String subManifestFile = bindingSet.getValue("manifestFile").stringValue();
+			for (BindingSet bindingSet : manifestResults) {
+				String subManifestFile = bindingSet.getValue("manifestFile").stringValue();
 
-			if (includeSubManifest(subManifestFile, excludedSubdirs)) {
-				suite.addTest(SPARQLQueryTest.suite(subManifestFile, factory, approvedTestsOnly));
+				if (includeSubManifest(subManifestFile, excludedSubdirs)) {
+					suite.addTest(SPARQLUpdateConformanceTest.suite(subManifestFile, factory, approvedTestsOnly));
+				}
 			}
 		}
-
-		manifestResults.close();
-		con.close();
 		manifestRep.shutDown();
 
 		logger.info("Created aggregated test suite with " + suite.countTestCases() + " test cases.");
 		return suite;
-	}
-
-	public static TestSuite suite(SPARQLUpdateConformanceTest.Factory factory, boolean officialWorkingGroupTests,
-			boolean approvedTestsOnly, boolean useRemote, String... excludedSubdirs) throws Exception {
-		final String manifestFile = getManifestFile(officialWorkingGroupTests, useRemote);
-
-		TestSuite suite = new TestSuite(factory.getClass().getName()) {
-
-			@Override
-			public void run(TestResult result) {
-				try {
-					super.run(result);
-				} finally {
-					if (tmpDir != null) {
-						try {
-							FileUtil.deleteDir(tmpDir);
-						} catch (IOException e) {
-							System.err.println(
-									"Unable to clean up temporary directory '" + tmpDir + "': " + e.getMessage());
-						}
-					}
-				}
-			}
-		};
-
-		Repository manifestRep = new SailRepository(new MemoryStore());
-		manifestRep.initialize();
-		RepositoryConnection con = manifestRep.getConnection();
-
-		addTurtle(con, new URL(manifestFile), manifestFile);
-
-		String query = " PREFIX qt: <http://www.w3.org/2001/sw/DataAccess/tests/test-query#> "
-				+ "PREFIX mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#> "
-				+ "SELECT DISTINCT ?manifestFile "
-				+ "WHERE { [] mf:include [ rdf:rest*/rdf:first ?manifestFile ] . }   ";
-
-		TupleQueryResult manifestResults = con.prepareTupleQuery(QueryLanguage.SPARQL, query, manifestFile).evaluate();
-
-		while (manifestResults.hasNext()) {
-			BindingSet bindingSet = manifestResults.next();
-			String subManifestFile = bindingSet.getValue("manifestFile").stringValue();
-
-			if (includeSubManifest(subManifestFile, excludedSubdirs)) {
-				suite.addTest(SPARQLUpdateConformanceTest.suite(subManifestFile, factory, approvedTestsOnly));
-			}
-		}
-
-		manifestResults.close();
-		con.close();
-		manifestRep.shutDown();
-
-		logger.info("Created aggregated test suite with " + suite.countTestCases() + " test cases.");
-		return suite;
-	}
-
-	private static String getManifestFile(boolean officialWorkingGroupTests, boolean useRemote) {
-		String manifestFile = null;
-		if (useRemote) {
-			manifestFile = "http://www.w3.org/2009/sparql/docs/tests/data-sparql11/manifest-all.ttl";
-		} else {
-			URL url = null;
-			if (officialWorkingGroupTests) {
-				url = SPARQL11ManifestTest.class.getResource("/testcases-sparql-1.1-w3c/manifest-all.ttl");
-			} else {
-				url = SPARQL11ManifestTest.class.getResource("/testcases-sparql-1.1/manifest-evaluation.ttl");
-			}
-
-			if ("jar".equals(url.getProtocol())) {
-				// Extract manifest files to a temporary directory
-				try {
-					tmpDir = FileUtil.createTempDir("sparql11-test-evaluation");
-
-					JarURLConnection con = (JarURLConnection) url.openConnection();
-					JarFile jar = con.getJarFile();
-
-					ZipUtil.extract(jar, tmpDir);
-
-					File localFile = new File(tmpDir, con.getEntryName());
-					manifestFile = localFile.toURI().toURL().toString();
-				} catch (IOException e) {
-					throw new AssertionError(e);
-				}
-			} else {
-				manifestFile = url.toString();
-			}
-		}
-		return manifestFile;
 	}
 
 	/**
