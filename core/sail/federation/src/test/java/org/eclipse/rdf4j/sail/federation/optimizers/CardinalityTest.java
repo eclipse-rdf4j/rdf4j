6df8cf1400abd4d25c7d630236737032e4c980c8
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-03-17 00:08:57 +0530

    #1873 re-assigned the vars list so that it contains distinct elements, added related tests
    Signed-off-by: Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>

---
+++ core/sail/federation/src/test/java/org/eclipse/rdf4j/sail/federation/optimizers/CardinalityTest.java
@@ -0,0 +1,58 @@
+package org.eclipse.rdf4j.sail.federation.optimizers;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.algebra.StatementPattern;
+import org.eclipse.rdf4j.query.algebra.Var;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CardinalityTest {
+
+	protected static StatementPattern statement;
+	protected static double cardinality;
+	protected static EvaluationStatistics.CardinalityCalculator calculator;
+	protected static SimpleValueFactory factory;
+	protected static String namespace;
+
+	@Before
+	public void setup() {
+		factory = SimpleValueFactory.getInstance();
+		namespace = "ex:";
+		EvaluationStatistics evaluationStatistics = new EvaluationStatistics();
+		calculator = evaluationStatistics.createCardinalityCalculator();
+	}
+
+	@Test
+	public void assertDuplicateElementsCardinality() {
+		IRI subjectIRI = factory.createIRI(namespace, "Set1");
+		IRI objectIRI = factory.createIRI(namespace, "Set1");
+		Var subject = new Var("Set1", subjectIRI);
+		Var predicate = new Var("subsetOf", null);
+		Var object = new Var("Set1", objectIRI);
+
+		statement = new StatementPattern(subject, predicate, object);
+		calculator.meet(statement);
+		cardinality = calculator.getCardinality();
+
+		assertEquals(31.623, cardinality, 0.001);
+	}
+
+	@Test
+	public void assertDistinctElementsCardinality() {
+		IRI subjectIRI = factory.createIRI(namespace, "Set1");
+		IRI objectIRI = factory.createIRI(namespace, "Set2");
+		Var subject = new Var("Set1", subjectIRI);
+		Var predicate = new Var("subsetOf", null);
+		Var object = new Var("Set2", objectIRI);
+
+		statement = new StatementPattern(subject, predicate, object);
+		calculator.meet(statement);
+		cardinality = calculator.getCardinality();
+
+		assertEquals(10.0, cardinality, 0.000001);
+	}
+
+}
