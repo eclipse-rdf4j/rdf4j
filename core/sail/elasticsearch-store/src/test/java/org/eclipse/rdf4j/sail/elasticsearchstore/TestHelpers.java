7c6952defb5a4e181a74e85d7b627acd9a48fd43
Håvard Ottestad <hmottestad@gmail.com>
2020-03-18 14:04:53 +0100

    Upgrade Elasticsearch to 6.8.7
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/TestHelpers.java
+++ core/sail/elasticsearch-store/src/test/java/org/eclipse/rdf4j/sail/elasticsearchstore/TestHelpers.java
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit;
 public class TestHelpers {
 
 	private final static Random random = new Random();
-	public static final String VERSION = "6.5.4";
+	public static final String VERSION = "6.8.7";
 	public static final String CLUSTER = "cluster1";
 	public static final String ELASTICSEARCH_DOWNLOAD_DIRECTORY = "tempElasticsearchDownload";
 
