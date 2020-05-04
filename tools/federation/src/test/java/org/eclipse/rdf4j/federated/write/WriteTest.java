2e1f18427152cd9225eca3a16d7633fc065f7e18
Andreas Schwarte <aschwarte10@gmail.com>
2020-05-04 21:38:01 +0200

    GH-2167: make FedX WriteStrategy configurable
    This change makes the FedX WriteStrategy configurable (e.g. for
    application developers who want to define a different implementation).
    
    Default behavior remains as before.
    
    Functionality is covered with a unit test.
    
    Signed-off-by: Andreas Schwarte <aschwarte10@gmail.com>

--- tools/federation/src/test/java/org/eclipse/rdf4j/federated/write/WriteTest.java
+++ tools/federation/src/test/java/org/eclipse/rdf4j/federated/write/WriteTest.java
@@ -10,20 +10,26 @@ package org.eclipse.rdf4j.federated.write;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.eclipse.rdf4j.common.iteration.Iterations;
+import org.eclipse.rdf4j.federated.FederationContext;
 import org.eclipse.rdf4j.federated.SPARQLBaseTest;
 import org.eclipse.rdf4j.federated.endpoint.Endpoint;
 import org.eclipse.rdf4j.federated.endpoint.EndpointBase;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.vocabulary.FOAF;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.query.Update;
+import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -161,9 +167,61 @@ public class WriteTest extends SPARQLBaseTest {
 		}
 	}
 
+	@Test
+	public void testCustomWriteStrategy() throws Exception {
+
+		prepareTest(Arrays.asList("/tests/basic/data_emptyStore.ttl", "/tests/basic/data_emptyStore.ttl"));
+
+		// configure the test write strategy factory
+		fedxRule.getFederationContext().getConfig().withWriteStrategyFactory(TestWriteStrategyFactory.class);
+
+		try (RepositoryConnection conn = fedxRule.getRepository().getConnection()) {
+			Update update = conn.prepareUpdate(QueryLanguage.SPARQL,
+					"PREFIX : <http://example.org/> INSERT { :subject a :Person } WHERE { }");
+			update.execute();
+
+			// test that statement is returned from federation
+			List<Statement> stmts = Iterations.asList(conn.getStatements(null, null, null, true));
+			Assertions.assertEquals(1, stmts.size());
+			Assertions.assertEquals(RDF.TYPE, stmts.get(0).getPredicate());
+
+			Assertions.assertEquals(1, writeOperations.get());
+		}
+	}
+
 	protected Statement simpleStatement() {
 		ValueFactory vf = SimpleValueFactory.getInstance();
 		IRI subject = vf.createIRI("http://example.org/person1");
 		return vf.createStatement(subject, RDF.TYPE, FOAF.PERSON);
+	}
+
+	@BeforeEach
+	public void clearWriteOperations() throws Exception {
+		writeOperations.set(0);
+	}
+
+	// write operations done
+	static final AtomicInteger writeOperations = new AtomicInteger(0);
+
+	public static class TestWriteStrategyFactory implements WriteStrategyFactory {
+
+		@Override
+		public WriteStrategy create(List<Endpoint> members, FederationContext federationContext) {
+			return new TestWriteStrategy(members.get(0).getRepository());
+		}
+	}
+
+	static class TestWriteStrategy extends RepositoryWriteStrategy {
+
+		public TestWriteStrategy(Repository writeRepository) {
+			super(writeRepository);
+		}
+
+		@Override
+		public void addStatement(Resource subj, IRI pred, Value obj, Resource... contexts) throws RepositoryException {
+			writeOperations.incrementAndGet();
+			super.addStatement(subj, pred, obj, contexts);
+		}
+
 	}
 }
