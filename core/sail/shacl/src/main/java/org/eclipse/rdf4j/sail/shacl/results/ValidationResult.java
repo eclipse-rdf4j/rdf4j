8de83c9d4dabc35d2bf830f430ee1007889533bc
vtanwer14 <vikas16114@iiitd.ac.in>
2020-05-29 22:03:28 +0530

    Add files via upload

--- core/sail/shacl/src/main/java/org/eclipse/rdf4j/sail/shacl/results/ValidationResult.java
+++ core/sail/shacl/src/main/java/org/eclipse/rdf4j/sail/shacl/results/ValidationResult.java
@@ -10,18 +10,20 @@ package org.eclipse.rdf4j.sail.shacl.results;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.model.vocabulary.SHACL;
+import org.eclipse.rdf4j.sail.shacl.SourceConstraintComponent;
 import org.eclipse.rdf4j.sail.shacl.AST.Path;
 import org.eclipse.rdf4j.sail.shacl.AST.PathPropertyShape;
 import org.eclipse.rdf4j.sail.shacl.AST.PropertyShape;
 import org.eclipse.rdf4j.sail.shacl.AST.SimplePath;
-import org.eclipse.rdf4j.sail.shacl.SourceConstraintComponent;
 
 /**
  * The ValidationResult represents the results from a SHACL validation in an easy-to-use Java API.
@@ -39,11 +41,15 @@ public class ValidationResult {
 	private Path path;
 	private ValidationResult detail;
 	private Value focusNode;
+	private Value Expected;
+	private Value Actual = SimpleValueFactory.getInstance().createLiteral("null");
+	private Value ActualFormat = SimpleValueFactory.getInstance().createLiteral("null");
 
 	public ValidationResult(PropertyShape sourceShape, Value focusNode) {
 		this.sourceShape = sourceShape;
 		this.focusNode = focusNode;
 		this.sourceConstraintComponent = sourceShape.getSourceConstraintComponent();
+		this.Expected = sourceShape.getEXP();
 		if (sourceShape instanceof PathPropertyShape) {
 			this.path = ((PathPropertyShape) sourceShape).getPath();
 		}
@@ -85,6 +91,9 @@ public class ValidationResult {
 		model.add(getId(), SHACL.FOCUS_NODE, getFocusNode());
 		model.add(getId(), SHACL.SOURCE_CONSTRAINT_COMPONENT, getSourceConstraintComponent().getIri());
 		model.add(getId(), SHACL.SOURCE_SHAPE, getSourceShapeResource());
+		model.add(getId(), SHACL.ACTUAL, getAct());
+		model.add(getId(), SHACL.ACTUALFORMAT, getActFor());
+		model.add(getId(), SHACL.EXPECTED, getExp());
 
 		if (getPath() != null) {
 			model.add(getId(), SHACL.RESULT_PATH, ((SimplePath) getPath()).getPath());
@@ -121,6 +130,58 @@ public class ValidationResult {
 
 	public Resource getId() {
 		return id;
+	}
+
+	private Value getAct() {
+		return Actual;
+	}
+
+	private Value getActFor() {
+		return ActualFormat;
+	}
+
+	public void SetAct(Value Ac) {
+		if (!(Ac instanceof Literal)) {
+			return;
+		}
+		this.Actual = Ac;
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.MinCountConstraintComponent)) {
+			this.ActualFormat = Ac;
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.DatatypeConstraintComponent)) {
+			this.ActualFormat = ((Literal) Ac).getDatatype();
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.LanguageInConstraintComponent)) {
+			Optional<String> x = ((Literal) Ac).getLanguage();
+			if (x.isPresent()) {
+				this.ActualFormat = SimpleValueFactory.getInstance().createLiteral(x.get());
+			}
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.MaxCountConstraintComponent)) {
+			this.ActualFormat = Ac;
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.MaxLengthConstraintComponent)) {
+			this.ActualFormat = SimpleValueFactory.getInstance()
+					.createLiteral((long) ((Literal) Ac).stringValue().length());
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.MinLengthConstraintComponent)) {
+			this.ActualFormat = SimpleValueFactory.getInstance()
+					.createLiteral((long) ((Literal) Ac).stringValue().length());
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.NodeKindConstraintComponent)) {
+			this.ActualFormat = SimpleValueFactory.getInstance().createLiteral(Ac.getClass().getSimpleName());
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.PatternConstraintComponent)) {
+			this.ActualFormat = Ac;
+		}
+		if (this.sourceConstraintComponent.equals(SourceConstraintComponent.MinExclusiveConstraintComponent)) {
+			this.ActualFormat = Ac;
+		}
+
+	}
+
+	private Value getExp() {
+		return Expected;
 	}
 
 	/**
