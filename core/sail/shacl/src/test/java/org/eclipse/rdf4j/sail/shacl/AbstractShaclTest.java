52d87b2083627da956711f79d654806803e8e93c
Håvard Ottestad <hmottestad@gmail.com>
2020-09-26 15:36:18 +0200

    made more tests to show a bug
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/sail/shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/AbstractShaclTest.java
+++ core/sail/shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/AbstractShaclTest.java
@@ -80,7 +80,7 @@ abstract public class AbstractShaclTest {
 	// formatter doesn't understand that the trailing ) needs to be on a new line.
 	private static final List<String> testCasePaths = Stream.of(
 
-		"test-cases/and-or/datatypeNodeShape",
+/*		"test-cases/and-or/datatypeNodeShape",
 //		"test-cases/class/allObjects",
 //		"test-cases/class/allSubjects",
 		"test-cases/class/and",
@@ -215,8 +215,10 @@ abstract public class AbstractShaclTest {
 //		"test-cases/hasValue/targetShapeAndOr",
 //		"test-cases/hasValue/targetShapeAndOr2",
 //		"test-cases/hasValueIn/targetShapeOr",
-//		"test-cases/hasValueIn/or"
-		)
+//		"test-cases/hasValueIn/or"*/
+				"test-cases/class/simpleNested"
+
+	)
 		.distinct()
 		.sorted()
 		.collect(Collectors.toList());
