9bddd2e0c9858d84b23d65e21d9fe4b052f0363a
knoan <22@metreeca.com>
2020-09-28 10:32:25 +0200

    GH-2538 Add default implementation for Namespace.compareTo()
    Signed-off-by: Alessandro Bollini <22@metreeca.com>

--- core/model/src/main/java/org/eclipse/rdf4j/model/Namespace.java
+++ core/model/src/main/java/org/eclipse/rdf4j/model/Namespace.java
@@ -8,11 +8,30 @@
 package org.eclipse.rdf4j.model;
 
 import java.io.Serializable;
+import java.util.Comparator;
 
 /**
  * A namespace, consisting of a namespace name and a prefix that has been assigned to it.
  */
 public interface Namespace extends Serializable, Comparable<Namespace> {
+
+	/**
+	 * The default comparator for namespaces.
+	 *
+	 * <p>Sorts namespaces first by {@linkplain #getPrefix() prefix} and then by {@linkplain #getName()} () name};
+	 * {@code null} values are sorted before other values.</p>
+	 */
+	static final Comparator<Namespace> COMPARATOR=Comparator.nullsFirst(
+			Comparator.comparing(Namespace::getPrefix).thenComparing(Namespace::getName)
+	);
+
+
+	/**
+	 * Gets the prefix of the current namespace. The default namespace is represented by an empty prefix string.
+	 *
+	 * @return prefix of namespace, or an empty string in case of the default namespace.
+	 */
+	public String getPrefix();
 
 	/**
 	 * Gets the name of the current namespace (i.e. its IRI).
@@ -21,10 +40,16 @@ public interface Namespace extends Serializable, Comparable<Namespace> {
 	 */
 	public String getName();
 
+
 	/**
-	 * Gets the prefix of the current namespace. The default namespace is represented by an empty prefix string.
+	 * @inheritDoc
 	 *
-	 * @return prefix of namespace, or an empty string in case of the default namespace.
+	 * <p>The default implementation compares this namespace with the reference namespace using the
+	 * {@linkplain #COMPARATOR default namespace comparator}.</p>
 	 */
-	public String getPrefix();
+	@Override
+	default int compareTo(Namespace o) {
+		return COMPARATOR.compare(this, o);
+	}
+
 }
