736c164383a37f390554c486a6e501fd48a4d750
Håvard Ottestad <hmottestad@gmail.com>
2020-09-30 14:33:24 +0200

    reduce allowed memory for benchmark
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/benchmark/QueryBenchmark.java
+++ core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/benchmark/QueryBenchmark.java
@@ -48,7 +48,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 @Warmup(iterations = 20)
 @BenchmarkMode({ Mode.AverageTime })
-@Fork(value = 1, jvmArgs = { "-Xms8G", "-Xmx8G", "-Xmn4G", "-XX:+UseSerialGC" })
+@Fork(value = 1, jvmArgs = { "-Xms512M", "-Xmx512M" })
 //@Fork(value = 1, jvmArgs = {"-Xms8G", "-Xmx8G", "-Xmn4G", "-XX:+UseSerialGC", "-XX:+UnlockCommercialFeatures", "-XX:StartFlightRecording=delay=60s,duration=120s,filename=recording.jfr,settings=profile", "-XX:FlightRecorderOptions=samplethreads=true,stackdepth=1024", "-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints"})
 @Measurement(iterations = 10)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -99,9 +99,7 @@ public class QueryBenchmark {
 
 			statementList = Iterations.asList(connection.getStatements(null, RDF.TYPE, null, false));
 		}
-
-		System.gc();
-
+		
 	}
 
 	private static InputStream getResourceAsStream(String name) {
