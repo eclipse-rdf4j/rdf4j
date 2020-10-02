2839267dae7c88a6876e38f9dc4ac74ffde24194
Alessandro Bollini <22@metreeca.com>
2020-10-02 08:48:46 +0200

    GH-2538 Revert interface-level equals/hashCode contract for Triple
    Signed-off-by: Alessandro Bollini <22@metreeca.com>

--- core/model-api/src/main/java/org/eclipse/rdf4j/model/Triple.java
+++ core/model-api/src/main/java/org/eclipse/rdf4j/model/Triple.java
@@ -15,10 +15,7 @@ import org.eclipse.rdf4j.common.annotation.Experimental;
  * <p>
  * Additional utility functionality for working with {@code Triple} objects is available in the
  * {@code org.eclipse.rdf4j.model.util.Statements} utility class.
- * <p>
- * <strong>Warning</strong> / In order to ensure interoperability of concrete classes implementing this interface,
- * {@link #equals(Object)} and {@link #hashCode()} methods must be implemented exactly as described in their specs.
- * 
+ *
  * @author Pavel Mihaylov
  */
 @Experimental
@@ -49,26 +46,5 @@ public interface Triple extends Resource {
 	 * @return The triple's object.
 	 */
 	Value getObject();
-
-	/**
-	 * Compares this triple to another object.
-	 *
-	 * @param other the object to compare this triple to
-	 *
-	 * @return {@code true} if the other object is an instance of {@code Triple} and if their {@linkplain #getSubject()
-	 *         subjects}, {@linkplain #getPredicate() predicates} and {@linkplain #getObject() objects; {@code false}
-	 *         otherwise
-	 */
-	@Override
-	public boolean equals(Object other);
-
-	/**
-	 * Computes the hash code of this triple.
-	 *
-	 * @return a hash code for this triple computed as {@link java.util.Objects#hash Objects.hash}(
-	 *         {@link #getSubject()}, {@link #getPredicate()}, {@link #getObject()})
-	 */
-	@Override
-	public int hashCode();
 
 }
