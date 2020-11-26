393e9524277342f5ce781fcff15c847846bfd4db
Jerven bolleman <jerven.bolleman@sib.swiss>
2020-11-26 10:49:45 +0100

    eclipseGH-2676 writer(string) method to give a chance to the encoder to avoid needing to encode latin1 strings
    Signed-off-by: Jerven bolleman <jerven.bolleman@sib.swiss>

--- core/rio/turtle/src/main/java/org/eclipse/rdf4j/rio/turtle/TurtleWriter.java
+++ core/rio/turtle/src/main/java/org/eclipse/rdf4j/rio/turtle/TurtleWriter.java
@@ -7,6 +7,7 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.rio.turtle;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -122,7 +123,9 @@ public class TurtleWriter extends AbstractRDFWriter implements RDFWriter {
 	public TurtleWriter(OutputStream out, ParsedIRI baseIRI) {
 		super(out);
 		this.baseIRI = baseIRI;
-		this.writer = new IndentingWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+		// The BufferedWriter is here to avoid to many calls to the CharEncoder
+		// see javadoc of OutputStreamWriter.
+		this.writer = new IndentingWriter(new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8)));
 	}
 
 	/**
