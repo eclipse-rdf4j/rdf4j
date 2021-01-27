06da6b82374fd164502652d0408ccaf0d1d723a9
Håvard Ottestad <hmottestad@gmail.com>
2021-01-27 11:35:05 +0100

    release notes for 3.5.1
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- release.sh
+++ release.sh
@@ -220,7 +220,7 @@ gh pr create -B develop --title "sync develop branch after release ${MVN_VERSION
 echo "It's ok to merge this PR later, so wait for the Jenkins tests to finish."
 read -n 1 -s -r -p "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
-git checkout $MVN_VERSION_RELEASE
+git checkout master
 
 echo "DONE!"
 
@@ -240,8 +240,6 @@ echo " - Go to https://github.com/eclipse/rdf4j/tree/master/site/content/release
 echo " - Edit the following file https://github.com/eclipse/rdf4j/blob/master/site/content/download.md"
 echo " - Go to https://github.com/eclipse/rdf4j/tree/master/site/content/news and create rdf4j-${NEWS_FILE_NAME}.md"
 echo " - Go to https://github.com/eclipse/rdf4j/releases/new and create a release for the ${MVN_VERSION_RELEASE} tag. Add a link to the release notes in the description."
-echo " - Post to Google Groups: https://groups.google.com/forum/#!forum/rdf4j-users"
-echo "     - Good example: https://groups.google.com/forum/#!topic/rdf4j-users/isrC7qdhplY"
 echo " - Upload the javadocs by adding a compressed tar.gz archive called ${MVN_VERSION_RELEASE}.tgz to site/static/javadoc/"
 echo "     - Aggregated javadoc can be found in target/site/apidocs or in the SDK zip file"
 echo "     - Make sure to also replace the site/static/javadoc/latest file with a copy. Do not use a symlink."
