ff190d5e932acb6f23139f62c7f02c26eff0fc8c
Håvard Ottestad <hmottestad@gmail.com>
2021-02-20 18:09:02 +0100

    GH-2846 introduce new transaction settings to allow users to specify if validation should be parallel or not, or cache should be used or not
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

---
+++ core/sail/shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/TransactionSettingsTest.java
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+
+package org.eclipse.rdf4j.sail.shacl;
+
+import org.eclipse.rdf4j.IsolationLevels;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+import org.eclipse.rdf4j.sail.NotifyingSailConnection;
+import org.eclipse.rdf4j.sail.SailConnection;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.junit.Test;
+
+public class TransactionSettingsTest {
+
+	@Test
+	public void testBulk() {
+		ShaclSail shaclSail = new ShaclSail(new MemoryStore());
+		shaclSail.setParallelValidation(true);
+		shaclSail.setCacheSelectNodes(true);
+
+		SailRepository sailRepository = new SailRepository(shaclSail);
+		try (SailRepositoryConnection connection = sailRepository.getConnection()) {
+
+			connection.begin(ShaclSail.TransactionSettings.ValidationApproach.Bulk);
+
+			ShaclSailConnection sailConnection = (ShaclSailConnection) connection.getSailConnection();
+			ShaclSailConnection.Settings transactionSettings = sailConnection.getTransactionSettings();
+
+			assert transactionSettings.getValidationApproach() == ShaclSail.TransactionSettings.ValidationApproach.Bulk;
+			assert !transactionSettings.isCacheSelectNodes();
+			assert !transactionSettings.isParallelValidation();
+
+			connection.commit();
+
+		}
+
+		sailRepository.shutDown();
+
+	}
+
+	@Test
+	public void testBulkParallel() {
+		ShaclSail shaclSail = new ShaclSail(new MemoryStore());
+		shaclSail.setParallelValidation(true);
+		shaclSail.setCacheSelectNodes(true);
+
+		SailRepository sailRepository = new SailRepository(shaclSail);
+		try (SailRepositoryConnection connection = sailRepository.getConnection()) {
+
+			connection.begin(ShaclSail.TransactionSettings.ValidationApproach.Bulk,
+					ShaclSail.TransactionSettings.PerformanceHint.ParallelValidation);
+
+			ShaclSailConnection sailConnection = (ShaclSailConnection) connection.getSailConnection();
+			ShaclSailConnection.Settings transactionSettings = sailConnection.getTransactionSettings();
+
+			assert transactionSettings.getValidationApproach() == ShaclSail.TransactionSettings.ValidationApproach.Bulk;
+			assert !transactionSettings.isCacheSelectNodes();
+			assert transactionSettings.isParallelValidation();
+
+			connection.commit();
+
+		}
+
+		sailRepository.shutDown();
+
+	}
+
+	@Test
+	public void testBulkParallelCache() {
+		ShaclSail shaclSail = new ShaclSail(new MemoryStore());
+		shaclSail.setParallelValidation(true);
+		shaclSail.setCacheSelectNodes(true);
+
+		SailRepository sailRepository = new SailRepository(shaclSail);
+		try (SailRepositoryConnection connection = sailRepository.getConnection()) {
+
+			connection.begin(ShaclSail.TransactionSettings.ValidationApproach.Bulk,
+					ShaclSail.TransactionSettings.PerformanceHint.ParallelValidation,
+					ShaclSail.TransactionSettings.PerformanceHint.CacheEnabled);
+
+			ShaclSailConnection sailConnection = (ShaclSailConnection) connection.getSailConnection();
+			ShaclSailConnection.Settings transactionSettings = sailConnection.getTransactionSettings();
+
+			assert transactionSettings.getValidationApproach() == ShaclSail.TransactionSettings.ValidationApproach.Bulk;
+			assert transactionSettings.isCacheSelectNodes();
+			assert transactionSettings.isParallelValidation();
+
+			connection.commit();
+
+		}
+
+		sailRepository.shutDown();
+
+	}
+
+	@Test
+	public void testDefault() {
+		ShaclSail shaclSail = new ShaclSail(new MemoryStore());
+		shaclSail.setParallelValidation(true);
+		shaclSail.setCacheSelectNodes(true);
+
+		SailRepository sailRepository = new SailRepository(shaclSail);
+		try (SailRepositoryConnection connection = sailRepository.getConnection()) {
+
+			connection.begin();
+
+			ShaclSailConnection sailConnection = (ShaclSailConnection) connection.getSailConnection();
+			ShaclSailConnection.Settings transactionSettings = sailConnection.getTransactionSettings();
+
+			assert transactionSettings.getValidationApproach() == ShaclSail.TransactionSettings.ValidationApproach.Auto;
+			assert transactionSettings.isCacheSelectNodes();
+			assert transactionSettings.isParallelValidation();
+
+			connection.commit();
+
+		}
+
+		sailRepository.shutDown();
+
+	}
+
+	@Test
+	public void testNulls() {
+		ShaclSail shaclSail = new ShaclSail(new MemoryStore());
+
+		SailRepository sailRepository = new SailRepository(shaclSail);
+		try (SailRepositoryConnection connection = sailRepository.getConnection()) {
+
+			connection.begin();
+
+			ShaclSailConnection sailConnection = (ShaclSailConnection) connection.getSailConnection();
+			ShaclSailConnection.Settings transactionSettings = sailConnection.getTransactionSettings();
+
+			assert transactionSettings.getValidationApproach() != null;
+			assert transactionSettings.isCacheSelectNodes();
+			assert transactionSettings.isParallelValidation();
+
+			connection.commit();
+
+		}
+
+		sailRepository.shutDown();
+
+	}
+
+	@Test
+	public void testDefaultOverride() {
+		ShaclSail shaclSail = new ShaclSail(new MemoryStore());
+		shaclSail.setParallelValidation(true);
+		shaclSail.setCacheSelectNodes(true);
+
+		SailRepository sailRepository = new SailRepository(shaclSail);
+		try (SailRepositoryConnection connection = sailRepository.getConnection()) {
+
+			connection.begin(ShaclSail.TransactionSettings.PerformanceHint.CacheDisabled,
+					ShaclSail.TransactionSettings.PerformanceHint.SerialValidation);
+
+			ShaclSailConnection sailConnection = (ShaclSailConnection) connection.getSailConnection();
+			ShaclSailConnection.Settings transactionSettings = sailConnection.getTransactionSettings();
+
+			assert transactionSettings.getValidationApproach() == ShaclSail.TransactionSettings.ValidationApproach.Auto;
+			assert !transactionSettings.isCacheSelectNodes();
+			assert !transactionSettings.isParallelValidation();
+
+			connection.commit();
+
+		}
+
+		sailRepository.shutDown();
+
+	}
+
+}
