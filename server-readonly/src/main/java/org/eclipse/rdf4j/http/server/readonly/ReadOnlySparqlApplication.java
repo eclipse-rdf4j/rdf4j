63ffe678b32d801b65d81c1d9bcce7b5148e4cfd
Jerven bolleman <jerven.bolleman@sib.swiss>
2021-02-22 11:43:18 +0100

    GH-2904 spring-boot based read-only sparql endpoint
    This introduces a minimal Spring boot based sparql endpoint that
    has no functionality other than implementing the sparql (read) protocol.
    
    There is a controller that does the response logic and
    an application that can be extended for tests.
    
    It should be in the tools directory however, the spring-boot
    conflicts with the existing spring-mvc usage.
    
    Also we use Jetty as tomcat tends to not like '{}|' in urls,
    which browsers per HTML5 spec don't escape.
    
    Signed-off-by: Jerven Bolleman <jerven.bolleman@sib.swiss>

---
+++ server-readonly/src/main/java/org/eclipse/rdf4j/http/server/readonly/ReadOnlySparqlApplication.java
@@ -0,0 +1,11 @@
+package org.eclipse.rdf4j.http.server.readonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadOnlySparqlApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReadOnlySparqlApplication.class, args);
+	}
+}
