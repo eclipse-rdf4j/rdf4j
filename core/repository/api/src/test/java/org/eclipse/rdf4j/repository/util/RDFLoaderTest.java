a68bbc65d0f802379b3593e3758fb8c5f3777737
Jeen Broekstra <jeen@fastmail.com>
2021-03-09 14:24:07 +1100

    GH-2828 rig https redirect stubbing for Wiremock

--- core/repository/api/src/test/java/org/eclipse/rdf4j/repository/util/RDFLoaderTest.java
+++ core/repository/api/src/test/java/org/eclipse/rdf4j/repository/util/RDFLoaderTest.java
@@ -9,13 +9,25 @@ package org.eclipse.rdf4j.repository.util;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.seeOther;
+import static com.github.tomakehurst.wiremock.client.WireMock.permanentRedirect;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.vocabulary.FOAF;
@@ -64,27 +76,89 @@ public class RDFLoaderTest {
 
 	@Test
 	public void testNonInformationResource() throws Exception {
-		stubFor(get("/Socrates")
-				.willReturn(seeOther("/Socrates.ttl")));
-		stubFor(get("/Socrates.ttl")
-				.willReturn(aResponse()
-						.withStatus(200)
-						.withHeader("Content-Type", RDFFormat.TURTLE.getDefaultMIMEType())
-						.withBody("<http://example.org/Socrates> a <http://xmlns.com/foaf/0.1/Person> .")));
+		final SSLSocketFactory toRestoreSocketFactory = disableSSLCertificatCheck();
+		try {
+			final HostnameVerifier toRestoreHostnameVerifier = disableHostnameVerifier();
+			try {
+				stubFor(get("/Socrates")
+						.willReturn(
+								permanentRedirect("https://localhost:" + wireMockRule.httpsPort() + "/Socrates.ttl")));
 
-		RDFLoader rdfLoader = new RDFLoader(new ParserConfig(), SimpleValueFactory.getInstance());
+				stubFor(get("/Socrates.ttl")
+						.willReturn(aResponse()
+								.withStatus(200)
+								.withHeader("Content-Type", RDFFormat.TURTLE.getDefaultMIMEType())
+								.withBody("<http://example.org/Socrates> a <http://xmlns.com/foaf/0.1/Person> .")));
 
-		RDFHandler rdfHandler = mock(RDFHandler.class);
+				RDFLoader rdfLoader = new RDFLoader(new ParserConfig(), SimpleValueFactory.getInstance());
 
-		rdfLoader.load(new URL("http://localhost:" + wireMockRule.port() + "/Socrates"), null, null, rdfHandler);
+				RDFHandler rdfHandler = mock(RDFHandler.class);
 
-		verify(rdfHandler).startRDF();
-		verify(rdfHandler)
-				.handleStatement(SimpleValueFactory.getInstance()
-						.createStatement(SimpleValueFactory.getInstance().createIRI("http://example.org/Socrates"),
-								RDF.TYPE,
-								FOAF.PERSON));
-		verify(rdfHandler).endRDF();
+				rdfLoader.load(new URL("http://localhost:" + wireMockRule.port() + "/Socrates"), null, null,
+						rdfHandler);
+
+				verify(rdfHandler).startRDF();
+				verify(rdfHandler)
+						.handleStatement(SimpleValueFactory.getInstance()
+								.createStatement(
+										SimpleValueFactory.getInstance().createIRI("http://example.org/Socrates"),
+										RDF.TYPE,
+										FOAF.PERSON));
+				verify(rdfHandler).endRDF();
+			} finally {
+				restoreHostnameVerifier(toRestoreHostnameVerifier);
+			}
+		} finally {
+			restoreSocketFactory(toRestoreSocketFactory);
+		}
 	}
 
+	private static HostnameVerifier disableHostnameVerifier() {
+		HostnameVerifier replaced = HttpsURLConnection.getDefaultHostnameVerifier();
+		// set a hostname verifier that just returns true for every request
+		HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		});
+
+		return replaced;
+	}
+
+	private static SSLSocketFactory disableSSLCertificatCheck()
+			throws KeyManagementException, NoSuchAlgorithmException {
+
+		// set a trust manager that just returns true for every request (this is _very_ unsafe and should only be used
+		// in the test environment)
+		TrustManager trustManager = new X509TrustManager() {
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+				// do nothing, accept all clients
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+				// do nothing accept all servers
+			}
+
+			@Override
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+		};
+
+		final SSLContext sslContext = SSLContext.getInstance("SSL");
+		sslContext.init(null, new TrustManager[] { trustManager }, SecureRandom.getInstanceStrong());
+
+		SSLSocketFactory replaced = HttpsURLConnection.getDefaultSSLSocketFactory();
+		HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+		return replaced;
+	}
+
+	private static void restoreHostnameVerifier(HostnameVerifier toRestore) {
+		HttpsURLConnection.setDefaultHostnameVerifier(toRestore);
+	}
+
+	private static void restoreSocketFactory(SSLSocketFactory toRestore) {
+		HttpsURLConnection.setDefaultSSLSocketFactory(toRestore);
+	}
 }
