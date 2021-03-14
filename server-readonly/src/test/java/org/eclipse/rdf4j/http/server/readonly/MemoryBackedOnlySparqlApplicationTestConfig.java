1c4bb669b32f3aa8f27bf26fba77466fbb0f88a4
Jerven Bolleman <jerven.bolleman@sib.swiss>
2021-03-14 10:07:41 +0100

    GH-2904 add missing license headers
    Signed-off-by: Jerven Bolleman <jerven.bolleman@sib.swiss>

--- server-readonly/src/test/java/org/eclipse/rdf4j/http/server/readonly/MemoryBackedOnlySparqlApplicationTestConfig.java
+++ server-readonly/src/test/java/org/eclipse/rdf4j/http/server/readonly/MemoryBackedOnlySparqlApplicationTestConfig.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+
 package org.eclipse.rdf4j.http.server.readonly;
 
 import org.eclipse.rdf4j.model.vocabulary.RDF;
@@ -10,7 +18,7 @@ import org.springframework.context.annotation.Bean;
 @TestConfiguration
 public class MemoryBackedOnlySparqlApplicationTestConfig {
 
-	@Bean
+	@Bean(destroy="shutDown")
 	public Repository getTestRepository() {
 		SailRepository sailRepository = new SailRepository(new MemoryStore());
 		sailRepository.init();
