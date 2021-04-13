0b41d60379c845090fa0f376d290e097890c93fa
tokovach <t.kovachev1996@gmail.com>
2021-04-13 12:28:04 +0300

    GH-2993 Introduced JMH Benchmarks for main parsers
    Signed-off-by: Tomas Kovachev <t.kovachev1996@gmail.com>

---
+++ testsuites/benchmark/src/main/java/org/eclipse/rdf4j/benchmark/rio/impl/TurtleParserBenchmark.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+
+package org.eclipse.rdf4j.benchmark.rio.impl;
+
+import org.eclipse.rdf4j.benchmark.rio.ParserBenchmark;
+import org.eclipse.rdf4j.rio.RDFParser;
+import org.eclipse.rdf4j.rio.turtle.TurtleParser;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * @author Tomas Kovachev t.kovachev1996@gmail.com
+ */
+public class TurtleParserBenchmark extends ParserBenchmark {
+
+	@Override
+	public RDFParser getParser() {
+		return new TurtleParser();
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(TurtleParserBenchmark.class.getSimpleName())
+				.build();
+		new Runner(opt).run();
+	}
+}
