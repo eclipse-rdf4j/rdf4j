676e30435c7b16289a503a3a334484dd99809722
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2021-11-11 20:15:33 +0100

    GH-3418 Added custom repository test to show that issue #3418 is fixed.
    GH-3418 reformatted changed code.

--- spring-components/spring-boot-sparql-web/src/test/java/org/eclipse/rdf4j/http/server/readonly/MemoryBackedOnlySparqlApplicationTest.java
+++ spring-components/spring-boot-sparql-web/src/test/java/org/eclipse/rdf4j/http/server/readonly/MemoryBackedOnlySparqlApplicationTest.java
@@ -8,7 +8,19 @@
 package org.eclipse.rdf4j.http.server.readonly;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.IOException;
+
+import org.eclipse.rdf4j.http.client.SPARQLProtocolSession;
+import org.eclipse.rdf4j.http.protocol.UnauthorizedException;
+import org.eclipse.rdf4j.query.MalformedQueryException;
+import org.eclipse.rdf4j.query.QueryInterruptedException;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.query.parser.ParsedQuery;
+import org.eclipse.rdf4j.query.parser.sparql.SPARQLParser;
+import org.eclipse.rdf4j.repository.RepositoryException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,6 +57,25 @@ public class MemoryBackedOnlySparqlApplicationTest {
 		String forObject = this.restTemplate.getForObject("http://localhost:" + port + "/sparql/?query={query}",
 				String.class, "SELECT * WHERE { ?s ?p ?o }");
 		assertThat(forObject).contains("http://www.w3.org/1999/02/22-rdf-syntax-ns#Bag");
+	}
+
+	@Test
+	public void testSPARQLRepository() throws UnauthorizedException, QueryInterruptedException, RepositoryException,
+			MalformedQueryException, IOException {
+		TestSPARQLRepository rep = new TestSPARQLRepository("http://localhost:" + port + "/sparql/");
+		rep.init();
+		String query = "SELECT * WHERE { ?s ?p ?o }";
+		ParsedQuery parseQuery = new SPARQLParser().parseQuery(query, null);
+		SPARQLProtocolSession session = rep.createSPARQLProtocolSession();
+		try {
+			TupleQueryResult sendTupleQuery = session.sendTupleQuery(QueryLanguage.SPARQL, query, null, false);
+			while (sendTupleQuery.hasNext()) {
+				assertNotNull(sendTupleQuery.next());
+			}
+		} finally {
+			session.close();
+			rep.shutDown();
+		}
 	}
 
 }
