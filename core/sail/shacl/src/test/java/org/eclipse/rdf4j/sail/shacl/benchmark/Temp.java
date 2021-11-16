7471823ec98282b8cc71ba7c42839953932b7afe
Håvard Ottestad <hmottestad@gmail.com>
2021-11-16 08:30:27 +0100

    wip
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- core/sail/shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/benchmark/Temp.java
+++ core/sail/shacl/src/test/java/org/eclipse/rdf4j/sail/shacl/benchmark/Temp.java
@@ -4,7 +4,7 @@ public class Temp {
 
 	public static void main(String[] args) {
 		ComplexLargeBenchmark complexLargeBenchmark = new ComplexLargeBenchmark();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 50; i++) {
 			System.out.println(i);
 			complexLargeBenchmark.noPreloadingBulkParallelCached();
 		}
