1fe70aba9acf2dc1de194165997b94ca7ae59e4d
Jeen Broekstra <jeen@fastmail.com>
2021-12-29 12:04:46 +1300

    GH-3528 switch Console CLI to use FedX

--- tools/console/src/main/java/org/eclipse/rdf4j/console/command/Federate.java
+++ tools/console/src/main/java/org/eclipse/rdf4j/console/command/Federate.java
@@ -17,10 +17,12 @@ import java.util.LinkedList;
 import org.eclipse.rdf4j.common.exception.RDF4JException;
 import org.eclipse.rdf4j.console.ConsoleIO;
 import org.eclipse.rdf4j.console.ConsoleState;
+import org.eclipse.rdf4j.federated.repository.FedXRepositoryConfig;
+import org.eclipse.rdf4j.federated.repository.FedXRepositoryConfigBuilder;
 import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.config.RepositoryConfig;
 import org.eclipse.rdf4j.repository.config.RepositoryConfigException;
 import org.eclipse.rdf4j.repository.manager.RepositoryManager;
-import org.eclipse.rdf4j.runtime.RepositoryManagerFederator;
 
 /**
  * Implements the 'federate' command for the RDF4J Console.
@@ -41,11 +43,7 @@ public class Federate extends ConsoleCommand {
 	@Override
 	public String getHelpLong() {
 		return PrintHelp.USAGE
-				+ "federate [distinct=<true|false>] [readonly=<true|false>] <fedID> <repoID_1> <repoID_2> [<repoID_n>]*\n"
-				+ "  [distinct=<true|false>]  If true, uses a DISTINCT filter that suppresses duplicate results for identical quads\n"
-				+ "                           from different federation members. Default is false.\n"
-				+ "  [readonly=<true|false>]  If true, sets the fedearated repository as read-only. If any member is read-only, then\n"
-				+ "                           this may only be set to true. Default is true. \n"
+				+ "federate  <fedID> <repoID_1> <repoID_2> [<repoID_n>]*\n"
 				+ "  <fedId>                  The id to assign the federated repository.\n"
 				+ "  <repoID1> <repoID2>      The id's of at least 2 repositories to federate.\n"
 				+ "  [<repoID_n>]*            The id's of 0 or mare additional repositories to federate.\n\n"
@@ -75,12 +73,10 @@ public class Federate extends ConsoleCommand {
 		} else {
 			LinkedList<String> plist = new LinkedList<>(Arrays.asList(parameters));
 			plist.remove(); // "federate"
-			boolean distinct = getOptionalParamValue(plist, "distinct", false);
-			boolean readonly = getOptionalParamValue(plist, "readonly", true);
 
 			if (distinctValues(plist)) {
 				String fedID = plist.pop();
-				federate(distinct, readonly, fedID, plist);
+				federate(fedID, plist);
 			} else {
 				writeError("Duplicate repository id's specified.");
 			}
@@ -100,23 +96,28 @@ public class Federate extends ConsoleCommand {
 	/**
 	 * Add one or more repositories to a repository federation
 	 *
-	 * @param distinct
-	 * @param readonly  true when all
 	 * @param fedID
 	 * @param memberIDs list of member
 	 */
-	private void federate(boolean distinct, boolean readonly, String fedID, Deque<String> memberIDs) {
+	private void federate(String fedID, Deque<String> memberIDs) {
 		if (LOGGER.isDebugEnabled()) {
-			logCallDetails(distinct, readonly, fedID, memberIDs);
+			logCallDetails(fedID, memberIDs);
 		}
 		RepositoryManager manager = state.getManager();
 		try {
 			if (manager.hasRepositoryConfig(fedID)) {
 				writeError(fedID + " already exists.");
-			} else if (validateMembers(manager, readonly, memberIDs)) {
+			} else if (validateMembers(manager, memberIDs)) {
 				String description = consoleIO.readln("Federation Description (optional): ");
-				RepositoryManagerFederator rmf = new RepositoryManagerFederator(manager);
-				rmf.addFed(fedID, description, memberIDs, readonly, distinct);
+
+				FedXRepositoryConfig federationConfig = FedXRepositoryConfigBuilder.create()
+						.withResolvableEndpoint(memberIDs)
+						.build();
+
+				RepositoryConfig config = new RepositoryConfig(fedID, federationConfig);
+				config.setTitle(description);
+
+				manager.addRepositoryConfig(config);
 				writeln("Federation created.");
 			}
 		} catch (RepositoryConfigException | RepositoryException | MalformedURLException rce) {
@@ -130,22 +131,14 @@ public class Federate extends ConsoleCommand {
 	 * Validate members of a federation
 	 *
 	 * @param manager   repository manager
-	 * @param readonly  set to true if read-only repositories are OK
 	 * @param memberIDs IDs of the federated repositories
 	 * @return true when all members are present
 	 */
-	private boolean validateMembers(RepositoryManager manager, boolean readonly, Deque<String> memberIDs) {
+	private boolean validateMembers(RepositoryManager manager, Deque<String> memberIDs) {
 		boolean result = true;
 		try {
 			for (String memberID : memberIDs) {
-				if (manager.hasRepositoryConfig(memberID)) {
-					if (!readonly) {
-						if (!manager.getRepository(memberID).isWritable()) {
-							result = false;
-							writeError(memberID + " is read-only.");
-						}
-					}
-				} else {
+				if (!manager.hasRepositoryConfig(memberID)) {
 					result = false;
 					writeError(memberID + " does not exist.");
 				}
@@ -159,36 +152,17 @@ public class Federate extends ConsoleCommand {
 	/**
 	 * Log basic details about calls to federated repositories
 	 *
-	 * @param distinct
-	 * @param readonly
 	 * @param fedID
 	 * @param memberIDs
 	 */
-	private void logCallDetails(boolean distinct, boolean readonly, String fedID, Deque<String> memberIDs) {
+	private void logCallDetails(String fedID, Deque<String> memberIDs) {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Federate called with federation ID = " + fedID + ", and member ID's = ");
 
 		for (String member : memberIDs) {
 			builder.append("[").append(member).append("]");
 		}
-		builder.append(".\n  Distinct set to ")
-				.append(distinct)
-				.append(", and readonly set to ")
-				.append(readonly)
-				.append(".\n");
 		LOGGER.debug(builder.toString());
-	}
-
-	/**
-	 * Get the value of an optional boolean parameter or the default
-	 *
-	 * @param parameters   set of parameters
-	 * @param name         name of the parameter
-	 * @param defaultValue default value
-	 * @return value or default
-	 */
-	private boolean getOptionalParamValue(Deque<String> parameters, String name, boolean defaultValue) {
-		return Boolean.parseBoolean(getOptionalParamValue(parameters, name, Boolean.toString(defaultValue)));
 	}
 
 	/**
