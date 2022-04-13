6f6597bee5ad05dd8f384d65c6cd0a09989679f2
Jerven Bolleman <jerven.bolleman@sib.swiss>
2022-04-13 21:02:20 +0200

    GH-3601 Prepare Exists
    This shows benefit of FilterIterator not making new
    BindingSets.
    
    Signed-off-by: Jerven Bolleman <jerven.bolleman@sib.swiss>

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/StrictEvaluationStrategy.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/impl/StrictEvaluationStrategy.java
@@ -950,7 +950,7 @@ public class StrictEvaluationStrategy implements EvaluationStrategy, FederatedSe
 		} else if (expr instanceof CompareAll) {
 			return new QueryValueEvaluationStep.Minimal(this, expr);
 		} else if (expr instanceof Exists) {
-			return new QueryValueEvaluationStep.Minimal(this, expr);
+			return prepare((Exists) expr, context);
 		} else if (expr instanceof If) {
 			return new QueryValueEvaluationStep.Minimal(this, expr);
 		} else if (expr instanceof ListMemberOperator) {
@@ -1869,6 +1869,20 @@ public class StrictEvaluationStrategy implements EvaluationStrategy, FederatedSe
 		try (CloseableIteration<BindingSet, QueryEvaluationException> iter = evaluate(node.getSubQuery(), bindings)) {
 			return BooleanLiteral.valueOf(iter.hasNext());
 		}
+	}
+
+	private QueryValueEvaluationStep prepare(Exists node, QueryEvaluationContext context)
+			throws QueryEvaluationException {
+		QueryEvaluationStep subQuery = precompile(node.getSubQuery(), context);
+		return new QueryValueEvaluationStep() {
+
+			@Override
+			public Value evaluate(BindingSet bindings) throws ValueExprEvaluationException, QueryEvaluationException {
+				try (CloseableIteration<BindingSet, QueryEvaluationException> iter = subQuery.evaluate(bindings)) {
+					return BooleanLiteral.valueOf(iter.hasNext());
+				}
+			}
+		};
 	}
 
 	@Override
