72b37ca240a15cfcb06fdebeb89f89aa0f22b095
Sanchit Trivedi <46599622+Sanchit-Trivedi@users.noreply.github.com>
2022-05-13 17:43:59 +0530

    GH-1090 : Upgraded Tests to Junit5

--- core/common/io/src/test/java/org/eclipse/rdf4j/common/lang/FileFormatTest.java
+++ core/common/io/src/test/java/org/eclipse/rdf4j/common/lang/FileFormatTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) $2022 Eclipse RDF4J contributors.
+ * Copyright (c) 2022 Eclipse RDF4J contributors.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
@@ -7,21 +7,19 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.common.lang;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 public class FileFormatTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Test
     public void testEmptyMimeType() {
-        thrown.expect(IllegalArgumentException.class);
-        new FileFormat("PLAIN TEXT", new ArrayList<String>(), null, Arrays.asList("txt"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            new FileFormat("PLAIN TEXT", new ArrayList<String>(), null, Arrays.asList("txt"));
+        });
     }
 }
