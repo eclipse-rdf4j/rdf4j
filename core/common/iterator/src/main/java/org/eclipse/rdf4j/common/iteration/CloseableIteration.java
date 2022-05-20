77a253f2c460ce3de1bf063f760624c64c4e89ba
Håvard Ottestad <hmottestad@gmail.com>
2022-05-20 15:10:37 +0200

    GH-3907 override stream() implementation in CloseableIteration

--- core/common/iterator/src/main/java/org/eclipse/rdf4j/common/iteration/CloseableIteration.java
+++ core/common/iterator/src/main/java/org/eclipse/rdf4j/common/iteration/CloseableIteration.java
@@ -8,6 +8,10 @@
 
 package org.eclipse.rdf4j.common.iteration;
 
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 /**
  * An {@link Iteration} that can be closed to free resources that it is holding. CloseableIterations automatically free
  * their resources when exhausted. If not read until exhaustion or if you want to make sure the iteration is properly
@@ -23,8 +27,31 @@ package org.eclipse.rdf4j.common.iteration;
  *   // process the exception that can be thrown while processing.
  * }
  * </pre>
+ *
+ * @implNote In the future this interface will stop extending {@link Iteration} and instead declare the same interface
+ *           methods directly. The interface will also stop requiring implementations to automatically close when
+ *           exhausted, instead making this an optional feature and requiring the user to always call close.
  */
 public interface CloseableIteration<E, X extends Exception> extends Iteration<E, X>, AutoCloseable {
+
+	/**
+	 * Convert the results to a Java 8 Stream.
+	 *
+	 * @return stream
+	 */
+	default Stream<E> stream() {
+		return StreamSupport
+				.stream(new CloseableIterationSpliterator<>(this), false)
+				.onClose(() -> {
+					try {
+						close();
+					} catch (RuntimeException e) {
+						throw e;
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				});
+	}
 
 	/**
 	 * Closes this iteration, freeing any resources that it is holding. If the iteration has already been closed then
