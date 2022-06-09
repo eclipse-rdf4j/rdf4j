f035beb6191de870511c775dd5c15d8249f42584
Håvard Ottestad <hmottestad@gmail.com>
2022-06-09 07:52:49 +0200

    GH-3978 add back our changes to SimpleNode and Node

--- core/queryparser/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/ast/Node.java
+++ core/queryparser/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/ast/Node.java
@@ -12,36 +12,47 @@ public interface Node {
 	 * This method is called after the node has been made the current node. It indicates that child nodes can now be
 	 * added to it.
 	 */
-	public void jjtOpen();
+	void jjtOpen();
 
 	/**
 	 * This method is called after all the child nodes have been added.
 	 */
-	public void jjtClose();
+	void jjtClose();
 
 	/**
 	 * This pair of methods are used to inform the node of its parent.
 	 */
-	public void jjtSetParent(Node n);
+	void jjtSetParent(Node n);
 
-	public Node jjtGetParent();
+	Node jjtGetParent();
 
 	/**
 	 * This method tells the node to add its argument to the node's list of children.
 	 */
-	public void jjtAddChild(Node n, int i);
+	void jjtAddChild(Node n, int i);
 
 	/**
 	 * This method returns a child node. The children are numbered from zero, left to right.
 	 */
-	public Node jjtGetChild(int i);
+	Node jjtGetChild(int i);
 
 	/** Return the number of children the node has. */
-	public int jjtGetNumChildren();
+	int jjtGetNumChildren();
 
-	public int getId();
+	int getId();
 
 	/** Accept the visitor. **/
-	public Object jjtAccept(SyntaxTreeBuilderVisitor visitor, Object data) throws VisitorException;
+	Object jjtAccept(SyntaxTreeBuilderVisitor visitor, Object data) throws VisitorException;
+
+	/**
+	 * Adds the supplied node as the last child node to this node.
+	 */
+	void jjtAppendChild(Node n);
+
+	/**
+	 * Replaces a child node with a new node.
+	 */
+	void jjtReplaceChild(Node oldNode, Node newNode);
+
 }
 /* JavaCC - OriginalChecksum=8c8117e4622f8da980c79e448cb4b5f6 (do not edit this line) */
--- core/queryparser/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/ast/SimpleNode.java
+++ core/queryparser/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/ast/SimpleNode.java
@@ -2,13 +2,19 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=true,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package org.eclipse.rdf4j.query.parser.sparql.ast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleNode implements Node {
+
+	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
 	protected Node parent;
 	protected Node[] children;
 	protected int id;
 	protected Object value;
 	protected SyntaxTreeBuilder parser;
+	private boolean isScopeChange;
 
 	public SimpleNode(int i) {
 		id = i;
@@ -19,35 +25,42 @@ public class SimpleNode implements Node {
 		parser = p;
 	}
 
+	@Override
 	public void jjtOpen() {
 	}
 
+	@Override
 	public void jjtClose() {
 	}
 
+	@Override
 	public void jjtSetParent(Node n) {
 		parent = n;
 	}
 
+	@Override
 	public Node jjtGetParent() {
 		return parent;
 	}
 
+	@Override
 	public void jjtAddChild(Node n, int i) {
 		if (children == null) {
 			children = new Node[i + 1];
 		} else if (i >= children.length) {
-			Node c[] = new Node[i + 1];
+			Node[] c = new Node[i + 1];
 			System.arraycopy(children, 0, c, 0, children.length);
 			children = c;
 		}
 		children[i] = n;
 	}
 
+	@Override
 	public Node jjtGetChild(int i) {
 		return children[i];
 	}
 
+	@Override
 	public int jjtGetNumChildren() {
 		return (children == null) ? 0 : children.length;
 	}
@@ -60,20 +73,26 @@ public class SimpleNode implements Node {
 		return value;
 	}
 
-	/** Accept the visitor. **/
+	/**
+	 * Accept the visitor.
+	 **/
+	@Override
 	public Object jjtAccept(SyntaxTreeBuilderVisitor visitor, Object data)
 			throws VisitorException {
 		return visitor.visit(this, data);
 	}
 
-	/** Accept the visitor. **/
-	public Object childrenAccept(SyntaxTreeBuilderVisitor visitor, Object data)
-			throws VisitorException {
+	/**
+	 * Accept the visitor.
+	 **/
+	public Object childrenAccept(SyntaxTreeBuilderVisitor visitor, Object data) throws VisitorException {
 		if (children != null) {
-			for (int i = 0; i < children.length; ++i) {
-				children[i].jjtAccept(visitor, data);
+			for (Node childNode : children) {
+				// Note: modified JavaCC code, child's data no longer ignored
+				data = childNode.jjtAccept(visitor, data);
 			}
 		}
+
 		return data;
 	}
 
@@ -88,7 +107,7 @@ public class SimpleNode implements Node {
 	}
 
 	public String toString(String prefix) {
-		return prefix + toString();
+		return prefix + this;
 	}
 
 	/*
@@ -107,9 +126,92 @@ public class SimpleNode implements Node {
 		}
 	}
 
+	@Override
 	public int getId() {
 		return id;
 	}
+
+	/**
+	 * Gets the (first) child of this node that is of the specific type.
+	 *
+	 * @param type The type of the child node that should be returned.
+	 * @return The (first) child node of the specified type, or <var>null</var> if no such child node was found.
+	 */
+	public <T extends Node> T jjtGetChild(Class<T> type) {
+		if (children != null) {
+			for (Node n : children) {
+				if (type.isInstance(n)) {
+					return (T) n;
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public <T extends Node> List<T> jjtGetChildren(Class<T> type) {
+		if (children == null) {
+			return List.of();
+		}
+
+		List<T> result = new ArrayList<>(children.length);
+
+		for (Node n : children) {
+			if (type.isInstance(n)) {
+				result.add((T) n);
+			}
+		}
+
+		return result;
+	}
+
+	public Node[] jjtGetChildren() {
+		return children;
+	}
+
+	public void jjtReplaceWith(Node newNode) {
+		if (parent != null) {
+			parent.jjtReplaceChild(this, newNode);
+		}
+
+		if (children != null) {
+			for (Node childNode : children) {
+				childNode.jjtSetParent(newNode);
+			}
+		}
+	}
+
+	public void jjtReplaceChild(Node oldNode, Node newNode) {
+		if (children != null) {
+			for (int i = 0; i < children.length; i++) {
+				if (children[i] == oldNode) {
+					children[i] = newNode;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void jjtAppendChild(Node n) {
+		jjtAddChild(n, children == null ? 0 : children.length);
+	}
+
+	/**
+	 * Check if this AST node constitutes a variable scope change.
+	 *
+	 * @return the isScopeChange
+	 */
+	public boolean isScopeChange() {
+		return isScopeChange;
+	}
+
+	/**
+	 * @param isScopeChange the isScopeChange to set
+	 */
+	public void setScopeChange(boolean isScopeChange) {
+		this.isScopeChange = isScopeChange;
+	}
+
 }
 
 /* JavaCC - OriginalChecksum=43de2f398dac53c0bbcf106e4a3ca6de (do not edit this line) */
