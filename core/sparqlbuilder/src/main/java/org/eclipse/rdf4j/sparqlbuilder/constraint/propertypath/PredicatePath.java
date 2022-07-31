b849630bfac9c84e0444390179409456da043b15
Håvard M. Ottestad <hmottestad@gmail.com>
2022-07-31 12:10:25 +0200

    GH-4091 add a newline in the copyright header (#4097)
    * GH-4091 add a newline in the copyright header
    
    * remove commented out files

--- core/sparqlbuilder/src/main/java/org/eclipse/rdf4j/sparqlbuilder/constraint/propertypath/PredicatePath.java
+++ core/sparqlbuilder/src/main/java/org/eclipse/rdf4j/sparqlbuilder/constraint/propertypath/PredicatePath.java
@@ -1,7 +1,8 @@
 /*
  * ******************************************************************************
  *  * Copyright (c) 2021 Eclipse RDF4J contributors.
- *  * All rights reserved. This program and the accompanying materials
+ *  *
+ * All rights reserved. This program and the accompanying materials
  *  * are made available under the terms of the Eclipse Distribution License v1.0
  *  * which accompanies this distribution, and is available at
  *  * http://www.eclipse.org/org/documents/edl-v10.php.
--- testsuites/sparql/src/main/java/org/eclipse/rdf4j/testsuite/sparql/tests/DescribeTest.java
+++ testsuites/sparql/src/main/java/org/eclipse/rdf4j/testsuite/sparql/tests/DescribeTest.java
@@ -1,9 +1,10 @@
-/******************************************************************************* 
+/*******************************************************************************
  * Copyright (c) 2022 Eclipse RDF4J contributors.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Eclipse Distribution License v1.0 
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/org/documents/edl-v10.php. 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
  ********************************************************************************/
 package org.eclipse.rdf4j.testsuite.sparql.tests;
 
@@ -27,7 +28,7 @@ import org.junit.Test;
 
 /**
  * Tests on SPARQL DESCRIBE queries
- * 
+ *
  * @author Jeen Broekstra
  */
 public class DescribeTest extends AbstractComplianceTest {
