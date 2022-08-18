9ec3fbd2dd1462fa5a5222b0646c590e7f5f22f7
Håvard Ottestad <hmottestad@gmail.com>
2022-08-18 09:20:17 +0200

    GH-4112 fix copyright header

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/optimizer/ParentReferenceChecker.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/optimizer/ParentReferenceChecker.java
@@ -5,7 +5,9 @@
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
- ******************************************************************************/
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *******************************************************************************/
 
 package org.eclipse.rdf4j.query.algebra.evaluation.optimizer;
 
@@ -32,6 +34,7 @@ import org.slf4j.LoggerFactory;
 public class ParentReferenceChecker implements QueryOptimizer {
 
 	private static final Logger logger = LoggerFactory.getLogger(ParentReferenceChecker.class);
+
 	private final QueryOptimizer previousOptimizerInPipeline;
 
 	public ParentReferenceChecker(QueryOptimizer previousOptimizerInPipeline) {
