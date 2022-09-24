892230ba6bb6cf42701d752bf77e2d4f28e61df6
Jeen Broekstra <jeen@fastmail.com>
2022-09-24 14:01:24 +1200

    GH-635 add javadoc

--- core/common/transaction/src/main/java/org/eclipse/rdf4j/common/transaction/QueryEvaluationMode.java
+++ core/common/transaction/src/main/java/org/eclipse/rdf4j/common/transaction/QueryEvaluationMode.java
@@ -11,17 +11,24 @@
 package org.eclipse.rdf4j.common.transaction;
 
 /**
+ * The Query Evaluation Mode determines the behaviour of the SPARQL query engine on RDF4J repositories. It currently
+ * supports two modes, {@link #STRICT} and and {@link #STANDARD}.
+ * 
  * @author Jeen Broekstra
  */
 public enum QueryEvaluationMode implements TransactionSetting {
 	/**
-	 * Strict minimally-compliant mode with respect to SPARQL 1.1 recommendation.
+	 * Strict minimally-compliant mode for SPARQL 1.1. In this mode, no operators have been extended (as described in
+	 * <a href="https://www.w3.org/TR/sparql11-query/#operatorExtensibility">section 17.3.1 of the SPARQL 1.1 Query
+	 * Recommendation</a>).
 	 */
 	STRICT,
 
 	/**
 	 * Standard mode extends minimal compliance with various practical operator behavioral extensions, in a way that is
-	 * still compliant with the SPARQL 1.1 Recommendation.
+	 * still compliant with the SPARQL 1.1 specification (as described in
+	 * <a href="https://www.w3.org/TR/sparql11-query/#operatorExtensibility">section 17.3.1 of the SPARQL 1.1 Query
+	 * Recommendation</a>).
 	 */
 	STANDARD
 
