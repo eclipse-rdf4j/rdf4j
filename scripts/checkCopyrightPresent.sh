1bb88b2162161b126ffc5c5064ea594c431b6dc7
Jerven bolleman <jerven.bolleman@sib.swiss>
2022-12-15 16:46:20 +0100

    GH-4321 Check that the copyright header has the SPDX header

--- scripts/checkCopyrightPresent.sh
+++ scripts/checkCopyrightPresent.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 filesWithOutCopyright=()
+filesWithOutSPDX=()
 for i in $(find ../ -name pom.xml); do
   # only look in non test files but make sure src/main exists.
   # and not in package-info
@@ -12,12 +13,21 @@ for i in $(find ../ -name pom.xml); do
         filesWithOutCopyright+=($f)
       fi
     done
+    for c in $(find ${dir} -name *.java -not -name package-info.java -exec grep -L 'SPDX-License-Identifier: BSD-3-Clause' {} \;); do
+        filesWithOutSPDX+=($c)
+    done
   fi
 done
 
 for f in ${filesWithOutCopyright[@]}; do
   echo "Missing copyright: $f"
 done
+for f in ${filesWithOutSPDX[@]}; do
+  echo "Missing SPDX line in: $f"
+done
 if [ ${#filesWithOutCopyright[@]} -ne 0 ]; then
   exit 1
+fi
+if [ ${#filesWithOutSPDX[@]} -ne 0 ]; then
+  exit 2
 fi
