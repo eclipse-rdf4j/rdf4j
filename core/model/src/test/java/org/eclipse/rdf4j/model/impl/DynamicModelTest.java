aa6b2a1f57a46a33c2792d52d69fd044ccf9da4d
domkun <dominik.kunz@gmail.com>
2023-01-27 22:30:42 +0100

    Fix code format

--- core/model/src/test/java/org/eclipse/rdf4j/model/impl/DynamicModelTest.java
+++ core/model/src/test/java/org/eclipse/rdf4j/model/impl/DynamicModelTest.java
@@ -11,58 +11,58 @@
 
 package org.eclipse.rdf4j.model.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Namespace;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class DynamicModelTest {
 
-    @Test
-    public void testAddNamespace() {
-        SimpleNamespace ns = new SimpleNamespace("ex", "example:namespace");
+	@Test
+	public void testAddNamespace() {
+		SimpleNamespace ns = new SimpleNamespace("ex", "example:namespace");
 
-        Model model = new DynamicModelFactory().createEmptyModel();
-        model.setNamespace(ns);
+		Model model = new DynamicModelFactory().createEmptyModel();
+		model.setNamespace(ns);
 
-        Model filtered = model.filter(null, null, null);
+		Model filtered = model.filter(null, null, null);
 
-        assertThat(filtered.getNamespace(ns.getPrefix()).get())
-                .isEqualTo(ns);
-    }
+		assertThat(filtered.getNamespace(ns.getPrefix()).get())
+				.isEqualTo(ns);
+	}
 
-    @Test
-    public void testAddNamespacePrefixAndName() {
-        String prefix = "ex";
-        String name = "example:namespace";
+	@Test
+	public void testAddNamespacePrefixAndName() {
+		String prefix = "ex";
+		String name = "example:namespace";
 
-        Model model = new DynamicModelFactory().createEmptyModel();
-        model.setNamespace(prefix, name);
+		Model model = new DynamicModelFactory().createEmptyModel();
+		model.setNamespace(prefix, name);
 
-        Model filtered = model.filter(null, null, null);
+		Model filtered = model.filter(null, null, null);
 
-        assertThat(filtered.getNamespace(prefix).isPresent())
-                .isTrue();
-        assertThat(filtered.getNamespace(prefix).get().getName())
-                .isEqualTo(name);
-    }
+		assertThat(filtered.getNamespace(prefix).isPresent())
+				.isTrue();
+		assertThat(filtered.getNamespace(prefix).get().getName())
+				.isEqualTo(name);
+	}
 
-    @Test
-    public void testRemoveNamespace() {
-        Namespace ns = new SimpleNamespace("ex", "example:namespace");
+	@Test
+	public void testRemoveNamespace() {
+		Namespace ns = new SimpleNamespace("ex", "example:namespace");
 
-        Model model = new DynamicModelFactory().createEmptyModel();
-        model.setNamespace(ns);
+		Model model = new DynamicModelFactory().createEmptyModel();
+		model.setNamespace(ns);
 
-        Model filtered = model.filter(null, null, null);
+		Model filtered = model.filter(null, null, null);
 
-        filtered.removeNamespace(ns.getPrefix());
+		filtered.removeNamespace(ns.getPrefix());
 
-        assertThat(filtered.getNamespace(ns.getPrefix()).isPresent())
-                .isFalse();
-        assertThat(model.getNamespace(ns.getPrefix()).isPresent())
-                .isFalse();
-    }
+		assertThat(filtered.getNamespace(ns.getPrefix()).isPresent())
+				.isFalse();
+		assertThat(model.getNamespace(ns.getPrefix()).isPresent())
+				.isFalse();
+	}
 
 }
