ea38fb3e6580e152e79940cf13b5ffe9958666f9
Håvard Ottestad <hmottestad@gmail.com>
2023-02-11 18:51:28 +0100

    news item and release-notes if relevant for 4.2.3
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -119,12 +119,12 @@ MVN_NEXT_SNAPSHOT_VERSION="$(increment_version "$MVN_VERSION_RELEASE" 3)-SNAPSHO
 
 echo "";
 echo "Your current maven snapshot version is: '${MVN_CURRENT_SNAPSHOT_VERSION}'"
-echo "Your maven release version will be: '4.2.3'"
+echo "Your maven release version will be: '${MVN_VERSION_RELEASE}'"
 echo "Your next maven snapshot version will be: '${MVN_NEXT_SNAPSHOT_VERSION}'"
 read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
 # set maven version
-mvn versions:set -DnewVersion="4.2.3"
+mvn versions:set -DnewVersion="${MVN_VERSION_RELEASE}"
 
 # set the MVN_VERSION_RELEASE version again just to be on the safe side
 MVN_VERSION_RELEASE=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
@@ -135,7 +135,7 @@ MVN_VERSION_RELEASE=$(xmllint --xpath "//*[local-name()='project']/*[local-name(
 mvn versions:commit
 
 
-BRANCH="releases/4.2.3"
+BRANCH="releases/${MVN_VERSION_RELEASE}"
 
 # delete old release branch if it exits
 if git show-ref --verify --quiet "refs/heads/${BRANCH}"; then
@@ -144,8 +144,8 @@ fi
 
 # checkout branch for release, commit this maven version and tag commit
 git checkout -b "${BRANCH}"
-git commit -s -a -m "release 4.2.3"
-git tag "4.2.3"
+git commit -s -a -m "release ${MVN_VERSION_RELEASE}"
+git tag "${MVN_VERSION_RELEASE}"
 
 echo "";
 echo "Pushing release branch to github"
@@ -153,14 +153,14 @@ read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
 # push release branch and tag
 git push -u origin "${BRANCH}"
-git push origin "4.2.3"
+git push origin "${MVN_VERSION_RELEASE}"
 
 echo "";
 echo "You need to tell Jenkins to start the release deployment processes, for SDK and maven artifacts"
 echo "- SDK deployment: https://ci.eclipse.org/rdf4j/job/rdf4j-deploy-release-sdk/ "
 echo "- Maven deployment: https://ci.eclipse.org/rdf4j/job/rdf4j-deploy-release-ossrh/ "
 echo "(if you are on linux or windows, remember to use CTRL+SHIFT+C to copy)."
-echo "Log in, then choose 'Build with Parameters' and type in 4.2.3"
+echo "Log in, then choose 'Build with Parameters' and type in ${MVN_VERSION_RELEASE}"
 read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
 # Cleanup
@@ -204,14 +204,14 @@ MVN_VERSION_DEVELOP=$(xmllint --xpath "//*[local-name()='project']/*[local-name(
 
 git checkout "${BRANCH}"
 
-git checkout -b "merge_main_into_develop_after_release_4.2.3"
+git checkout -b "merge_main_into_develop_after_release_${MVN_VERSION_RELEASE}"
 mvn versions:set -DnewVersion="${MVN_VERSION_DEVELOP}"
 mvn versions:commit
 git commit -s -a -m "set correct version"
-git push --set-upstream origin "merge_main_into_develop_after_release_4.2.3"
+git push --set-upstream origin "merge_main_into_develop_after_release_${MVN_VERSION_RELEASE}"
 
 echo "Creating pull request to merge the merge-branch into develop"
-gh pr create -B develop --title "sync develop branch after release 4.2.3" --body "Merge using merge commit rather than rebase"
+gh pr create -B develop --title "sync develop branch after release ${MVN_VERSION_RELEASE}" --body "Merge using merge commit rather than rebase"
 echo "It's ok to merge this PR later, so wait for the Jenkins tests to finish."
 read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
@@ -225,22 +225,24 @@ mvn clean -Dmaven.clean.failOnError=false
 echo "Build javadocs"
 read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
-git checkout "4.2.3"
+git checkout "${MVN_VERSION_RELEASE}"
+mvn clean -Dmaven.clean.failOnError=false
 mvn clean
+mvn compile -Pquick -Dmaven.compiler.failOnError=false
 mvn package -Passembly -DskipTests
 
 git checkout main
-RELEASE_NOTES_BRANCH="4.2.3-release-notes"
-git checkout -b "4.2.3-release-notes"
+RELEASE_NOTES_BRANCH="${MVN_VERSION_RELEASE}-release-notes"
+git checkout -b "${RELEASE_NOTES_BRANCH}"
 
-tar -cvzf "site/static/javadoc/4.2.3.tgz" -C target/site/apidocs .
-cp -f "site/static/javadoc/4.2.3.tgz" "site/static/javadoc/latest.tgz"
+tar -cvzf "site/static/javadoc/${MVN_VERSION_RELEASE}.tgz" -C target/site/apidocs .
+cp -f "site/static/javadoc/${MVN_VERSION_RELEASE}.tgz" "site/static/javadoc/latest.tgz"
 git add --all
-git commit -s -a -m "javadocs for 4.2.3"
-git push --set-upstream origin "4.2.3-release-notes"
-gh pr create -B main --title "4.2.3-release-notes" --body "Javadocs, release-notes and news item for 4.2.3"
+git commit -s -a -m "javadocs for ${MVN_VERSION_RELEASE}"
+git push --set-upstream origin "${RELEASE_NOTES_BRANCH}"
+gh pr create -B main --title "${RELEASE_NOTES_BRANCH}" --body "Javadocs, release-notes and news item for ${MVN_VERSION_RELEASE}"
 
-echo "Javadocs are in git branch 4.2.3-release-notes"
+echo "Javadocs are in git branch ${RELEASE_NOTES_BRANCH}"
 
 git checkout main
 mvn clean
@@ -256,9 +258,9 @@ echo ""
 echo "You will now want to inform the community about the new release!"
 echo " - Check if all recently completed issues have the correct milestone: https://github.com/eclipse/rdf4j/issues?q=is%3Aissue+no%3Amilestone+-label%3A%22cannot+reproduce%22+-label%3A%22🔧+internal+task%22+-label%3Awontfix+-label%3Astale+-label%3Aduplicate+sort%3Aupdated-desc+is%3Aclosed"
 echo " - Create a new milestone for ${MVN_NEXT_SNAPSHOT_VERSION/-SNAPSHOT/} : https://github.com/eclipse/rdf4j/milestones/new"
-echo " - Close the 4.2.3 milestone: https://github.com/eclipse/rdf4j/milestones"
+echo " - Close the ${MVN_VERSION_RELEASE} milestone: https://github.com/eclipse/rdf4j/milestones"
 echo "     - Make sure that all issues in the milestone are closed, or move them to the next milestone"
 
 echo ""
 echo "To generate the news item and release-notes you will want to run the following command:"
-echo "./release-notes.sh 4.2.3 patch-release-notes.md patch-news-item.md 4.2.3-release-notes"
+echo "./release-notes.sh ${MVN_VERSION_RELEASE} patch-release-notes.md patch-news-item.md ${RELEASE_NOTES_BRANCH}"
