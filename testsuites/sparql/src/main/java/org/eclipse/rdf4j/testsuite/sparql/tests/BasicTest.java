4cec4ee969e530b86de356e6dfc416106d94c35e
Håvard Ottestad <hmottestad@gmail.com>
2023-07-17 10:47:46 +0200

    GH-4659 add test case

--- testsuites/sparql/src/main/java/org/eclipse/rdf4j/testsuite/sparql/tests/BasicTest.java
+++ testsuites/sparql/src/main/java/org/eclipse/rdf4j/testsuite/sparql/tests/BasicTest.java
@@ -10,9 +10,17 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.testsuite.sparql.tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.vocabulary.DC;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.query.AbstractTupleQueryResultHandler;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.QueryLanguage;
@@ -43,6 +51,30 @@ public class BasicTest extends AbstractComplianceTest {
 						fail("nobody is self published");
 					}
 				});
+	}
+
+	@Test
+	public void testIdenticalVariablesSubjectContextInStatementPattern() {
+		conn.add(EX.ALICE, FOAF.KNOWS, EX.BOB, EX.ALICE);
+		conn.add(EX.ALICE, RDF.TYPE, FOAF.PERSON, EX.ALICE);
+		conn.add(EX.ALICE, FOAF.KNOWS, EX.A, EX.BOB);
+		conn.add(EX.ALICE, FOAF.KNOWS, EX.B, EX.BOB);
+		conn.add(EX.ALICE, FOAF.KNOWS, EX.C, EX.BOB);
+		conn.add(EX.ALICE, FOAF.KNOWS, EX.MARY, EX.BOB);
+
+		String queryBuilder = "SELECT ?knows { " +
+				"	graph ?alice {" +
+				"		?alice a <" + FOAF.PERSON + ">; " +
+				"			<" + FOAF.KNOWS + "> ?knows ." +
+				"		}" +
+				"}";
+
+		try (Stream<BindingSet> stream = conn.prepareTupleQuery(QueryLanguage.SPARQL, queryBuilder)
+				.evaluate()
+				.stream()) {
+			List<Value> knows = stream.map(b -> b.getValue("knows")).collect(Collectors.toList());
+			assertEquals(List.of(EX.BOB), knows);
+		}
 	}
 
 }
