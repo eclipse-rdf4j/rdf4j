cc3782c919e67c591cd175a43f193774f3d9eac9
Frens Jan Rumph <frens.jan@web-iq.com>
2023-12-23 20:04:09 +0100

    GH-4859 add instead of remove QueryRoot for explaining optimized Sail queries

--- core/repository/sail/src/test/java/org/eclipse/rdf4j/repository/sail/SailRepositoryConnectionTest.java
+++ core/repository/sail/src/test/java/org/eclipse/rdf4j/repository/sail/SailRepositoryConnectionTest.java
@@ -10,8 +10,10 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.repository.sail;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -24,7 +26,9 @@ import org.eclipse.rdf4j.query.BooleanQuery;
 import org.eclipse.rdf4j.query.GraphQuery;
 import org.eclipse.rdf4j.query.Query;
 import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.algebra.QueryRoot;
 import org.eclipse.rdf4j.query.algebra.TupleExpr;
+import org.eclipse.rdf4j.query.explanation.Explanation;
 import org.eclipse.rdf4j.sail.SailConnection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -146,6 +150,23 @@ public class SailRepositoryConnectionTest {
 		query.evaluate();
 		// check that the TupleExpr implementation created by the underlying sail was passed to the evaluation
 		verify(sailConnection).evaluate(eq(expr), any(), any(), anyBoolean());
+	}
+
+	@Test
+	public void testExplainQuery() {
+		TupleExpr expr = mock(TupleExpr.class);
+		when(expr.clone()).thenReturn(expr);
+		Explanation explanation = mock(Explanation.class);
+
+		when(sailConnection.prepareQuery(any(), any(), any(), any())).thenReturn(Optional.of(expr));
+		when(sailConnection.explain(any(), any(TupleExpr.class), any(), any(), anyBoolean(), anyInt()))
+				.thenReturn(explanation);
+
+		TupleQuery query = subject.prepareTupleQuery("SELECT * WHERE { ?s ?p ?o }");
+		assertThat(query.explain(Explanation.Level.Unoptimized)).isEqualTo(explanation);
+
+		verify(sailConnection).explain(eq(Explanation.Level.Unoptimized), any(QueryRoot.class), any(), any(),
+				anyBoolean(), anyInt());
 	}
 
 }
