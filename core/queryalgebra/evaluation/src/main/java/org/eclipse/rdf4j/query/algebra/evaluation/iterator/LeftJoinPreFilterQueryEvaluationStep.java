6962ee710f957899ea0284af5a138d21797107ec
Jasper van Maanen <jasper@web-iq.com>
2025-04-07 14:31:48 +0200

    GH-5286 Moving filter logic to separate concepts

---
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/iterator/LeftJoinPreFilterQueryEvaluationStep.java
@@ -0,0 +1,31 @@
+package org.eclipse.rdf4j.query.algebra.evaluation.iterator;
+
+import org.eclipse.rdf4j.common.iteration.CloseableIteration;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.algebra.evaluation.QueryEvaluationStep;
+
+class LeftJoinPreFilterQueryEvaluationStep implements QueryEvaluationStep {
+
+	private final QueryEvaluationStep wrapped;
+	private final ScopeBindingsJoinConditionEvaluator joinConditionEvaluator;
+
+    LeftJoinPreFilterQueryEvaluationStep(
+            QueryEvaluationStep wrapped,
+			ScopeBindingsJoinConditionEvaluator joinConditionEvaluator) {
+		this.wrapped = wrapped;
+        this.joinConditionEvaluator = joinConditionEvaluator;
+    }
+
+	@Override
+	public CloseableIteration<BindingSet> evaluate(BindingSet leftBindings) {
+		if (shouldEvaluate(leftBindings)) {
+			return wrapped.evaluate(leftBindings);
+		}
+
+		return QueryEvaluationStep.EMPTY_ITERATION;
+	}
+
+	private boolean shouldEvaluate(BindingSet leftBindings) {
+		return joinConditionEvaluator.evaluate(leftBindings);
+	}
+}
