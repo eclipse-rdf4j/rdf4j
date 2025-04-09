66ae5e3101ce5247a0744b1e61ae96888bf900e0
Jerven Bolleman <jerven.bolleman@sib.swiss>
2025-04-09 17:33:56 +0200

    GH-5290 expand test case.

--- core/queryresultio/xlsx/src/test/java/org/eclipse/rdf4j/query/resultio/sparqlxml/SPARQLXLSXTupleTest.java
+++ core/queryresultio/xlsx/src/test/java/org/eclipse/rdf4j/query/resultio/sparqlxml/SPARQLXLSXTupleTest.java
@@ -17,13 +17,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.impl.MapBindingSet;
 import org.eclipse.rdf4j.query.impl.TupleQueryResultBuilder;
 import org.eclipse.rdf4j.query.resultio.sparqlxslx.SPARQLResultsXLSXWriter;
@@ -38,21 +41,30 @@ public class SPARQLXLSXTupleTest {
 	@Test
 	void simpleCase(@TempDir Path dir) throws IOException {
 		Files.createDirectories(dir);
+
 		Path tf = dir.resolve("test.xlsx");
+//		Path tf = Paths.get("/home/jbollema/test.xlsx");
 		TupleQueryResultBuilder b = new TupleQueryResultBuilder();
 		b.startQueryResult(List.of("boolean", "iri", "int"));
 		MapBindingSet bs = new MapBindingSet();
 		bs.setBinding("boolean", SimpleValueFactory.getInstance().createLiteral(true));
-		bs.setBinding("iri", SimpleValueFactory.getInstance().createIRI("https://example.org/iri/"));
+		bs.setBinding("iri", SimpleValueFactory.getInstance().createIRI("https://example.org/iri"));
 		bs.setBinding("int", SimpleValueFactory.getInstance().createLiteral(1));
 		b.handleSolution(bs);
+		MapBindingSet bs2 = new MapBindingSet();
+		bs2.setBinding("boolean", SimpleValueFactory.getInstance().createLiteral(false));
+		bs2.setBinding("iri", SimpleValueFactory.getInstance().createIRI("https://example.org/iri/test"));
+		bs2.setBinding("int", SimpleValueFactory.getInstance().createLiteral(-9));
+		b.handleSolution(bs2);
 		List<String> links = List.of("http://example.org/link1");
 		b.handleLinks(links);
 		try (var out = Files.newOutputStream(tf)) {
 			SPARQLResultsXLSXWriter writer = new SPARQLResultsXLSXWriter(out);
 			writer.handleLinks(links);
 			writer.startQueryResult(new ArrayList<>(bs.getBindingNames()));
-			writer.handleSolution(bs);
+			Iterator<BindingSet> iterator = b.getQueryResult().iterator();
+			while (iterator.hasNext())
+				writer.handleSolution(iterator.next());
 			writer.endQueryResult();
 		}
 		assertTrue(Files.size(tf) > 0);
