73758d2c946944ad8da085e24fac911948e3aa25
Håvard M. Ottestad <hmottestad@gmail.com>
2025-04-13 13:27:50 +0200

    GH-5294 stack overflow sparql update (#5295)
    (cherry picked from commit 14256d0bcab294143efe9372999ded49029852be)

--- core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/MemoryStoreConnectionTest.java
+++ core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/MemoryStoreConnectionTest.java
@@ -10,15 +10,47 @@
  *******************************************************************************/
 package org.eclipse.rdf4j.sail.memory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.File;
 
+import org.eclipse.rdf4j.common.transaction.IsolationLevels;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.testsuite.repository.RepositoryConnectionTest;
+import org.junit.jupiter.api.Test;
 
 public class MemoryStoreConnectionTest extends RepositoryConnectionTest {
 	@Override
 	protected Repository createRepository(File dataDir) {
 		return new SailRepository(new MemoryStore());
+	}
+
+	@Test
+	public void reallyBigUpdateToTriggerPotentialStackOverflowTest() {
+		setupTest(IsolationLevels.NONE);
+
+		IRI g1 = vf.createIRI("urn:test:g1");
+		testCon.begin();
+		testCon.add(vf.createBNode(), RDF.TYPE, g1);
+		testCon.commit();
+
+		testCon.begin();
+
+		StringBuilder bigUpdate = new StringBuilder();
+		bigUpdate.append("DELETE { ?s ?p ?o } INSERT{\n");
+
+		for (int i = 0; i < 20000; i++) {
+			bigUpdate.append(" [] <urn:test:prop> \"").append(i).append("\".\n");
+		}
+		bigUpdate.append("\n} WHERE { ?s ?p ?o }");
+
+		testCon.prepareUpdate(bigUpdate.toString()).execute();
+
+		long size = testCon.size();
+		assertThat(size).isEqualTo(20000);
+		testCon.commit();
 	}
 }
