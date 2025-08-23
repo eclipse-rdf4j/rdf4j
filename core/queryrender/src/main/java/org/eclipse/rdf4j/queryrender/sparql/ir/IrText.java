6fc4901cb2c00ee06b8d0786cc53af2212aa9863
Håvard Ottestad <hmottestad@gmail.com>
2025-08-23 21:21:02 +0200

    starting proper IR

---
+++ core/queryrender/src/main/java/org/eclipse/rdf4j/queryrender/sparql/ir/IrText.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Eclipse RDF4J contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *******************************************************************************/
+package org.eclipse.rdf4j.queryrender.sparql.ir;
+
+/**
+ * Generic textual line node when no more specific IR type is available.
+ */
+public class IrText extends IrNode {
+	private final String text;
+
+	public IrText(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+}
