8ee2da632d1e15f567613a257d580a9d82bde13e
Håvard Ottestad <hmottestad@gmail.com>
2025-08-25 11:40:51 +0200

    starting proper IR

--- core/queryparser/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/ast/ASTGraphPatternGroup.java
+++ core/queryparser/sparql/src/main/java/org/eclipse/rdf4j/query/parser/sparql/ast/ASTGraphPatternGroup.java
@@ -32,7 +32,10 @@ public class ASTGraphPatternGroup extends SimpleNode {
 		if (!(this.parent instanceof ASTExistsFunc
 				|| this.parent instanceof ASTNotExistsFunc
 				|| this.parent instanceof ASTGraphGraphPattern
-				|| this.parent instanceof ASTWhereClause)) {
+				|| this.parent instanceof ASTWhereClause
+				|| this.parent instanceof ASTUnionGraphPattern // UNION would already have introduced a scope change, so
+																// rely on super.isScopeChange()
+		)) {
 			return true;
 		}
 		return super.isScopeChange();
