66781d592474e1d7d9174021f51b6488ce4dddff
Håvard Ottestad <hmottestad@gmail.com>
2025-08-25 21:06:57 +0200

    starting proper IR

---
+++ core/queryrender/src/main/java/org/eclipse/rdf4j/queryrender/sparql/ir/util/transform/ApplyCollectionsTransform.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Eclipse RDF4J contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *******************************************************************************/
+package org.eclipse.rdf4j.queryrender.sparql.ir.util.transform;
+
+import org.eclipse.rdf4j.queryrender.sparql.TupleExprIRRenderer;
+import org.eclipse.rdf4j.queryrender.sparql.ir.IrBGP;
+import org.eclipse.rdf4j.queryrender.sparql.ir.util.IrTransforms;
+
+public final class ApplyCollectionsTransform {
+	private ApplyCollectionsTransform() {
+	}
+
+	public static IrBGP apply(IrBGP bgp, TupleExprIRRenderer r) {
+		return IrTransforms.applyCollections(bgp, r);
+	}
+}
