fed6d7c68dd6af57b87e983e28c1dee04c0012c9
Håvard Ottestad <hmottestad@gmail.com>
2025-08-27 14:07:35 +0200

    starting proper IR

--- core/queryrender/src/test/java/org/eclipse/rdf4j/queryrender/VarNameNormalizer.java
+++ core/queryrender/src/test/java/org/eclipse/rdf4j/queryrender/VarNameNormalizer.java
@@ -1,6 +1,13 @@
 package org.eclipse.rdf4j.queryrender;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
