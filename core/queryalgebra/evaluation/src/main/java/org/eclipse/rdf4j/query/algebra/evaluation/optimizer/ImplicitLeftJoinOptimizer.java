7ffc7f461fd43bac1b227a8f401ddc45ecd30aae
Håvard Ottestad <hmottestad@gmail.com>
2025-09-10 01:23:39 +0200

    codex asked to simplify code

--- core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/optimizer/ImplicitLeftJoinOptimizer.java
+++ core/queryalgebra/evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/optimizer/ImplicitLeftJoinOptimizer.java
@@ -1,4 +1,13 @@
-// File: core/queryalgebra-evaluation/src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/optimizer/ImplicitLeftJoinOptimizer.java
+/*******************************************************************************
+ * Copyright (c) 2025 Eclipse RDF4J contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ ******************************************************************************/
 package org.eclipse.rdf4j.query.algebra.evaluation.optimizer;
 
 import java.util.ArrayList;
