c9185eed10431a4048774789ece0ad80140f9caf
Håvard Ottestad <hmottestad@gmail.com>
2025-09-19 09:05:56 +0200

    wip2

--- core/sail/lmdb/src/main/java/org/eclipse/rdf4j/sail/lmdb/Varint.java
+++ core/sail/lmdb/src/main/java/org/eclipse/rdf4j/sail/lmdb/Varint.java
@@ -14,6 +14,7 @@ import java.nio.ByteBuffer;
 
 import org.eclipse.rdf4j.sail.lmdb.util.Bytes;
 import org.eclipse.rdf4j.sail.lmdb.util.SignificantBytesBE;
+import org.eclipse.rdf4j.sail.lmdb.util.VarUInt;
 
 /**
  * Encodes and decodes unsigned values using variable-length encoding.
