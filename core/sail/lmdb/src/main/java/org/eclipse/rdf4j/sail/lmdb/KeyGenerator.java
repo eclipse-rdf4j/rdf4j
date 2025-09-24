533092cfdb2e855e133c410f00d38e6a128f2111
Håvard Ottestad <hmottestad@gmail.com>
2025-09-24 10:08:14 +0200

    key cache improvements wip

--- core/sail/lmdb/src/main/java/org/eclipse/rdf4j/sail/lmdb/KeyGenerator.java
+++ core/sail/lmdb/src/main/java/org/eclipse/rdf4j/sail/lmdb/KeyGenerator.java
@@ -20,7 +20,7 @@ import java.util.function.Supplier;
  */
 final class KeyGenerator implements AutoCloseable {
 
-	static final int CACHE_THRESHOLD = 5;
+	static int CACHE_THRESHOLD = 2;
 	static final int WINDOW_SIZE = 10000;
 
 	private static final int FILTER_BITS = 1 << 12;
@@ -42,7 +42,7 @@ final class KeyGenerator implements AutoCloseable {
 	}
 
 	ByteBuffer keyFor(long subj, long pred, long obj, long context, Supplier<ByteBuffer> supplier, boolean pooled,
-			boolean allowCache) {
+					  boolean allowCache) {
 		long sum = subj * 3 + (subj + pred) * 5 + (subj + obj) * 7 + context * 11;
 		int filterIndex = (int) (sum & FILTER_MASK);
 
@@ -61,16 +61,47 @@ final class KeyGenerator implements AutoCloseable {
 		buffer.flip();
 
 		if (allowCache) {
-			maybePromote(buffer, sum, filterIndex, subj, pred, obj, context);
+			if (timeSinceLastCacheThresholdIncrease + 5 > epoch && epoch% 32 == 0) {
+				// Don't promote to cache too soon after a threshold increase
+			} else {
+				maybePromote(buffer, sum, filterIndex, subj, pred, obj, context);
+			}
+			if (windowCallCount++ % WINDOW_SIZE == 0) {
+				epoch = (epoch + 1) & Integer.MAX_VALUE;
+				churn /= 2;
+				if(epoch % 128 == 0 && churn < CACHE_THRESHOLD / 2 && CACHE_THRESHOLD > 2) {
+					CACHE_THRESHOLD--;
+					System.out.println("Decreased cache threshold to: " + CACHE_THRESHOLD);
+					timeSinceLastCacheThresholdIncrease = epoch;
+				}
+			}
 		}
+
 
 		return buffer;
 	}
 
+
 	int max = 0;
 
+	int prev = 0;
+	int prev2 = 0;
+
+	int churn = 0;
+	int timeSinceLastCacheThresholdIncrease = 0;
+
 	private void maybePromote(ByteBuffer buffer, long sum, int filterIndex, long subj, long pred, long obj,
-			long context) {
+							  long context) {
+
+		// prev and prev2 store the two previous filter indices we saw, if ours mataches any of these then we can continue, if not we return
+		if (filterIndex != prev && filterIndex != prev2) {
+			prev2 = prev;
+			prev = filterIndex;
+		} else {
+			// System.out.println("Skipping due to locality");
+			return;
+		}
+
 		if (counterEpoch[filterIndex] != epoch) {
 			counters[filterIndex] = 0;
 			counterEpoch[filterIndex] = epoch;
@@ -85,6 +116,18 @@ final class KeyGenerator implements AutoCloseable {
 			CacheEntry existing = cacheEntries[filterIndex];
 			boolean matches = existing != null ? existing.matches(subj, pred, obj, context) : false;
 			if (!matches) {
+				if (existing != null) {
+					churn++;
+					if (churn % 50 == 0) {
+						System.out.println("Cache churn: " + churn + ", current threshold: " + CACHE_THRESHOLD);
+						CACHE_THRESHOLD++;
+						timeSinceLastCacheThresholdIncrease = epoch;
+					}
+				}
+
+//				if(existing != null) {
+//					System.out.println("Churn");
+//				}
 				CacheEntry entry = createEntry(subj, pred, obj, context, buffer);
 				cacheEntries[filterIndex] = entry;
 			}
@@ -93,12 +136,11 @@ final class KeyGenerator implements AutoCloseable {
 			// counters[filterIndex] = newCount;
 		}
 
-		if (windowCallCount++ % WINDOW_SIZE == 0) {
-			epoch = (epoch + 1) & Integer.MAX_VALUE;
-		}
+
 	}
 
 	private CacheEntry createEntry(long subj, long pred, long obj, long context, ByteBuffer buffer) {
+//		System.out.println("Promoting to cache: " + subj + " " + pred + " " + obj + " " + context);
 		ByteBuffer duplicate = buffer.duplicate();
 		ByteBuffer readOnlyView = duplicate.asReadOnlyBuffer();
 		readOnlyView.position(0);
