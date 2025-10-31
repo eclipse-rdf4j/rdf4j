eb12a7a873f1b24741ab2dfdcd0d418010afe118
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2025-10-31 14:18:08 +0100

    GH-5076: Change rdf4j-spring6-demo depencencies so rdf4j-spring6 is actually used

--- spring6-components/rdf4j-spring6-demo/src/main/java/org/eclipse/rdf4j/spring/demo/ArtDemoConfig.java
+++ spring6-components/rdf4j-spring6-demo/src/main/java/org/eclipse/rdf4j/spring/demo/ArtDemoConfig.java
@@ -17,11 +17,7 @@ import org.eclipse.rdf4j.spring.demo.support.InitialDataInserter;
 import org.eclipse.rdf4j.spring.support.DataInserter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.core.io.Resource;
 
 /**
