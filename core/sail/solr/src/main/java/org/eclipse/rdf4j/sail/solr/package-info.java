3cc1fd7d57ccbed745a32b13e5473f6f0263104f
Håvard Ottestad <hmottestad@gmail.com>
2025-12-17 12:49:44 +0100

    formatting

--- core/sail/solr/src/main/java/org/eclipse/rdf4j/sail/solr/package-info.java
+++ core/sail/solr/src/main/java/org/eclipse/rdf4j/sail/solr/package-info.java
@@ -2,7 +2,7 @@
  * Solr-based search integration.
  *
  * @deprecated since 5.3.0 and scheduled for removal. Use alternative search integrations instead, such as the Lucene or
- *     Elasticsearch Sail implementations.
+ *             Elasticsearch Sail implementations.
  */
 @Deprecated(forRemoval = true, since = "5.3.0")
 package org.eclipse.rdf4j.sail.solr;
