543ebb8c80835df021d4fae8016c1789c70eb3ad
Håvard Ottestad <hmottestad@gmail.com>
2025-12-27 07:30:18 +0100

    wip

--- scripts/checkCopyrightPresent.sh
+++ scripts/checkCopyrightPresent.sh
@@ -9,13 +9,13 @@ filesWithAdunaYearAfter2020=()
 expectedHeader=(
   "/*******************************************************************************"
   "__YEAR__"
-  "*"
-  "* All rights reserved. This program and the accompanying materials"
-  "* are made available under the terms of the Eclipse Distribution License v1.0"
-  "* which accompanies this distribution, and is available at"
-  "* http://www.eclipse.org/org/documents/edl-v10.php."
-  "*"
-  "* SPDX-License-Identifier: BSD-3-Clause"
+  " *"
+  " * All rights reserved. This program and the accompanying materials"
+  " * are made available under the terms of the Eclipse Distribution License v1.0"
+  " * which accompanies this distribution, and is available at"
+  " * http://www.eclipse.org/org/documents/edl-v10.php."
+  " *"
+  " * SPDX-License-Identifier: BSD-3-Clause"
   " *******************************************************************************/"
 )
 
