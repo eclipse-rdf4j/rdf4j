1e4ec737d923c2de01c545c2db97ed22828a5330
Håvard Ottestad <hmottestad@gmail.com>
2026-02-24 12:04:48 +0100

    formatting

--- core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/SparqlOptimizerRewriteTest.java
+++ core/sail/memory/src/test/java/org/eclipse/rdf4j/sail/memory/SparqlOptimizerRewriteTest.java
@@ -1,15 +1,15 @@
-package org.eclipse.rdf4j.sail.memory;
-
-/**
- * **************************************************************************** Copyright (c) 2025 Eclipse RDF4J
- * contributors.
+/*******************************************************************************
+ * Copyright (c) 2026 Eclipse RDF4J contributors.
  *
- * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
- * Distribution License v1.0 which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
- * SPDX-License-Identifier: BSD-3-Clause ****************************************************************************
- */
+ * SPDX-License-Identifier: BSD-3-Clause
+ *******************************************************************************/
+
+package org.eclipse.rdf4j.sail.memory;
 
 public class SparqlOptimizerRewriteTest {
 //
