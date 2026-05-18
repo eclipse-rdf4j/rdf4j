89001d4e1067899400cba88597595b5c3bd22cb5
desislava.hristova <desislava.hristova@ontotext.com>
2026-05-18 16:22:33 +0300

    Test fixes of grammar and changes after merge

--- core/queryalgebra/model/src/main/java/org/eclipse/rdf4j/query/algebra/AnnotatedTripleRef.java
+++
@@ -1,4 +0,0 @@
-package org.eclipse.rdf4j.query.algebra;
-
-public class AnnotatedTripleRef {
-}
--- core/spin/src/test/java/org/eclipse/rdf4j/spin/SpinParserTest.java
+++ core/spin/src/test/java/org/eclipse/rdf4j/spin/SpinParserTest.java
@@ -36,7 +36,6 @@ import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.Rio;
 import org.eclipse.rdf4j.rio.helpers.StatementCollector;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -68,8 +67,6 @@ public class SpinParserTest {
 
 	@ParameterizedTest(name = "{0}")
 	@MethodSource("testData")
-	// FIXME RDF1.2
-	@Disabled("Needs to be fixed")
 	public void testSpinParser(String testName, URL testURL) throws IOException, RDF4JException {
 		StatementCollector expected = new StatementCollector();
 		RDFParser parser = Rio.createParser(RDFFormat.TURTLE);
