ced9b36e238aab66550f0a186b9066e21e1fa97a
m-alreeni <Mohammad.Alreeni@iwu.fraunhofer.de>
2026-06-03 10:27:57 +0200

    GH-4315 fix copyright header

--- testsuites/rio/src/main/java/org/eclipse/rdf4j/testsuite/rio/rdfxml/RDFXMLParser12TestCase.java
+++ testsuites/rio/src/main/java/org/eclipse/rdf4j/testsuite/rio/rdfxml/RDFXMLParser12TestCase.java
@@ -6,11 +6,6 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Distribution License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
  * SPDX-License-Identifier: BSD-3-Clause
  *******************************************************************************/
 package org.eclipse.rdf4j.testsuite.rio.rdfxml;
