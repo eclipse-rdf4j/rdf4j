bd902c2e7f9bf8deaf526087f682ec09e45fa06d
Håvard Ottestad <hmottestad@gmail.com>
2026-06-11 13:11:39 +0200

    cleanup

--- core/sail/lmdb/src/test/java/org/eclipse/rdf4j/sail/lmdb/benchmark/ThemeQueryPlanRunBenchmark.java
+++ core/sail/lmdb/src/test/java/org/eclipse/rdf4j/sail/lmdb/benchmark/ThemeQueryPlanRunBenchmark.java
@@ -131,14 +131,14 @@ public class ThemeQueryPlanRunBenchmark {
 
 		@Param({
 				"MEDICAL_RECORDS",
-//				"SOCIAL_MEDIA",
-//				"LIBRARY",
-//				"ENGINEERING",
-//				"HIGHLY_CONNECTED",
-//				"TRAIN",
-//				"ELECTRICAL_GRID",
-//				"PHARMA",
-//				"SPARSE"
+				"SOCIAL_MEDIA",
+				"LIBRARY",
+				"ENGINEERING",
+				"HIGHLY_CONNECTED",
+				"TRAIN",
+				"ELECTRICAL_GRID",
+				"PHARMA",
+				"SPARSE"
 		})
 		public String themeName;
 
