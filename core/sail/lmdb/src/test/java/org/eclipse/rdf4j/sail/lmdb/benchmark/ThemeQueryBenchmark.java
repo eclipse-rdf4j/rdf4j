c31d62855f4ccba9fd82fbe1dbb43bd350e60411
Håvard Ottestad <hmottestad@gmail.com>
2026-07-11 17:49:33 +0200

    refactor

--- core/sail/lmdb/src/test/java/org/eclipse/rdf4j/sail/lmdb/benchmark/ThemeQueryBenchmark.java
+++ core/sail/lmdb/src/test/java/org/eclipse/rdf4j/sail/lmdb/benchmark/ThemeQueryBenchmark.java
@@ -107,29 +107,29 @@ public class ThemeQueryBenchmark {
 
 	@Param({
 			"0",
-//			"1",
-//			"2",
-//			"3",
-//			"4",
+			"1",
+			"2",
+			"3",
+			"4",
 			"5",
-//			"6",
-//			"7",
-//			"8",
-//			"9",
-//			"10",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10",
 //			"11",
 //			"12"
 	})
 	public int z_queryIndex;
 
 	@Param({
-//			"MEDICAL_RECORDS",
-//			"SOCIAL_MEDIA",
-//			"LIBRARY",
-//			"ENGINEERING",
-//			"HIGHLY_CONNECTED",
-//			"TRAIN",
-//			"ELECTRICAL_GRID",
+			"MEDICAL_RECORDS",
+			"SOCIAL_MEDIA",
+			"LIBRARY",
+			"ENGINEERING",
+			"HIGHLY_CONNECTED",
+			"TRAIN",
+			"ELECTRICAL_GRID",
 			"PHARMA"
 	})
 	public String themeName;
