e217160982abb1a691f06e0c6861c498ef4cbbfd
Håvard Ottestad <hmottestad@gmail.com>
2026-09-22 22:41:36 +0200

    cleanup

--- core/sail/lmdb/src/main/java/org/eclipse/rdf4j/sail/lmdb/estimate/LmdbPageCardinalityEstimator.java
+++ core/sail/lmdb/src/main/java/org/eclipse/rdf4j/sail/lmdb/estimate/LmdbPageCardinalityEstimator.java
@@ -22,7 +22,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.eclipse.rdf4j.sail.lmdb.util.GroupMatcher;
